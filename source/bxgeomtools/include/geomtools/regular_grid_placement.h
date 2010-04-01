35028169241d7282be48228015e675d2eb01c184
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-04-01 22:48:20 +0000

    add regular grid placement

---
+++ source/bxgeomtools/include/geomtools/regular_grid_placement.h
@@ -0,0 +1,173 @@
+// -*- mode: c++; -*- 
+/* regular_grid_placement.h
+ * Author(s):     Francois Mauger <mauger@lpccaen.in2p3.fr>
+ * Creation date: 2010-02-26
+ * Last modified: 2010-02-26
+ * 
+ * License: 
+ * 
+ * Description: 
+ *  XY grid 2D placement
+ *
+ *   y^
+ *    |   column  0      1      2      3      4                                          
+ *    |           :      :      :      :      :     row              
+ *    |           x......x......x......x......x......2                           
+ *    |           :10    :11    :12    :13    :14                              
+ * yc +.......... x......x......c......x......x......1                        
+ *    |      dy   :5     :6     :7     :8     :9                        
+ * yo +...........o......x......x......x......x......0                        
+ *    |           :0 dx   1     :2      3      4                      
+ *    |           :             :                                  
+ *    +-----------+-------------+--------------------> x           
+ *   O           xo             xc
+ *
+ *
+ *
+ * History: 
+ * 
+ */
+
+#ifndef __geomtools__regular_grid_placement_h
+#define __geomtools__regular_grid_placement_h 1
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include <geomtools/utils.h>
+#include <geomtools/i_placement.h>
+#include <geomtools/placement.h>
+
+namespace geomtools {
+
+  using namespace std;
+ 
+  class regular_grid_placement
+    : public i_placement 
+    {
+    public:
+      enum mode_t
+	{
+	  MODE_XY = 0,
+	  MODE_YZ = 1,
+	  MODE_XZ = 2
+	};
+
+      static const bool CENTERED = true;
+      static const bool UNCENTERED = false;
+      
+    private:
+      placement __basic_placement;
+      double    __column_step; // y
+      double    __row_step;    // x
+      size_t    __number_of_columns;
+      size_t    __number_of_rows;
+      int       __mode;
+      bool      __centered;
+
+    public:
+ 
+      virtual bool has_only_one_rotation () const;
+
+      bool is_replica () const;
+
+      bool is_valid () const;
+
+      void invalidate ();
+
+      int get_mode () const;
+
+      bool is_mode_xy () const;
+
+      bool is_mode_yz () const;
+
+      bool is_mode_xz () const;
+
+      void set_mode (int m_);
+
+      bool is_centered () const;
+
+      void set_centered (bool c_);
+
+      void set_column_step (double dx_);
+
+      void set_row_step (double dx_);
+
+      void set_steps (double dx_, double dy_);
+
+      double get_column_step () const;
+
+      double get_row_step () const;
+
+      double get_x_step () const;
+
+      double get_y_step () const;
+
+      double get_z_step () const;
+
+      void set_basic_placement (const placement & bp_);
+
+      const placement & get_basic_placement () const;
+
+      placement & get_basic_placement ();
+
+      void set_number_of_rows (size_t nr_);
+
+      void set_number_of_columns (size_t nc_);
+
+      void set_dimensions (size_t nc_, size_t nr_);
+ 
+      size_t get_number_of_rows () const;
+ 
+      size_t get_number_of_columns () const;
+
+      void get_placement (int col_, int row_, placement & p_) const;
+
+      placement get_placement (int col_, int row_) const;
+
+    public: 
+      // i_placement interface:
+      virtual size_t get_number_of_items () const;
+   
+      virtual void get_placement (int item_, placement & p_) const;
+
+    public: 
+      // ctor:
+      regular_grid_placement ();
+		
+      // ctor:
+      regular_grid_placement (const placement & basic_placement_, 
+				 double column_step_,
+				 double row_step_,
+				 size_t number_of_columns_,
+				 size_t number_of_rows_,
+				 int mode_,
+				 bool centered_ = true);
+		
+       // dtor:
+      virtual ~regular_grid_placement ();
+
+      void init (const placement & basic_placement_, 
+		 double column_step_,
+		 double row_step_,
+		 size_t number_of_columns_,
+		 size_t number_of_rows_,
+		 int mode_,
+		 bool centered_ = true);
+      
+      virtual void reset ();
+
+      // i_tree_dump interface:
+      virtual void tree_dump (ostream & out_  = clog, 
+			      const string & title_ = "geomutils::regular_grid_placement", 
+			      const string & indent_ = "", 
+			      bool inherit_ = false) const;
+
+    };
+
+} // end of namespace geomtools
+
+#endif // __geomtools__regular_grid_placement_h
+
+// end of regular_grid_placement.h
---
+++ source/bxgeomtools/src/regular_grid_placement.cc
@@ -0,0 +1,324 @@
+// -*- mode: c++; -*- 
+/* regular_grid_placement.cc 
+ */
+
+#include <geomtools/regular_grid_placement.h>
+
+namespace geomtools {
+
+  using namespace std;
+  
+  bool regular_grid_placement::has_only_one_rotation () const
+  {
+    return true;
+  }
+
+  bool regular_grid_placement::is_replica () const
+  {
+    return false;
+  }
+ 
+  bool regular_grid_placement::is_valid () const
+  {
+    return (__number_of_columns > 0)
+      && (__number_of_rows > 0) 
+      && __basic_placement.is_valid ()
+      && isnormal (__column_step)
+      && isnormal (__row_step);
+  }
+
+  void regular_grid_placement::invalidate ()
+  {
+    reset ();
+  }
+
+  bool regular_grid_placement::is_centered () const
+  {
+    return __centered;
+  }
+
+  void regular_grid_placement::set_centered (bool c_)
+  {
+    __centered = c_;
+  }
+
+  void regular_grid_placement::set_column_step (double dx_)
+  {
+    __column_step = dx_;
+  }
+
+  void regular_grid_placement::set_row_step (double dy_)
+  {
+    __row_step = dy_;
+  }
+
+  void set_steps (double dx_, double dy_);
+
+  void regular_grid_placement::set_steps (double dx_, double dy_)
+  {
+    set_column_step (dx_);
+    set_row_step (dy_);
+  }
+
+  double regular_grid_placement::get_column_step () const
+  {
+    return __column_step;
+  }
+
+  double regular_grid_placement::get_row_step () const
+  {
+    return __row_step;
+  }
+
+  double regular_grid_placement::get_x_step () const
+  {
+    if (is_mode_xy ()) return get_column_step ();
+    if (is_mode_xz ()) return get_column_step ();
+    if (is_mode_yz ()) return 0.0;
+    return 0.0;
+  }
+
+  double regular_grid_placement::get_y_step () const
+  {
+    if (is_mode_xy ()) return get_row_step ();
+    if (is_mode_xz ()) return 0.0;
+    if (is_mode_yz ()) return get_column_step ();
+    return 0.0;
+  }
+
+  double regular_grid_placement::get_z_step () const
+  {
+    if (is_mode_xy ()) return 0.0;
+    if (is_mode_xz ()) return get_row_step ();
+    if (is_mode_yz ()) return get_row_step ();
+    return 0.0;
+  }
+
+  void regular_grid_placement::set_basic_placement (const placement & bp_)
+  {
+    __basic_placement = bp_;
+  }
+  
+  const placement & regular_grid_placement::get_basic_placement () const
+  {
+    return __basic_placement;
+  }
+  
+  placement & regular_grid_placement::get_basic_placement ()
+  {
+    return __basic_placement;
+  }
+
+  void regular_grid_placement::set_number_of_columns (size_t nc_)
+  {
+    __number_of_columns = nc_;
+  }
+
+  void regular_grid_placement::set_number_of_rows (size_t nr_)
+  {
+    __number_of_rows = nr_;
+  }
+  
+  size_t regular_grid_placement::get_number_of_rows () const
+  {
+    return __number_of_rows;
+  }
+  
+  size_t regular_grid_placement::get_number_of_columns () const
+  {
+    return __number_of_columns;
+  }
+  
+  size_t regular_grid_placement::get_number_of_items () const
+  {
+    return __number_of_columns * __number_of_rows;
+  }
+
+  void regular_grid_placement::get_placement (int col_, int row_, placement & p_) const
+  {
+    int item = col_ + row_ * __number_of_columns;
+    get_placement (item, p_);
+  }
+
+  placement regular_grid_placement::get_placement (int col_, int row_) const
+  {
+    placement p;
+    get_placement (col_, row_, p);
+    return p;
+  }
+  
+  void regular_grid_placement::get_placement (int item_, placement & p_) const
+  {
+    p_ = __basic_placement;
+    vector_3d trans = p_.get_translation ();
+    vector_3d step;
+    double x, y, z;
+    if (is_mode_xy ())
+      {
+	x = (item_ % __number_of_columns) * __column_step;
+	y = (item_ / __number_of_columns) * __row_step;
+	z = 0.0;
+	if (__centered)
+	  {
+	    x -= 0.5 * (__number_of_columns - 1) * __column_step;
+	    y -= 0.5 * (__number_of_rows    - 1) * __row_step;
+	  }
+      }
+    if (is_mode_xz ())
+      {
+	x = (item_ % __number_of_columns) * __column_step;
+	z = (item_ / __number_of_columns) * __row_step;
+	y = 0.0;
+	if (__centered)
+	  {
+	    x -= 0.5 * (__number_of_columns - 1) * __column_step;
+	    z -= 0.5 * (__number_of_rows    - 1) * __row_step;
+	  }
+      }
+    if (is_mode_yz ())
+      {
+	y = (item_ % __number_of_columns) * __column_step;
+	z = (item_ / __number_of_columns) * __row_step;
+	x = 0.0;
+	if (__centered)
+	  {
+	    y -= 0.5 * (__number_of_columns - 1) * __column_step;
+	    z -= 0.5 * (__number_of_rows    - 1) * __row_step;
+	  }
+      }
+    step.set (x, y, z);
+    trans += step;
+    p_.set_translation (trans);
+  }
+
+  bool regular_grid_placement::is_mode_xy () const
+  {
+    return __mode == MODE_XY;
+  }
+
+  bool regular_grid_placement::is_mode_yz () const
+  {
+    return __mode == MODE_YZ;
+  }
+  
+  bool regular_grid_placement::is_mode_xz () const
+  {
+    return __mode == MODE_XZ;
+  }
+
+  int regular_grid_placement::get_mode () const
+  {
+    return __mode;
+  }
+
+  void regular_grid_placement::set_mode (int mode_)
+  {
+    if ((mode_ < MODE_XY) || (mode_ > MODE_XZ))
+      {
+	throw runtime_error ("regular_grid_placement: Invalid mode !");
+      }
+    __mode = mode_;
+  }
+ 
+  // ctor:
+  regular_grid_placement::regular_grid_placement () : i_placement ()
+  {
+    reset ();
+  }
+		
+  // ctor:
+  regular_grid_placement::regular_grid_placement (const placement & basic_placement_, 
+					double column_step_,
+					double row_step_,
+					size_t number_of_columns_,
+					size_t number_of_rows_,
+					int mode_,
+					bool centered_) : i_placement ()
+  {
+    init (basic_placement_,
+	  column_step_,
+	  row_step_,
+	  number_of_columns_,
+	  number_of_rows_,
+	  mode_,
+	  centered_);
+  }
+
+  // dtor:
+  regular_grid_placement::~regular_grid_placement ()
+  {
+  }
+ 
+  void regular_grid_placement::init (const placement & basic_placement_, 
+					double column_step_,
+					double row_step_,
+					size_t number_of_columns_,
+					size_t number_of_rows_,
+					int mode_,
+					bool centered_)
+  {
+    set_basic_placement (basic_placement_);
+    set_steps (column_step_, row_step_);
+    set_number_of_columns (number_of_columns_);
+    set_number_of_rows (number_of_rows_);
+    set_mode (mode_);
+    set_centered (centered_);
+  }
+  
+  void regular_grid_placement::reset ()
+  {
+    __basic_placement.invalidate ();
+    __column_step = std::numeric_limits<double>::quiet_NaN();
+    __row_step = std::numeric_limits<double>::quiet_NaN();
+    __number_of_columns = 0;
+    __number_of_rows = 0;
+    __mode = MODE_XY;
+    __centered = true;
+  }
+    
+  void regular_grid_placement::tree_dump (ostream & out_, 
+					    const string & title_, 
+					    const string & indent_, 
+					    bool inherit_) const
+  {
+    namespace du = datatools::utils;
+    string indent;
+    if (! indent_.empty ()) indent = indent_;
+    this->i_placement::tree_dump (out_, title_, indent, true);
+
+    {
+      ostringstream oss_title;
+      oss_title << indent << i_tree_dumpable::tag << "Basic placement :";
+      ostringstream oss_indent;
+      oss_indent << indent << i_tree_dumpable::skip_tag;
+      __basic_placement.tree_dump (out_, 
+				   oss_title.str (), 
+				   oss_indent.str ());
+    }
+
+    out_ << indent << i_tree_dumpable::tag << "Mode : " 
+	 << __mode  << endl;
+
+    out_ << indent << i_tree_dumpable::tag << "Column step : " 
+	 << __column_step  << endl;
+
+    out_ << indent << i_tree_dumpable::tag << "Row step : " 
+	 << __row_step  << endl;
+
+    out_ << indent << i_tree_dumpable::tag << "Number of columns : " 
+	 << __number_of_columns  << endl;
+
+    out_ << indent << i_tree_dumpable::tag << "Number of rows : " 
+	 << __number_of_rows  << endl;
+
+    out_ << indent << i_tree_dumpable::inherit_tag (inherit_)  
+	 << "Centered :" << is_centered () << endl;
+
+    return;
+  }
+
+
+} // end of namespace geomtools
+
+// end of regular_grid_placement.cc
+
