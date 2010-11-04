36a16679b9667033a8172fda6386134edebc7099
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-11-04 12:26:51 +0000

    add comment in some sample program

--- source/bxbrio/programs/test_brio_reader_1.cxx
+++ source/bxbrio/programs/test_brio_reader_1.cxx
@@ -9,8 +9,11 @@ int main (void)
     {
        brio::test::data a_data;
        my_reader.load_next (a_data);
+       // or : my_reader.load (a_data); 
+       /* default 'load' behaviour 
+	* traverses the store sequentially
+	*/
     }
-
   my_reader.close (); // not mandatory (automatic at destruction)
 
   return 0;
