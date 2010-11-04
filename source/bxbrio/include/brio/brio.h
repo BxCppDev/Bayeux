a4e6d1914d61fe8ea6a1a0a2e079024613d273c1
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-11-04 09:54:08 +0000

    add doxygen doc

--- source/bxbrio/include/brio/brio.h
+++ source/bxbrio/include/brio/brio.h
@@ -2,7 +2,7 @@
 /* brio.h global header file 
  * Author(s):     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2010-10-28
- * Last modified: 
+ * Last modified: 2010-11-04
  *
  *
  */
@@ -11,11 +11,30 @@
  *
  * \section intro_section Introduction
  *
- * This section is empty.
+ *  The \t brio package implements some \e writer and \e reader classes that enable
+ *   to store/load arbitrary \e serializable objects using (\e  quasi-)\e portable 
+ *   \e binary \e archives from the Boost/Serialization system and ROOT files and trees 
+ *   as storage media. 
+ *   
+ *   While loading back the data, serialized objects can then be accessed sequentially or 
+ *   non-sequentially while serialized archives (ASCII, XML or binary) using standard
+ *   I/O filestreams can only be accessed sequentially (see the \t datatools::serialization::data_reader
+ *   and \  datatools::serialization::data_writer classes).
+ *
+ *   More, files may contains several \e stores of archived data that can be accessed independantly.
  *
  * \section contents_section Contents 
  *
- * This section is empty.
+ * This version contains the \t brio::writer and the \t brio::reader classes.
+ *
+ * Sample programs can be found in the \t programs directory:
+ * - \t programs/test_data_io.cxx: show how to use a sample serializable \t data class 
+ *   (\t include/brio/test/data.h and \t src/data.cc) and a \e brio \e writer to 
+ * store a list of \t data instances in a ROOT files then reload these instances 
+ * from the file using a \e brio \e reader.
+ * This example makes use of the so-called \e automatic \e store dynamically created 
+ * within the ROOT file.
+ * 
  *
  * \section build_section Installation instructions 
  *
