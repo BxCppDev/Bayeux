d8fd66cf641e6030a8d5ac504225c38b1e88b90f
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-11-09 15:35:04 +0000

    prepare version 1.3 in trunk

--- source/bxgenbb_help/include/genbb_help/genbb_help_version.h
+++ source/bxgenbb_help/include/genbb_help/genbb_help_version.h
@@ -11,13 +11,13 @@
 //  GENBB_HELP_VERSION / 100 % 1000 is the minor version
 //  GENBB_HELP_VERSION / 100000     is the major version
 
-#define GENBB_HELP_VERSION 100200
+#define GENBB_HELP_VERSION 100300
 
 //  GENBB_HELP_LIB_VERSION must be defined to be the same as GENBB_HELP_VERSION
 //  but as a *string* in the form "x.y[.z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define GENBB_HELP_LIB_VERSION "1.2"
+#define GENBB_HELP_LIB_VERSION "1.3"
 
 #endif // __genbb_help__genbb_help_version_h
 
