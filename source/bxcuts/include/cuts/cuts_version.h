948516719b5b4d5f7a7a4a4499ac1b8b18f9bfba
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-11-22 11:23:14 +0000

    prepare version 0.3 in trunk

--- source/bxcuts/include/cuts/cuts_version.h
+++ source/bxcuts/include/cuts/cuts_version.h
@@ -11,13 +11,13 @@
 //  CUTS_VERSION / 100 % 1000 is the minor version
 //  CUTS_VERSION / 100000     is the major version
 
-#define CUTS_VERSION 100
+#define CUTS_VERSION 300
 
 //  CUTS_LIB_VERSION must be defined to be the same as CUTS_VERSION
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define CUTS_LIB_VERSION "0.1"
+#define CUTS_LIB_VERSION "0.3"
 
 #endif // __cuts__cuts_version_h
 
