1a06996182143b0a77d392131564eb3d30a4ec52
Francois Mauger <mauger@lpccaen.in2p3.fr>
2010-11-24 09:23:24 +0000

    prepare fixed version 2.5.3 in trunk

--- source/bxdatatools/include/datatools/datatools_version.h
+++ source/bxdatatools/include/datatools/datatools_version.h
@@ -11,13 +11,13 @@
 //  DATATOOLS_VERSION / 100 % 1000 is the minor version
 //  DATATOOLS_VERSION / 100000     is the major version
 
-#define DATATOOLS_VERSION 200502
+#define DATATOOLS_VERSION 200503
 
 //  DATATOOLS_LIB_VERSION must be defined to be the same as DATATOOLS_VERSION
 //  but as a *string* in the form "x.y[.z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define DATATOOLS_LIB_VERSION "2.5.2"
+#define DATATOOLS_LIB_VERSION "2.5.3"
 
 #endif // __datatools__datatools_version_h
 
