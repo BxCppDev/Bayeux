304fb19be4d0d8d2894f32868f0dbc90aaa2e609
Francois Mauger <mauger@lpccaen.in2p3.fr>
2011-06-02 15:41:36 +0000

    add archive instantiation macros

--- source/bxdatatools/include/datatools/serialization/archives_instantiation.h
+++ source/bxdatatools/include/datatools/serialization/archives_instantiation.h
@@ -1,13 +1,13 @@
 /* -*- mode: c++; -*-
 /* datatools::serialization:: archives_instantiation.h */
-/* 
+/*
  * Description :
  *
  * Define some macro to instantiate serialization methods for all archives
  * supported by datatools.
  *
  * Copyright (C) 2011 Francois Mauger <mauger@lpccaen.in2p3.fr>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or (at
@@ -20,18 +20,19 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
- */ 
+ */
 
 #ifndef __datatools__serialization__archives_instantiation_h
 #define __datatools__serialization__archives_instantiation_h 1
 
 #include <datatools/serialization/archives_list.h>
 
+// intrusive version :
 #define DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE(T,A) \
-template void T::serialize(A & ar, const unsigned int version);  \
+template void T::serialize (A & ar, const unsigned int version);  \
 /**/
 
 #ifdef IOFACTORY_USE_PBA
@@ -51,6 +52,31 @@ DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_oarchi
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_iarchive)    \
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::binary_oarchive) \
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::binary_iarchive) \
+/**/
+#endif // ! IOFACTORY_USE_PBA
+
+// non-intrusive version :
+#define DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,A) \
+template void boost::serialization::serialize (A & ar, T & object, const unsigned int version); \
+/**/
+
+#ifdef IOFACTORY_USE_PBA
+#define DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE_ALL(T)		                    \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::text_oarchive)                  \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::text_iarchive)                  \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_oarchive)                   \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_iarchive)                   \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::quasi_portable_binary_oarchive) \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::quasi_portable_binary_iarchive) \
+/**/
+#else
+#define DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE_ALL(T)		     \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::text_oarchive)   \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::text_iarchive)   \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_oarchive)    \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::xml_iarchive)    \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::binary_oarchive) \
+DATATOOLS_SERIALIZATION_NON_INTRUSIVE_CLASS_SERIALIZE_INSTANTIATE(T,boost::archive::binary_iarchive) \
 /**/
 #endif // ! IOFACTORY_USE_PBA
 
