9a0c6a25a3fa98d476e86747aaae400d8889d6ef
Francois Mauger <mauger@lpccaen.in2p3.fr>
2011-07-01 11:01:42 +0000

    fix DLL build

--- source/bxmaterials/include/materials/materials_version.h
+++ source/bxmaterials/include/materials/materials_version.h
@@ -11,13 +11,13 @@
 //  MATERIALS_VERSION / 100 % 1000 is the minor version
 //  MATERIALS_VERSION / 100000     is the major version
 
-#define MATERIALS_VERSION 200000
+#define MATERIALS_VERSION 200001
 
 //  MATERIALS_LIB_VERSION must be defined to be the same as MATERIALS_VERSION
 //  but as a *string* in the form "x.y[.z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define MATERIALS_LIB_VERSION "2.0"
+#define MATERIALS_LIB_VERSION "2.1"
 
 #endif // __materials__version_h
 
