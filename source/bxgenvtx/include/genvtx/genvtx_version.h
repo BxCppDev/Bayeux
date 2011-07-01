af08acb2873d1b9fea053841cc2239537975259b
Francois Mauger <mauger@lpccaen.in2p3.fr>
2011-07-01 11:08:33 +0000

    fix DLL build

--- source/bxgenvtx/include/genvtx/genvtx_version.h
+++ source/bxgenvtx/include/genvtx/genvtx_version.h
@@ -11,13 +11,13 @@
 //  GENVTX_VERSION / 100 % 1000 is the minor version
 //  GENVTX_VERSION / 100000     is the major version
 
-#define GENVTX_VERSION 200000
+#define GENVTX_VERSION 200001
 
 //  GENVTX_LIB_VERSION must be defined to be the same as GENVTX_VERSION
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define GENVTX_LIB_VERSION "2.0"
+#define GENVTX_LIB_VERSION "2.1"
 
 #endif // __genvtx__version_h
 
