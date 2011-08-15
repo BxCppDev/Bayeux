9305c54c806e7e82aaf7cb4a94f02c6993509568
Xavier Garrido <garrido@lal.in2p3.fr>
2011-08-15 13:00:23 +0000

    constness is always ignored

--- source/bxmaterials/include/materials/material.h
+++ source/bxmaterials/include/materials/material.h
@@ -1,20 +1,20 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 /* material.h
  * Author (s) :  Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2010-09-22
  * Last modified: 2010-09-22
- * 
- * License: 
- * 
- * Description: 
+ *
+ * License:
+ *
+ * Description:
  *   A simple class to handle "abstract" material and their basic properties
- * 
- *  Note: 
+ *
+ *  Note:
  *   some useful data here:
  *     http://www.nucsafe.com/cms/Selecting+Gamma+Detector/43.html
  *
- * History: 
- * 
+ * History:
+ *
  */
 
 #ifndef __materials__material_h
@@ -28,85 +28,85 @@
 #include <datatools/utils/properties.h>
 
 namespace mat {
-  
+
   using namespace std;
   using namespace datatools::utils;
-  
+
   class material;
   class element;
-  
+
   struct compound_entry
   {
     bool             owned;
-    const element *  elt_ptr; 
+    const element *  elt_ptr;
     const material * mat_ptr;
-    double           weight; 
+    double           weight;
     int              nb_of_atoms;
-    
+
     compound_entry ();
     ~compound_entry ();
     bool is_valid () const;
-  }; 
-  
-  typedef std::map<string, compound_entry>   composition_map_t; 
-  
+  };
+
+  typedef std::map<string, compound_entry>   composition_map_t;
+
   class material : public datatools::utils::i_tree_dumpable
   {
-    
+
     public:
-    
-    //! Define the proportion units : 
+
+    //! Define the proportion units :
     /*! KP_ATOM (number of atoms by molecule) or KP_MASS (% mass)
      */
-   
-    enum proportion_unit_type 
-      { 
-	KP_UNDEFINED = -1, 
-	KP_ATOM = 0, 
+
+    enum proportion_unit_type
+      {
+	KP_UNDEFINED = -1,
+	KP_ATOM = 0,
 	NUMBER_OF_ATOMS = 0,
-	KP_MASS = 1, 
+	KP_MASS = 1,
 	FRACTION_MASS = 1,
-	KP_ZA = 2, 
+	KP_ZA = 2,
 	MEAN_ZA = 2
       };
-    
+
       static const double g_per_cm3;
 
-    public: 
-    
-     /* constructor / destructor */   
-     material ();          //!< Defaut Constructor 
-    
-    //! Normal Constructor. 
+    public:
+
+     /* constructor / destructor */
+     material ();          //!< Defaut Constructor
+
+    //! Normal Constructor.
     /*!
       \param name_ : Name of the material
-    */ 
-    material (const string & name_);   
+    */
+    material (const string & name_);
 
     virtual ~material ();          //!<  Destructor
 
 
-   private: 
+   private:
 
     string     __name;    //!< Name
-    double     __density; //!< Density 
+    double     __density; //!< Density
     double     __mean_z;  //!< Mean atomic number
     double     __mean_a;  //!< Mean mass number
 
     // composition:
-  
+
     proportion_unit_type __proportion_unit; //!< KP_ATOM (number of atoms by molecule) or KP_MASS (% mass)
     composition_map_t   __composition;      //!<  composition of the material [std::map<string, compound_entry>]
-        
+
     properties __properties;                //!< datatools properties
     bool       __locked;                    //!< boolean flags : true when composition is validated & derived properties are computed
     void _lock_check (const string & where_) const;
-   
-    void   __compute_molar_mass ();         //!<  Compute molar mass in[g/mol]   
-    void   __lock ();                       //!<  Lock the element : boolean flag '__locked' is set to true     
 
- 
-  public: 
+    void   __compute_molar_mass ();         //!<  Compute molar mass in[g/mol]
+    void   __lock ();                       //!<  Lock the element : boolean flag '__locked' is set to true
+
+
+  public:
 
     bool is_composed_by_mean_z_a () const;
 
@@ -124,7 +124,7 @@ namespace mat {
 
   private:
 
-    void __set_mean_z (double); 
+    void __set_mean_z (double);
 
   public:
 
@@ -132,52 +132,52 @@ namespace mat {
 
   private:
 
-    void __set_mean_a (double); 
+    void __set_mean_a (double);
 
   public:
 
-    void set_mean_z_a (double z_, double a_); 
+    void set_mean_z_a (double z_, double a_);
 
     double get_density () const;
 
-    void set_density (double); 
-    
-    void add_element_by_mass (const element & elt_ref_ , double weight_  , bool owned_ = false);    
-    //!<  Add an element with weight in KP_MASS proportion unit          
-  
-    void add_element_by_mass (const element * elt_ptr_ , double weight_ );                                   
-    //!<  Add an element with weight in KP_MASS proportion unit                                        
+    void set_density (double);
 
-    void add_element_by_nb_of_atoms (const element & elt_ref_ , int nb_of_atoms_  , bool owned_ = false);    
-     //!<  Add an element  in KP_ATOM proportion unit          
-  
-     void add_element_by_nb_of_atoms (const element * elt_ptr_ , int nb_of_atoms_   );                                   
-     //!<  Add an element in KP_ATOM proportion unit             
-          
-     void add_material_by_mass (const material & mat_ref_ , double weight_  , bool owned_ = false);    
-    //!<  Add an material with weight in KP_MASS proportion unit          
-  
-    void add_material_by_mass (const material * mat_ptr_ , double weight_ );                                   
-    //!<  Add an element with weight in KP_MASS proportion unit    
-    
-    void build (); //!<  Build the material :  compute molar mass and lock (or not).                                                                              
+    void add_element_by_mass (const element & elt_ref_ , double weight_  , bool owned_ = false);
+    //!<  Add an element with weight in KP_MASS proportion unit
+
+    void add_element_by_mass (const element * elt_ptr_ , double weight_ );
+    //!<  Add an element with weight in KP_MASS proportion unit
+
+    void add_element_by_nb_of_atoms (const element & elt_ref_ , int nb_of_atoms_  , bool owned_ = false);
+     //!<  Add an element  in KP_ATOM proportion unit
+
+     void add_element_by_nb_of_atoms (const element * elt_ptr_ , int nb_of_atoms_   );
+     //!<  Add an element in KP_ATOM proportion unit
+
+     void add_material_by_mass (const material & mat_ref_ , double weight_  , bool owned_ = false);
+    //!<  Add an material with weight in KP_MASS proportion unit
+
+    void add_material_by_mass (const material * mat_ptr_ , double weight_ );
+    //!<  Add an element with weight in KP_MASS proportion unit
+
+    void build (); //!<  Build the material :  compute molar mass and lock (or not).
   private:
-    
-    void __normalize_weights ();    
 
-  public: 
+    void __normalize_weights ();
 
-     const bool is_locked () const {return __locked;} //!<  Return true if composition is valid, weights are normalized and molar mass is computed.       
+  public:
+
+     bool is_locked () const {return __locked;} //!<  Return true if composition is valid, weights are normalized and molar mass is computed.
 
     const properties & grab_properties () const {return __properties;} //!< Get reference of datatools::properties private attribute
 
     properties & grab_properties () {return __properties;} //!< Get reference of datatools::properties private attribute
-   
-    virtual void tree_dump (ostream & out_  = clog,  
-			    const string & title_  = "",  
-			    const string & indent_ = "",  
+
+    virtual void tree_dump (ostream & out_  = clog,
+			    const string & title_  = "",
+			    const string & indent_ = "",
 			    bool inherit_ = false) const;  //!<  print info virtual method
- 
+
   };
 
 } // end of namespace mat
