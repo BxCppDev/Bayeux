b267b7a343acb9d49135489cbd819f9e44201a41
Francois Mauger <mauger@lpccaen.in2p3.fr>
2011-11-02 20:08:27 +0000

    prepare version 2.1.1 in trunk

--- source/bxmygsl/include/mygsl/mygsl_version.h
+++ source/bxmygsl/include/mygsl/mygsl_version.h
@@ -11,13 +11,13 @@
 //  MYGSL_VERSION / 100 % 1000 is the minor version
 //  MYGSL_VERSION / 100000     is the major version
 
-#define MYGSL_VERSION 200101
+#define MYGSL_VERSION 200102
 
 //  MYGSL_LIB_VERSION must be defined to be the same as MYGSL_VERSION
 //  but as a *string* in the form "x.y[.z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define MYGSL_LIB_VERSION "2.1.1"
+#define MYGSL_LIB_VERSION "2.1.2"
 
 #endif // __mygsl__mygsl_version_h
 
