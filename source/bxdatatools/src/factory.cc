2cc3b8f4ae6dd87ca40d0a24992185f02164a809
Francois Mauger <mauger@lpccaen.in2p3.fr>
2012-04-10 06:11:52 +0000

    add the forgotten  factorysource file

---
+++ source/bxdatatools/src/factory.cc
@@ -0,0 +1,25 @@
+// -*- mode: c++; -*- 
+/* factory.cc
+ */
+
+#include <datatools/factory/factory.h>
+
+namespace datatools {
+
+  namespace factory {
+
+    base_factory_register::base_factory_register ()
+    {
+      return;
+    }
+
+    base_factory_register::~base_factory_register ()
+    {
+      return;
+    }
+
+  } // end of namespace factory 
+
+} // end of namespace datatools 
+
+// end of factory.cc
