3c80e9b220f32f075f08fbd1d75ac8135cb7285f
Ben Morgan <ben.morgan@warwick.ac.uk>
2012-06-21 18:32:20 +0000

    Tidy up formatting of handle classes
    Reorder layout of handle_pool to follow common practice,
    moving implementation outside of class declaration to improve
    clarity of public interface.

--- source/bxdatatools/include/datatools/utils/handle_macros.h
+++ source/bxdatatools/include/datatools/utils/handle_macros.h
@@ -3,14 +3,18 @@
  * Creation date : 2012-03-20
  * Last modified : 2012-03-20
  */
-
-#ifndef __datatools__utils__handle_macros_h
-#define __datatools__utils__handle_macros_h 1
-
-#include <datatools/utils/handle.h>
+#ifndef DATATOOLS_UTILS_HANDLE_MACROS_H_
+#define DATATOOLS_UTILS_HANDLE_MACROS_H_
+// Standard Library
 #include <stdexcept>
 #include <sstream>
+
+// Third Party
+// - Boost
 #include <boost/preprocessor/stringize.hpp>
+
+// This Project
+#include <datatools/utils/handle.h>
 
 /************
  *  MACROS  *
@@ -22,43 +26,33 @@
   /**/
 
 #define DATATOOLS_UTILS_HANDLE_DECLARE_NEW(HandleVar,Type) \
-  datatools::utils::handle<Type> HandleVar (new Type); \
+  datatools::utils::handle<Type> HandleVar(new Type); \
 \
   /**/
 
 #define DATATOOLS_UTILS_HANDLE_GET_REF(RefVar,HandleVar,Type) \
-  if (! HandleVar.has_data ()) \
-    {\
-      std::ostringstream _datatools_handle_get_ref_message;\
-      _datatools_handle_get_ref_message << "macro DATATOOLS_HANDLE" << "_GET_CONST_REF: " \
+  if (! HandleVar.has_data()) {\
+    std::ostringstream _datatools_handle_get_ref_message;\
+    _datatools_handle_get_ref_message << "macro DATATOOLS_HANDLE" << "_GET_CONST_REF: " \
                                               << "Handle '" << BOOST_PP_STRINGIZE(HandleVar) \
                                               << "' has no data !";     \
-      throw std::logic_error (_datatools_handle_get_ref_message.str ());\
+      throw std::logic_error(_datatools_handle_get_ref_message.str());\
     }\
-  const Type & RefVar = HandleVar.get (); \
+  const Type& RefVar = HandleVar.get(); \
 \
   /**/
 
 #define DATATOOLS_UTILS_HANDLE_GRAB_REF(RefVar,HandleVar,Type) \
-  if (! HandleVar.has_data ()) \
-    {\
+  if (! HandleVar.has_data()) {\
       std::ostringstream _datatools_handle_grab_ref_message;\
       _datatools_handle_grab_ref_message << "macro DATATOOLS_HANDLE" << "_GRAB_REF: " \
                                          << "Handle '" << BOOST_PP_STRINGIZE(HandleVar) \
                                          << "' has no data !";\
-      throw std::logic_error (_datatools_handle_grab_ref_message.str ());\
+      throw std::logic_error (_datatools_handle_grab_ref_message.str());\
     }\
-  Type & RefVar = HandleVar.get (); \
+  Type& RefVar = HandleVar.get(); \
 \
   /**/
 
-#endif // __datatools__utils__handle_macros_h 
+#endif // DATATOOLS_UTILS_HANDLE_MACROS_H_
 
-/* end of datatools/utils/handle.h */
-/*
-** Local Variables: --
-** mode: c++ --
-** c-file-style: "gnu" --
-** tab-width: 2 --
-** End: --
-*/
--- source/bxdatatools/include/datatools/utils/handle_pool.h
+++ source/bxdatatools/include/datatools/utils/handle_pool.h
@@ -25,216 +25,254 @@
  *  A pool of handles.
  *
  */
-
-#ifndef __datatools__utils__handle_pool_h
-#define __datatools__utils__handle_pool_h 1
-
+#ifndef DATATOOLS_UTILS_HANDLE_POOL_H
+#define DATATOOLS_UTILS_HANDLE_POOL_H
+// Standard Library
 #include <iostream>
 #include <string>
 #include <stdexcept>
 #include <vector>
 
+// Third Party
+// - A
+
+// This Project
 #include <datatools/utils/handle.h>
 
-// using namespace   std;
 
 namespace datatools {
+namespace utils {
 
-  namespace utils {
+template <class T>
+class handle_pool  {
+ public:
+  typedef T element_type;
+  typedef datatools::utils::handle<element_type> handle_type;
 
-    template <class T>
-    class handle_pool 
-    {
-    public:
-      typedef T element_type;
-      typedef datatools::utils::handle<element_type> handle_type;
+ public:
+  /// Construct an empty pool
+  handle_pool();
 
-    protected:
-      void _resize (size_t a_size)
-      {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::_resize: Entering...\n";
-#endif
-        size_t current_size = _buffer.size ();
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::_resize:   current_size = " << current_size << "\n";
-#endif
-        if (a_size <= current_size)
-          {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-            std::clog << "DEVEL: " << "handle_pool<T>::_resize:   Capacity is enough for " << a_size << "\n";
-#endif
-            return;
-          }
-        _buffer.reserve (a_size);
-        for (int i = current_size; i < (int) a_size; i++)
-          {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-            std::clog << "DEVEL: " << "handle_pool<T>::_resize:   Add a handle with a new element...\n";
-#endif
-            _buffer.push_back (handle_type (new element_type));
-          }
-        _number_of_used_item = 0; 
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::_resize: Exiting.\n";
-#endif
-        return;
-      }
-    public:
+  /// Construct a pool with a default size
+  explicit handle_pool(size_t size);
 
-      void clear ()
-      {
-        _buffer.clear ();
-        return;
-      }
+  /// Destructor
+  virtual ~handle_pool(); 
 
-      size_t get_capacity () const
-      {
-        return _buffer.size ();
-      }
+ public:
+  /// Empty the pool
+  void clear(); 
 
-      size_t get_number_of_used_item () const
-      {
-        return _number_of_used_item;
-      }
+  /// Return the number of handles the pool can contain
+  size_t get_capacity() const;
 
-      void reset ()
-      {
-        _number_of_used_item = 0;
-        return;
-      }
-      void resize (size_t a_size)
-      {
-        if (_number_of_used_item > 0)
-          {
-            throw std::logic_error ("datatools::utils::handle_pool<T>::resize: Operation not allowed !");
-          }
-        _resize (a_size);
-        return;
-      } 
-      handle_pool ()
-      {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::ctor: Entering...\n";
-#endif
-        _number_of_used_item = 0;
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::ctor: Exiting.\n";
-#endif
-        return;
-      }
-      handle_pool (size_t a_size)
-      {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::ctor: Entering...\n";
-#endif
-        _number_of_used_item = 0;
-        _resize (a_size);
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::ctor: Exiting.\n";
-#endif
-        return;
-      }
-      virtual ~handle_pool ()
-      {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::dtor: Entering...\n";
-        std::clog << "DEVEL: " << "handle_pool<T>::dtor: "
-             << "  buffer size is " << _buffer.size () << "\n";
-        std::clog << "DEVEL: " << "handle_pool<T>::dtor: " 
-             << " number of used element " << _number_of_used_item << "\n";
-#endif
-        _number_of_used_item = 0;
-        _buffer.clear ();
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::dtor: Exiting.\n";
-#endif
-        return;
-      }
-      const handle_type & create ()
-      {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::create: Entering...\n";
-        std::clog << "DEVEL: " << "handle_pool<T>::create:   buffer size is " << _buffer.size () << "\n";
-        std::clog << "DEVEL: " << "handle_pool<T>::create: " 
-             << "  number of used element " << _number_of_used_item << "\n";
-#endif
-        if (_number_of_used_item < _buffer.size ())
-          {
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-            std::clog << "DEVEL: " << "handle_pool<T>::create: " 
-                 << "  Use a pre-allocated element @ position " << _number_of_used_item  << " \n";
-            std::clog << "DEVEL: " << "handle_pool<T>::create: Exiting.\n";
-#endif
-            return _buffer[_number_of_used_item++];
-          }
-        element_type * ptr = new element_type;
-        _buffer.push_back (handle_type (ptr));
-        _number_of_used_item++;
-#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
-        std::clog << "DEVEL: " << "handle_pool<T>::create: " 
-             << "  Use a newly allocated element @ position " << (_number_of_used_item - 1) << " \n";
-        std::clog << "DEVEL: " << "handle_pool<T>::create: Exiting.\n";
-#endif
-        return _buffer.back ();
-      }
+  /// Return the number of handles currently in use
+  size_t get_number_of_used_item() const; 
 
-      void dump (std::ostream & a_out, const std::string & a_title, bool short_ = true) const
-      {
-        if (! a_title.empty ())
-          {
-            a_out << a_title << " : " << std::endl;
-          }
-        a_out << "|-- " << "Capacity     : " << get_capacity () << std::endl;
-        a_out << "|-- " << "# Used items : " << _number_of_used_item << std::endl;
-        a_out << "`-- " << "Items @ " << std::endl;
-        for (int i = 0; i < _buffer.size (); i++)
-          {
-            if (short_)
-              {
-                int max_short = 10;
-                if (i >= max_short) continue;
-              }
-            a_out << "    ";
-            const handle_type & h = _buffer[i];
-            if (i < _buffer.size () - 1)
-              {
-                a_out << "|-- ";
-              }
-            else
-              {
-                a_out << "`-- ";
-              }
-            a_out << "index " << i << " : ";
-            if (! h.has_data ())
-              {
-                a_out << "No element!" << std::endl;
-              }
-            else
-              {
-                a_out << "@ " << &h.get () << " [" << h.count () << "]"<< std::endl;
-              } 
-          }
-        return;
-      }
+  /// Set the number of used handles in the pool to zero
+  void reset(); 
 
-    protected:
+  /// Change number of handles pool can contain
+  void resize(size_t size); 
 
-      std::vector<handle_type> _buffer;
-      size_t                   _number_of_used_item;
+  /// Return a const reference to a new handle
+  const handle_type& create(); 
 
-    };
+  /// Input pool capacity and used handles to an output stream
+  void dump(std::ostream& out, const std::string& title, 
+            bool abridged = true) const;
+  
+ protected:
+  /// Implementation for public resize method
+  void resize_impl(size_t size);
 
-  } // namespace utils 
+ protected:
+  std::vector<handle_type> buffer_; //<! Collection of handles held in the pool
+  size_t number_of_used_item_; //<! Number of pool handles currently in use
+};
 
+
+//----------------------------------------------------------------------
+// Implementation of handle_pool methods
+
+template <typename T>
+handle_pool<T>::handle_pool() {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::ctor: Entering...\n";
+#endif
+  number_of_used_item_ = 0;
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::ctor: Exiting.\n";
+#endif
+}
+
+
+template <typename T>
+handle_pool<T>::handle_pool(size_t size) {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::ctor: Entering...\n";
+#endif
+  number_of_used_item_ = 0;
+  this->resize_impl(size);
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::ctor: Exiting.\n";
+#endif
+}
+
+
+template <typename T>
+handle_pool<T>::~handle_pool() {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::dtor: Entering...\n";
+  std::clog << "DEVEL: " << "handle_pool<T>::dtor: "
+      << "  buffer size is " << buffer.size() << "\n";
+  std::clog << "DEVEL: " << "handle_pool<T>::dtor: " 
+      << " number of used element " << _number_of_used_item << "\n";
+#endif
+  number_of_used_item_ = 0;
+  buffer_.clear();
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::dtor: Exiting.\n";
+#endif
+}
+
+
+template <typename T>
+void handle_pool<T>::clear() {
+  buffer_.clear();
+}
+
+
+template <typename T>
+size_t handle_pool<T>::get_capacity() const {
+  return buffer_.size();
+}
+
+
+template <typename T>
+size_t handle_pool<T>::get_number_of_used_item() const {
+  return number_of_used_item_;
+}
+
+
+template <typename T>
+void handle_pool<T>::reset() {
+  number_of_used_item_ = 0;
+}
+
+
+template <typename T>
+void handle_pool<T>::resize(size_t size) {
+  if (number_of_used_item_ > 0) {
+    throw std::logic_error("datatools::utils::handle_pool<T>::resize: Cannot resize pool with items in use");
+  }
+  this->resize_impl(size);
+} 
+
+
+template <typename T>
+const typename handle_pool<T>::handle_type& handle_pool<T>::create() {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::create: Entering...\n";
+  std::clog << "DEVEL: " << "handle_pool<T>::create:   buffer size is " 
+            << buffer_.size() << "\n";
+  std::clog << "DEVEL: " << "handle_pool<T>::create: " 
+            << "  number of used element " 
+            << number_of_used_item_ << "\n";
+#endif
+
+  if (number_of_used_item_ < buffer_.size()) {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+    std::clog << "DEVEL: " << "handle_pool<T>::create: " 
+        << "  Use a pre-allocated element @ position " 
+        << number_of_used_item_  << " \n";
+    std::clog << "DEVEL: " << "handle_pool<T>::create: Exiting.\n";
+#endif
+    return buffer_[number_of_used_item_++];
+  }
+
+  element_type *ptr = new element_type;
+  buffer_.push_back(handle_type(ptr));
+  number_of_used_item_++;
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::create: " 
+      << "  Use a newly allocated element @ position " 
+      << (number_of_used_item_ - 1) << " \n";
+  std::clog << "DEVEL: " << "handle_pool<T>::create: Exiting.\n";
+#endif
+  return buffer_.back();
+}
+
+
+template <typename T>
+void handle_pool<T>::dump(std::ostream& out, const std::string& title, 
+                          bool abridged) const {
+  if (!title.empty()) {
+    out << title << " : " << std::endl;
+  }
+  out << "|-- " << "Capacity     : " << this->get_capacity() << std::endl;
+  out << "|-- " << "# Used items : " << number_of_used_item_ << std::endl;
+  out << "`-- " << "Items @ " << std::endl;
+  for (size_t i = 0; i < buffer_.size(); ++i) {
+    if (abridged) {
+      if (i >= 10) continue;
+    }
+    out << "    ";
+    const handle_type& h = buffer_[i];
+    if (i < buffer_.size() - 1) {
+      out << "|-- ";
+    } else {
+      out << "`-- ";
+    }
+    out << "index " << i << " : ";
+    if (!h.has_data()) {
+      out << "No element!" << std::endl;
+    }
+    else {
+      out << "@ " << &h.get() << " [" << h.count() << "]"<< std::endl;
+    } 
+  }
+}
+
+
+template <typename T>
+void handle_pool<T>::resize_impl(size_t size) {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::resize: Entering...\n";
+#endif
+
+  size_t current_size = buffer_.size();
+
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::resize:   current_size = " << current_size << "\n";
+#endif
+
+  if (size <= current_size) {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+    std::clog << "DEVEL: " << "handle_pool<T>::resize:   Capacity is enough for " << size << "\n";
+#endif
+    return;
+  }
+
+  buffer_.reserve(size);
+  for (size_t i = current_size; i < size; ++i) {
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+    std::clog << "DEVEL: " << "handle_pool<T>::resize:   Add a handle with a new element...\n";
+#endif
+    buffer_.push_back(handle_type(new element_type));
+  }
+
+  number_of_used_item_ = 0; 
+
+#ifdef DATATOOLS_UTILS_HANDLE_POOL_DEVEL
+  std::clog << "DEVEL: " << "handle_pool<T>::_resize: Exiting.\n";
+#endif
+
+  return;
+}
+
+} // namespace utils 
 } // namespace datatools 
 
-#endif // __datatools__utils__handle_pool_h
-/*
-** Local Variables: --
-** mode: c++ --
-** c-file-style: "gnu" --
-** tab-width: 2 --
-** End: --
-*/
+#endif // DATATOOLS_UTILS_HANDLE_POOL_H
 
