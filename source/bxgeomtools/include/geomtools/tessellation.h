afbe90864ee5449d6f6643953b8287e6244c3dad
Xavier Garrido <garrido@lal.in2p3.fr>
2012-09-13 13:26:00 +0000

    fix bad integer assignment

--- source/bxgeomtools/include/geomtools/tessellation.h
+++ source/bxgeomtools/include/geomtools/tessellation.h
@@ -1,15 +1,15 @@
-// -*- mode: c++; -*- 
+// -*- mode: c++; -*-
 /* tessellation.h
  * Author(s):     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2010-06-04
  * Last modified: 2010-06-04
- * 
- * License: 
- * 
- * Description: 
- * 
- * History: 
- * 
+ *
+ * License:
+ *
+ * Description:
+ *
+ * History:
+ *
  */
 
 #ifndef __geomtools__tessellation_h
@@ -67,8 +67,8 @@ namespace geomtools {
     virtual size_t get_number_of_vertices () const;
     virtual const facet_vertex & get_vertex (int i_) const;
     facet3 ();
-    facet3 (const facet_vertex & v1_, 
-            const facet_vertex & v2_, 
+    facet3 (const facet_vertex & v1_,
+            const facet_vertex & v2_,
             const facet_vertex & v3_);
 
   };
@@ -83,16 +83,16 @@ namespace geomtools {
     virtual size_t get_number_of_vertices () const;
     virtual const facet_vertex & get_vertex (int i_) const;
     facet4 ();
-    facet4 (const facet_vertex & v1_, 
-            const facet_vertex & v2_, 
-            const facet_vertex & v3_, 
+    facet4 (const facet_vertex & v1_,
+            const facet_vertex & v2_,
+            const facet_vertex & v3_,
             const facet_vertex & v4_);
 
   };
 
   /*** tessellated_solid ***/
 
-  class tessellated_solid : public i_shape_3d 
+  class tessellated_solid : public i_shape_3d
   {
   public:
     static const int32_t FACE_ALL= 0xFFFFFFFF;
@@ -100,17 +100,17 @@ namespace geomtools {
     static const std::string TESSELLATED_LABEL;
     typedef map<unsigned int, facet_vertex> vertices_col_t;
     typedef map<unsigned int, i_facet *> facets_col_t;
-    static const int INVALID_VERTEX = -1.0;
+    static const int INVALID_VERTEX = -1;
     static const int MAX_VERTEX     = 0x0FFFFFFF;
-    
+
   private:
     bool           _locked_;
     bool           _consistent_;
     vertices_col_t _vertices_;
     facets_col_t   _facets_;
-    
+
   public:
-      
+
     virtual std::string get_shape_name () const;
 
     static bool validate_index (int);
@@ -121,13 +121,13 @@ namespace geomtools {
     const vertices_col_t & vertices () const;
     const facets_col_t & facets () const;
     int add_vertex (unsigned int vtx_key_, const facet_vertex & vtx_);
-    int add_vertex (unsigned int vtx_key_, 
+    int add_vertex (unsigned int vtx_key_,
                     double x_, double y_, double z_);
-    void add_facet (unsigned int facet_key_, 
+    void add_facet (unsigned int facet_key_,
                     int ivtx0_, int ivtx1_, int ivtx2_, int ivtx3_ = INVALID_VERTEX);
-    void add_facet3 (unsigned int facet_key_, 
+    void add_facet3 (unsigned int facet_key_,
                      int ivtx0_, int ivtx1_, int ivtx2_);
-    void add_facet4 (unsigned int facet_key_, 
+    void add_facet4 (unsigned int facet_key_,
                      int ivtx0_, int ivtx1_, int ivtx2_, int ivtx3_);
     void lock ();
     void unlock ();
@@ -135,27 +135,27 @@ namespace geomtools {
     void dump (ostream & out_ = clog) const;
 
     //void initialize (const string & filename_);
-      
-    virtual bool is_inside (const vector_3d &, 
+
+    virtual bool is_inside (const vector_3d &,
                             double skin_ = GEOMTOOLS_PROPER_TOLERANCE) const;
 
     // if 'skin' < 0 no skin is taken into account:
-    virtual bool is_on_surface (const vector_3d & , 
-                                int index_   = FACE_ALL , 
+    virtual bool is_on_surface (const vector_3d & ,
+                                int index_   = FACE_ALL ,
                                 double skin_ = GEOMTOOLS_PROPER_TOLERANCE) const;
-    
+
     virtual vector_3d get_normal_on_surface (const vector_3d & position_) const;
-    
-    virtual bool find_intercept (const vector_3d & from_, 
+
+    virtual bool find_intercept (const vector_3d & from_,
                                  const vector_3d & direction_,
                                  intercept_t & intercept_,
                                  double skin_ = GEOMTOOLS_PROPER_TOLERANCE) const;
-    
+
   private:
     bool __check ();
 
   };
-  
+
 } // end of namespace geomtools
 
 #endif // __geomtools__tessellation_h
