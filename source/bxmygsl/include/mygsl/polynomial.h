ac6d57885ce5bfdfcea0d9d2bfc5d60a529d00d6
Ben Morgan <ben.morgan@warwick.ac.uk>
2013-02-25 22:03:13 +0000

    pImplize polynomial
    Refactor data members of polynomial::solver inner class into a pImpl
    to hide usage of gsl_poly_complex_workspace object. Move all GSL
    headers and function calls into implementation file.

--- source/bxmygsl/include/mygsl/polynomial.h
+++ source/bxmygsl/include/mygsl/polynomial.h
@@ -3,124 +3,89 @@
 #ifndef __mygsl__polynomial_h 
 #define __mygsl__polynomial_h 1
 
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_fit.h>
-
 #include <vector>
-
-#include <gsl/gsl_poly.h>
 
 #include <mygsl/unary_eval.h>
 
-// using namespace   std;
-
 namespace mygsl {
+class polynomial : public unary_eval {
+ public:
+  polynomial(size_t sz_);
 
-  class polynomial : public unary_eval
-  {
-  private:
+  polynomial(double c0_);
 
-    size_t         _size_;
-    std::vector<double> _c_;
+  polynomial(double c0_, double c1_);
 
-  public:
+  polynomial(double c0_, double c1_, double c2_);
 
-    size_t get_degree () const;
+  polynomial(const std::vector<double>& c_);
 
-    size_t get_ncoeffs () const;
+  polynomial(const polynomial& p_);
 
-    double get_coeff (size_t i_) const;
+  virtual ~polynomial();
 
-    void set_coeff (size_t i_, double c_);
+  size_t get_degree() const;
 
-    polynomial (size_t sz_);
+  size_t get_ncoeffs() const;
+  double get_coeff(size_t i_) const;
+  void set_coeff(size_t i_, double c_);
 
-    polynomial (double c0_);
+  double eval(double x_) const;
 
-    polynomial (double c0_, double c1_);
+  void print (std::ostream& out_ = std::clog, 
+              int format_ = 0, 
+              bool eol_ = false) const;
 
-    polynomial (double c0_, double c1_, double c2_);
+  /*
+   * L(x) = p0 + p1 * x^1
+   *
+   */
+  static bool solve_linear(double p0_, double p1_, size_t& nsols_,
+                           double& x0_);
 
-    polynomial (const std::vector<double> & c_);
+  /*
+   * Q(x) = p0 + p1 * x^1 + p2 * x^2
+   *
+   */
+  static bool solve_quadratic(double p0_, double p1_, double p2_, 
+                              size_t & nsols_, double & x0_, double & x1_);
 
-    polynomial (const polynomial & p_);
+  /*
+   * C(x) =  p0 + p1 * x^1 + p2 * x^2 + x^3
+   *
+   */
+  static bool solve_cubic(double p0_, double p1_, double p2_, 
+                          size_t & nsols_, 
+                          double & x0_, double & x1_, double & x2_);
 
-    virtual ~polynomial ();
+  /*
+   * C(x) = p0 + p1 * x^1 + p2 * x^2 + p3 * x^3
+   *
+   */
+  static bool solve_cubic(double p0_, double p1_, double p2_, double p3_,
+                          size_t & nsols_,
+                          double & x0_, double & x1_, double & x2_);
 
-    double eval (double x_) const;
+  class solver {
+   public:
+    solver(size_t sz_ = 0);
+    virtual ~solver();
+    bool solve(const polynomial& p_);
+ 
+   private:
+    void _init_(size_t);
+    void _reset_();
 
-    void print (std::ostream & out_ = std::clog, 
-                int format_ = 0, 
-                bool eol_ = false) const;
-
-
-    /*
-     * L(x) = p0 + p1 * x^1
-     *
-     */
-    static bool solve_linear (double p0_, double p1_, 
-                              size_t & nsols_,
-                              double & x0_);
-
-
-    /*
-     * Q(x) = p0 + p1 * x^1 + p2 * x^2
-     *
-     */
-    static bool solve_quadratic (double p0_, double p1_, double p2_, 
-                                 size_t & nsols_,
-                                 double & x0_, double & x1_);
-
-
-    /*
-     * C(x) =  p0 + p1 * x^1 + p2 * x^2 + x^3
-     *
-     */
-    static bool solve_cubic (double p0_, double p1_, double p2_, 
-                             size_t & nsols_,
-                             double & x0_, double & x1_, double & x2_);
-
-
-    /*
-     * C(x) = p0 + p1 * x^1 + p2 * x^2 + p3 * x^3
-     *
-     */
-    static bool solve_cubic (double p0_, double p1_, double p2_, double p3_,
-                             size_t & nsols_,
-                             double & x0_, double & x1_, double & x2_);
-  
-    class solver
-    {
-    private:
-      int    _status_;
-      size_t _sz_;
-      gsl_poly_complex_workspace * _ws_;
-
-    private:
-
-      void _init_ (size_t);
-
-      void _reset_ ();
-
-    public:
-
-      solver (size_t sz_ = 0);
-
-      virtual ~solver ();
-
-      bool solve (const polynomial & p_);
-
-    };
-
+   private:
+    struct solver_impl;
+    solver_impl *pImpl;
   };
 
+ private:
+  size_t         _size_;
+  std::vector<double> _c_;
+};
 }
 
 #endif // __mygsl__polynomial_h
 
-/* Local Variables: */
-/* mode: c++        */
-/* coding: utf-8    */
-/* End:             */
-
-// end of mygsl::polynomial.h
--- source/bxmygsl/src/polynomial.cc
+++ source/bxmygsl/src/polynomial.cc
@@ -6,311 +6,228 @@
 #include <sstream>
 #include <cmath> // for std::abs
 
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fit.h>
+#include <gsl/gsl_poly.h>
+
 namespace mygsl {
+using namespace std;
 
-  using namespace std;
+size_t polynomial::get_ncoeffs() const {
+  return _c_.size();
+}
 
-  size_t polynomial::get_ncoeffs () const
-  {
-    return _c_.size ();
-  }
-  
-  size_t polynomial::get_degree () const
-  {
-    return get_ncoeffs () - 1;
-  }
-  
-  double polynomial::get_coeff (size_t i_) const
-  {
-    return _c_.at (i_);
-  }
-  
-  void polynomial::set_coeff (size_t i_, double c_)
-  {
-    if (i_ > get_degree ())
-      {
-        size_t sz = _c_.size ();
-        _c_.resize (i_ + 1);
-        for (int i = sz; i < _c_.size (); i++)
-          {
-            _c_[i] = 0.0;
-          }
-      }
-    _c_.at (i_) = c_;
-    return;
-  }
-  
-  polynomial::polynomial (size_t degree_)
-  {
-    _c_.assign (degree_ + 1, 0.0);
-    _c_ [degree_] = 1.0;
-    return;
-  }
-  
-  polynomial::polynomial (double c0_)
-  {
-    _c_.assign (1, c0_);
-    return;
-  }
-  
-  polynomial::polynomial (double c0_, double c1_)
-  {
-    _c_.assign (2, 0.0);
-    _c_ [0] = c0_;
-    _c_ [1] = c1_;
-    return;
-  }
-  
-  polynomial::polynomial (double c0_, double c1_, double c2_)
-  {
-    _c_.assign (3, 0.0);
-    _c_ [0] = c0_;
-    _c_ [1] = c1_;
-    _c_ [2] = c2_;
-    return;
-  }
-  
-  polynomial::polynomial (const vector<double> & c_)
-  {
-    _c_ = c_;
-    return;
-  }
-  
-  polynomial::polynomial (const polynomial & p_)
-  {
-    _c_ = p_._c_;
-    return;
-  }
-  
-  polynomial::~polynomial ()
-  {
-    _c_.clear ();
-    return;
-  }
-   
-  double polynomial::eval (double x_) const
-  {
-    size_t sz = _c_.size ();
-    const double * first_arg = 0;
-    if (sz > 0)
-      {
-        first_arg = &(_c_[0]);
-      }
-    return gsl_poly_eval (first_arg, sz, x_);
-  }
+size_t polynomial::get_degree() const {
+  return get_ncoeffs() - 1;
+}
 
-  void polynomial::print (ostream & out_, int format_, bool eol_) const
-  {
-    string prod_sym = "×";
-    string exp_sym  = "^";
-    prod_sym = "*";
-    exp_sym  = "**";
-    if (format_ == 1)
-      {
-        prod_sym = "×";
-        exp_sym  = "^";
-      }
-    bool first = true;
-    for (int deg = 0; deg <= get_degree (); deg++)
-      {
-        if (_c_[deg] == 0.0)
-          {
-            continue;
-          }
-        else
-          {
-            if (! first)
-              {
-                out_ << (_c_[deg] > 0? " + ": " - ");
-              }
-            else
-              {
-                out_ << (_c_[deg] > 0? "": " - ");
-                first = false;      
-              }
-            out_.unsetf (ios::showpos);
-            // 2011-06-16 FM: add std::abs to remove template ambiguity for some compiler :
-            out_ << std::abs(_c_[deg]);
-            if (deg >= 1)
-              {
-                out_ << ' ' << prod_sym << ' ' << 'X';
-              } 
-            if (deg >= 2)
-              {
-                out_ << exp_sym << deg;
-              } 
-          }
-      }
-    if (eol_) out_ << endl;
-  }
+double polynomial::get_coeff(size_t i_) const {
+  return _c_.at(i_);
+}
 
-  bool polynomial::solve_linear (double p0_, 
-                                 double p1_, 
-                                 size_t & nsols_,
-                                 double & x0_)
-  {
-    if (p1_ == 0.0)
-      {
-        if (p0_ == 0.0)
-          {
-            nsols_ = -1;
-            return false;
-          }
-        else
-          {
-            nsols_ = 0;
-            return false;
-          }
-        x0_ = GSL_NAN;
-      }
-    x0_ = -p0_ / p1_;
-    nsols_ = 1;
-    return true;
-  }
-
-  bool polynomial::solve_quadratic (double p0_, 
-                                    double p1_, 
-                                    double p2_, 
-                                    size_t & nsols_,
-                                    double & x0_, double &  x1_)
-  {
-    if (p2_ == 0)
-      {
-        x1_ = GSL_NAN;
-        return solve_linear (p0_, p1_, nsols_, x0_);    
-      }
-    nsols_ = (size_t) gsl_poly_solve_quadratic (p2_, p1_, p0_, &x0_, &x1_);
-    if (nsols_ < 1)
-      {
-        x0_ = GSL_NAN;
-      }
-    if (nsols_ < 2)
-      {
-        x1_ = GSL_NAN;
-      }
-    return nsols_ > 0;
-  } 
-
-  bool polynomial::solve_cubic (double p0_, 
-                                double p1_, 
-                                double p2_, 
-                                size_t & nsols_,
-                                double & x0_, 
-                                double & x1_, 
-                                double & x2_)
-  {
-    nsols_ = (size_t) gsl_poly_solve_cubic (p2_, p1_, p0_, &x0_, &x1_, &x2_);
-    if (nsols_ < 1)
-      {
-        x0_ = GSL_NAN;
-      }
-    if (nsols_ < 2)
-      {
-        x1_ = GSL_NAN;
-      }
-    if (nsols_ < 3)
-      {
-        x2_ = GSL_NAN;
-      }
-    return nsols_ > 0;
-  }
-
-  bool polynomial::solve_cubic (double p0_, 
-                                double p1_, 
-                                double p2_,
-                                double p3_,
-                                size_t & nsols_,
-                                double & x0_, 
-                                double & x1_, 
-                                double & x2_)
-  {
-    if (p3_ == 0)
-      {
-        x2_ = GSL_NAN;  
-        return solve_quadratic (p0_, p1_, p2_, nsols_, x0_, x1_);
-      }
-    nsols_ = (size_t) solve_cubic (p2_ / p3_, p1_ / p3_, p0_ / p3_, nsols_, x0_, x1_, x2_);
-    if (nsols_ < 1)
-      {
-        x0_ = GSL_NAN;
-      }
-    if (nsols_ < 2)
-      {
-        x1_ = GSL_NAN;
-      }
-    if (nsols_ < 3)
-      {
-        x2_ = GSL_NAN;
-      }
-    return nsols_ > 0;
-  }
-
-  /**************************************/
-
-  void polynomial::solver::_init_ (size_t sz_)
-  {
-    _status_ = GSL_EFAILED;
-    if (_ws_ != 0)
-      {
-        if (_sz_ >= sz_)
-          {
-            return;
-          }
-        else
-          {
-            _reset_ ();
-          }
-      }
-    if (sz_ > 1)
-      {
-        _ws_ = gsl_poly_complex_workspace_alloc (sz_);
-        _sz_ = sz_;
-      }
-    return;
-  }
-
-  void polynomial::solver::_reset_ ()
-  {
-    if (_ws_ != 0)
-      {
-        gsl_poly_complex_workspace_free (_ws_);
-        _ws_ = 0;
-        _sz_ = 0;
-      }
-    _status_ = GSL_EFAILED;
-    return;
-  }
-
-  polynomial::solver::solver (size_t sz_)
-  {
-    _status_ = GSL_EFAILED;
-    _sz_ = 0;
-    _ws_ = 0;
-    _init_ (sz_);
-    return;
-  }
-
-  polynomial::solver::~solver ()
-  {
-    _reset_ ();
-    return;
-  }
-
-  bool polynomial::solver::solve (const polynomial & p_)
-  {
-    size_t deg = p_.get_degree ();
-    _init_ (deg + 1);
-    {
-      throw logic_error ("polynomial::solver::solve:  Not implemented yet!");
+void polynomial::set_coeff(size_t i_, double c_) {
+  if (i_ > get_degree()) {
+    size_t sz = _c_.size();
+    _c_.resize(i_ + 1);
+    for (int i = sz; i < _c_.size(); ++i) {
+      _c_[i] = 0.0;
     }
-    return _status_ == GSL_SUCCESS;
   }
+  _c_.at(i_) = c_;
+}
+
+polynomial::polynomial(size_t degree_) {
+  _c_.assign(degree_ + 1, 0.0);
+  _c_[degree_] = 1.0;
+}
+
+polynomial::polynomial(double c0_) {
+  _c_.assign(1, c0_);
+}
+
+polynomial::polynomial(double c0_, double c1_) {
+  _c_.assign(2, 0.0);
+  _c_[0] = c0_;
+  _c_[1] = c1_;
+}
+
+polynomial::polynomial(double c0_, double c1_, double c2_) {
+  _c_.assign(3, 0.0);
+  _c_[0] = c0_;
+  _c_[1] = c1_;
+  _c_[2] = c2_;
+}
+
+polynomial::polynomial(const vector<double>& c_) {
+  _c_ = c_;
+}
+
+polynomial::polynomial(const polynomial& p_) {
+  _c_ = p_._c_;
+}
+
+polynomial::~polynomial() {
+  _c_.clear();
+}
+
+double polynomial::eval(double x_) const {
+  size_t sz = _c_.size();
+  const double * first_arg = 0;
+  if (sz > 0) {
+    first_arg = &(_c_[0]);
+  }
+  return gsl_poly_eval(first_arg, sz, x_);
+}
+
+void polynomial::print(ostream & out_, int format_, bool eol_) const {
+  string prod_sym = "×";
+  string exp_sym  = "^";
+  prod_sym = "*";
+  exp_sym  = "**";
+  if (format_ == 1) {
+    prod_sym = "×";
+    exp_sym  = "^";
+  }
+  bool first = true;
+  for (int deg = 0; deg <= get_degree(); ++deg) {
+    if (_c_[deg] == 0.0) {
+      continue;
+    } else {
+      if (!first) {
+        out_ << (_c_[deg] > 0? " + ": " - ");
+      } else {
+        out_ << (_c_[deg] > 0? "": " - ");
+        first = false;      
+      }
+      out_.unsetf(ios::showpos);
+      // 2011-06-16 FM: add std::abs to remove template ambiguity for some compiler :
+      out_ << std::abs(_c_[deg]);
+      if (deg >= 1) {
+        out_ << ' ' << prod_sym << ' ' << 'X';
+      } 
+      if (deg >= 2) {
+        out_ << exp_sym << deg;
+      } 
+    }
+  }
+  if (eol_) out_ << endl;
+}
+
+bool polynomial::solve_linear(double p0_, double p1_, size_t& nsols_,
+                              double &x0_) {
+  if (p1_ == 0.0) {
+    if (p0_ == 0.0) {
+      nsols_ = -1;
+      return false;
+    } else {
+      nsols_ = 0;
+      return false;
+    }
+    x0_ = GSL_NAN;
+  }
+  x0_ = -p0_ / p1_;
+  nsols_ = 1;
+  return true;
+}
+
+bool polynomial::solve_quadratic(double p0_, double p1_, double p2_, 
+                                 size_t& nsols_,
+                                 double& x0_, double& x1_) {
+  if (p2_ == 0) {
+    x1_ = GSL_NAN;
+    return solve_linear(p0_, p1_, nsols_, x0_);    
+  }
+  nsols_ = (size_t)gsl_poly_solve_quadratic(p2_, p1_, p0_, &x0_, &x1_);
+  if (nsols_ < 1) x0_ = GSL_NAN;
+  
+  if (nsols_ < 2) x1_ = GSL_NAN;
+  
+  return nsols_ > 0;
+} 
+
+bool polynomial::solve_cubic(double p0_, double p1_, double p2_, 
+                             size_t& nsols_,
+                             double& x0_, double& x1_, double& x2_) {
+  nsols_ = (size_t)gsl_poly_solve_cubic(p2_, p1_, p0_, &x0_, &x1_, &x2_);
+  
+  if (nsols_ < 1) x0_ = GSL_NAN;
+  
+  if (nsols_ < 2) x1_ = GSL_NAN;
+  
+  if (nsols_ < 3) x2_ = GSL_NAN;
+  
+  return nsols_ > 0;
+}
+
+bool polynomial::solve_cubic(double p0_, double p1_, double p2_, double p3_,
+                             size_t& nsols_,
+                             double& x0_, double& x1_, double& x2_) {
+  if (p3_ == 0) {
+    x2_ = GSL_NAN;  
+    return solve_quadratic(p0_, p1_, p2_, nsols_, x0_, x1_);
+  }
+
+  nsols_ = (size_t)solve_cubic(p2_/p3_, p1_/p3_, p0_/p3_, nsols_, 
+                               x0_, x1_, x2_);
+  if (nsols_ < 1) x0_ = GSL_NAN;
+  
+  if (nsols_ < 2) x1_ = GSL_NAN;
+  
+  if (nsols_ < 3) x2_ = GSL_NAN;
+  
+  return nsols_ > 0;
+}
+
+//----------------------------------------------------------------------
+// Inner solver class implementation
+//
+struct polynomial::solver::solver_impl {
+  int    _status_;
+  size_t _sz_;
+  gsl_poly_complex_workspace *_ws_;
+};
+
+polynomial::solver::solver(size_t sz_) {
+  pImpl = new solver_impl;
+  _init_(sz_);
+}
+
+polynomial::solver::~solver() {
+  _reset_();
+}
+
+void polynomial::solver::_init_(size_t sz_) {
+  pImpl->_status_ = GSL_EFAILED;
+  if (pImpl->_ws_ != 0) {
+    if (pImpl->_sz_ >= sz_) {
+      return;
+    } else {
+      _reset_();
+    }
+  }
+  if (sz_ > 1) {
+    pImpl->_ws_ = gsl_poly_complex_workspace_alloc(sz_);
+    pImpl->_sz_ = sz_;
+  }
+}
+
+void polynomial::solver::_reset_() {
+  if (pImpl->_ws_ != 0) {
+    gsl_poly_complex_workspace_free(pImpl->_ws_);
+    pImpl->_ws_ = 0;
+    pImpl->_sz_ = 0;
+  }
+  pImpl->_status_ = GSL_EFAILED;
+}
+
+bool polynomial::solver::solve(const polynomial& p_) {
+  size_t deg = p_.get_degree();
+  _init_(deg + 1);
+  {
+    throw logic_error("polynomial::solver::solve:  Not implemented yet!");
+  }
+  return pImpl->_status_ == GSL_SUCCESS;
+}
 
 }
 
-/* Local Variables: */
-/* mode: c++        */
-/* coding: utf-8    */
-/* End:             */
-
-// end of mygsl::polynomial.cc
