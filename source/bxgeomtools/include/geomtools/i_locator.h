53bfd1383f4099879b1b7848f9d79aab7c806652
Xavier Garrido <garrido@lal.in2p3.fr>
2013-05-29 17:44:20 +0000

    prefer forward declaration to header inclusion

--- source/bxgeomtools/include/geomtools/i_locator.h
+++ source/bxgeomtools/include/geomtools/i_locator.h
@@ -1,41 +1,42 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 /* i_locator.h
  * Author (s):     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2010-02-09
  * Last modified: 2011-04-13
- * 
- * License: 
- * 
- * Description: 
+ *
+ * License:
+ *
+ * Description:
  *   Geometry locator abstract interface
- * 
- * History: 
- * 
+ *
+ * History:
+ *
  */
 
 #ifndef GEOMTOOLS_I_LOCATOR_H_
 #define GEOMTOOLS_I_LOCATOR_H_ 1
 
-#include <geomtools/utils.h>
+#include <geomtools/i_object_3d.h>
 #include <geomtools/geom_id.h>
-#include <geomtools/geom_info.h>
 
 namespace geomtools {
+
+  class geom_info;
 
   /**! An abstract interface for simple locators that build the geometry ID
    *   associated to some position for some geometry type.
    */
   class i_base_locator
   {
-    virtual bool find_geom_id (const vector_3d & a_position, 
+    virtual bool find_geom_id (const vector_3d & a_position,
                                int a_type,
-                               geom_id & a_gid,  
+                               geom_id & a_gid,
                                double a_tolerance = i_object_3d::USING_PROPER_TOLERANCE) const = 0;
-    
+
   };
 
   /**! An abstract interface for locators based on some internal lookup table.
-   *   Its methods return some reference to some stored geoemtrical meta data.
+   *   Its methods return some reference to some stored geometrical meta data.
    */
   class i_locator
   {
@@ -51,11 +52,11 @@ namespace geomtools {
     virtual const geom_info & get_geom_info (const geom_id & a_id) const = 0;
 
 
-    /** Given a position and the type of some geometry volume, 
+    /** Given a position and the type of some geometry volume,
      *  returns the associated geom_id instance (from some internal table).
      */
-    virtual const geom_id & get_geom_id (const vector_3d & a_position, 
-                                         int a_type = geom_id::INVALID_TYPE, 
+    virtual const geom_id & get_geom_id (const vector_3d & a_position,
+                                         int a_type = geom_id::INVALID_TYPE,
                                          double a_tolerance = i_object_3d::USING_PROPER_TOLERANCE) const = 0;
 
   };
