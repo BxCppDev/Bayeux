46cfa87f383691e8cc78d46c6516f22d44d4f56d
Xavier Garrido <garrido@lal.in2p3.fr>
2013-06-07 16:10:37 +0000

    removing global use of std namespace

--- source/bxgeomtools/src/i_model.cc
+++ source/bxgeomtools/src/i_model.cc
@@ -16,14 +16,10 @@
 
 namespace geomtools {
 
-  using namespace std;
-
   DATATOOLS_FACTORY_SYSTEM_REGISTER_IMPLEMENTATION (i_model, "geomtools::i_model/__system__");
 
-
   // static
-  const i_model::constants &
-  i_model::constants::instance ()
+  const i_model::constants & i_model::constants::instance ()
   {
     static boost::scoped_ptr<i_model::constants> g_global_constants (0);
     if ( g_global_constants.get () == 0) {
@@ -34,29 +30,30 @@ namespace geomtools {
 
   i_model::constants::constants ()
   {
-    SOLID_SUFFIX = ".solid";
-    LOGICAL_SUFFIX = ".log";
-    PHYSICAL_SUFFIX = ".phys";
+    SOLID_SUFFIX       = ".solid";
+    LOGICAL_SUFFIX     = ".log";
+    PHYSICAL_SUFFIX    = ".phys";
     PHANTOM_SOLID_FLAG = "phantom_solid";
     return;
   }
 
-  datatools::logger::priority i_model::get_logging_priority() const
+  datatools::logger::priority i_model::get_logging_priority () const
   {
     return _logging_priority;
   }
 
-  void i_model::set_logging_priority(datatools::logger::priority p_)
+  void i_model::set_logging_priority (datatools::logger::priority p_)
   {
     _logging_priority = p_;
+    return;
   }
 
 
-  void i_model::assert_constructed (const string & where_,
-                                    const string & what_) const
+  void i_model::assert_constructed (const std::string & where_,
+                                    const std::string & what_) const
   {
     if (is_constructed ()) return;
-    ostringstream message;
+    std::ostringstream message;
     if (! where_.empty ()) {
       message << where_ << ": ";
     }
@@ -66,14 +63,14 @@ namespace geomtools {
       message << "Operation not allowed ! Model has not been constructed yet";
     }
     message << " !";
-    DT_THROW_IF(true, logic_error, message.str ());
+    DT_THROW_IF(true, std::logic_error, message.str ());
   }
 
-  void i_model::assert_unconstructed (const string & where_,
-                                      const string & what_) const
+  void i_model::assert_unconstructed (const std::string & where_,
+                                      const std::string & what_) const
   {
     if (! is_constructed ()) return;
-    ostringstream message;
+    std::ostringstream message;
     if (! where_.empty ()) {
       message << where_ << ": ";
     }
@@ -83,73 +80,68 @@ namespace geomtools {
       message << "Operation not allowed ! Model has already been constructed";
     }
     message << " !";
-    DT_THROW_IF(true, logic_error, message.str ());
+    DT_THROW_IF(true, std::logic_error, message.str ());
   }
 
-  string i_model::make_solid_name (const string & basename_)
+  std::string i_model::make_solid_name (const std::string & basename_)
   {
-    ostringstream oss;
-    oss << basename_ << i_model::constants::instance().SOLID_SUFFIX;
+    return basename_ + i_model::constants::instance().SOLID_SUFFIX;
   }
 
-  string i_model::make_logical_volume_name (const string & basename_)
+  std::string i_model::make_logical_volume_name (const std::string & basename_)
   {
-    ostringstream oss;
-    oss << basename_ << i_model::constants::instance().LOGICAL_SUFFIX;
-    return oss.str ();
+    return basename_ + i_model::constants::instance().LOGICAL_SUFFIX;
   }
 
-  string i_model::make_physical_volume_name (const string & basename_)
+  std::string i_model::make_physical_volume_name (const std::string & basename_)
   {
-    ostringstream oss;
-    oss << basename_ << i_model::constants::instance().PHYSICAL_SUFFIX;
-    return oss.str ();
+    return basename_ + i_model::constants::instance().PHYSICAL_SUFFIX;
   }
 
-  string i_model::make_physical_volume_name (const string & basename_,
-                                             int nitems_)
+  std::string i_model::make_physical_volume_name (const std::string & basename_,
+                                                  int nitems_)
   {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << basename_ << i_model::constants::instance().PHYSICAL_SUFFIX
         << "." << "__" << 0 << ".." << (nitems_ - 1) << "__";
     return oss.str ();
   }
 
-  string i_model::make_physical_volume_name_per_item (const string & basename_,
-                                                      int item_)
+  std::string i_model::make_physical_volume_name_per_item (const std::string & basename_,
+                                                           int item_)
   {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << basename_ << i_model::constants::instance().PHYSICAL_SUFFIX
         << "." << "__" << item_ << "__";
     return oss.str ();
   }
 
-  string i_model::make_physical_volume_name_per_item (const string & basename_,
-                                                      int i_, int j_)
+  std::string i_model::make_physical_volume_name_per_item (const std::string & basename_,
+                                                           int i_, int j_)
   {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << basename_ << i_model::constants::instance().PHYSICAL_SUFFIX
         << "." << "__" << i_ << "__"
         << "__" << j_ << "__";
     return oss.str ();
   }
 
-  string i_model::make_physical_volume_name (const string & basename_,
-                                             int ncols_,
-                                             int nrows_)
+  std::string i_model::make_physical_volume_name (const std::string & basename_,
+                                                  int ncols_,
+                                                  int nrows_)
   {
-    ostringstream oss;
+    std::ostringstream oss;
     oss << basename_ << i_model::constants::instance().PHYSICAL_SUFFIX
         << "." << "__" << 0 << ".." << (ncols_ - 1) << "__"
         << "__" << 0 << ".." << (nrows_ - 1) << "__";
     return oss.str ();
   }
 
-  string i_model::extract_label_from_physical_volume_name (const string & physical_volume_name_)
+  std::string i_model::extract_label_from_physical_volume_name (const std::string & physical_volume_name_)
   {
-    size_t pos = physical_volume_name_.rfind (i_model::constants::instance().PHYSICAL_SUFFIX);
+    const size_t pos = physical_volume_name_.rfind (i_model::constants::instance().PHYSICAL_SUFFIX);
     DT_THROW_IF(pos == physical_volume_name_.npos,
-                logic_error,
+                std::logic_error,
                 "Do not recognize a physical volume name from '"
                 << physical_volume_name_ << "' !");
     return physical_volume_name_.substr (0, pos);
@@ -175,12 +167,12 @@ namespace geomtools {
     return;
   }
 
-  const string & i_model::get_name () const
+  const std::string & i_model::get_name () const
   {
     return _name_;
   }
 
-  void i_model::set_name (const string & name_)
+  void i_model::set_name (const std::string & name_)
   {
     _name_ = name_;
     return;
@@ -197,25 +189,23 @@ namespace geomtools {
     return;
   }
 
-  const datatools::properties &
-  i_model::parameters () const
+  const datatools::properties & i_model::parameters () const
   {
     return _parameters_;
   }
 
-  datatools::properties &
-  i_model::parameters ()
+  datatools::properties & i_model::parameters ()
   {
     return _parameters_;
   }
 
   // ctor:
-  i_model::i_model (const string & name_)
+  i_model::i_model (const std::string & name_)
   {
-    _constructed_ = false;
+    _constructed_     = false;
     _logging_priority = datatools::logger::PRIO_WARNING;
+    _phantom_solid    = false;
     set_name (name_);
-    _phantom_solid = false;
     return;
   }
 
@@ -241,24 +231,17 @@ namespace geomtools {
     return this->i_model::grab_logical ();
   }
 
-  void i_model::_at_construct (const string & name_,
+  void i_model::_at_construct (const std::string & name_,
                                const datatools::properties & setup_,
                                models_col_type * models_)
   {
-    DT_THROW_IF(true,runtime_error, "This method MUST be overloaded !");
+    DT_THROW_IF (true, std::logic_error, "This method MUST be overloaded !");
     return;
   }
 
   void i_model::_pre_construct (datatools::properties & setup_)
   {
-    if (setup_.has_key("logging.priority")) {
-      std::string lp_label = setup_.fetch_string("logging.priority");
-      datatools::logger::priority lp = datatools::logger::get_priority(lp_label);
-      DT_THROW_IF(lp ==  datatools::logger::PRIO_UNDEFINED,
-                  std::logic_error,
-                  "Invalid logging priority '" << lp_label << "' !");
-      set_logging_priority(lp);
-    }
+    set_logging_priority (datatools::logger::extract_logging_configuration (setup_));
 
     if (setup_.has_flag (i_model::constants::instance().PHANTOM_SOLID_FLAG)) {
       _set_phantom_solid (true);
@@ -278,14 +261,14 @@ namespace geomtools {
     return;
   }
 
-  void i_model::construct (const string & name_,
+  void i_model::construct (const std::string & name_,
                            const datatools::properties & setup_,
                            models_col_type * models_)
   {
     bool devel_track_name = false;
 
-    DT_THROW_IF (_constructed_,
-                 logic_error,
+    DT_THROW_IF (is_constructed (),
+                 std::logic_error,
                  "Model '" << name_ << "' has been already constructed !");
     datatools::properties & setup = const_cast<datatools::properties &> (setup_);
     _pre_construct (setup);
@@ -295,9 +278,9 @@ namespace geomtools {
     return;
   }
 
-  void i_model::tree_dump (ostream & out_,
-                           const string & title_,
-                           const string & indent_,
+  void i_model::tree_dump (std::ostream & out_,
+                           const std::string & title_,
+                           const std::string & indent_,
                            bool inherit_) const
   {
     std::string indent;
@@ -324,7 +307,7 @@ namespace geomtools {
            << "Parameters  : ";
       out_ << std::endl;
       {
-        ostringstream indent_oss;
+        std::ostringstream indent_oss;
         indent_oss << indent;
         indent_oss << datatools::i_tree_dumpable::skip_tag;
         _parameters_.tree_dump (out_,"",indent_oss.str ());
@@ -334,7 +317,7 @@ namespace geomtools {
     {
       out_ << indent << datatools::i_tree_dumpable::inherit_tag (inherit_)
            << "Logical  : " << std::endl;
-      ostringstream indent_oss;
+      std::ostringstream indent_oss;
       indent_oss << indent;
       indent_oss << datatools::i_tree_dumpable::inherit_skip_tag (inherit_);
       _logical.tree_dump (out_, "", indent_oss.str ());
