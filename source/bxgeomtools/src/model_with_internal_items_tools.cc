7277f0d6ccd402bb733dc64c893c0af418779430
Xavier Garrido <garrido@lal.in2p3.fr>
2013-06-10 21:31:55 +0000

    exception handler and logger

--- source/bxgeomtools/src/model_with_internal_items_tools.cc
+++ source/bxgeomtools/src/model_with_internal_items_tools.cc
@@ -1,4 +1,4 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 /* model_with_internal_items_tools.cc
  */
 
@@ -20,21 +20,19 @@
 
 namespace geomtools {
 
-  using namespace std;  
-
   model_with_internal_items_tools::item_type::item_type ()
   {
     _model_ = 0;
     return;
   }
 
-  void model_with_internal_items_tools::item_type::set_label (const string & label_)
+  void model_with_internal_items_tools::item_type::set_label (const std::string & label_)
   {
     _label_ = label_;
     return;
   }
 
-  const string & model_with_internal_items_tools::item_type::get_label () const
+  const std::string & model_with_internal_items_tools::item_type::get_label () const
   {
     return _label_;
   }
@@ -47,13 +45,7 @@ namespace geomtools {
 
   const i_model & model_with_internal_items_tools::item_type::get_model () const
   {
-    if (_model_ == 0)
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::item_type::get_model: "
-                << "Missing model !";
-        throw logic_error (message.str ());
-      }
+    DT_THROW_IF (_model_ == 0, std::logic_error, "Missing model !");
     return *_model_;
   }
 
@@ -63,92 +55,51 @@ namespace geomtools {
     return;
   }
 
-  const placement & 
-  model_with_internal_items_tools::item_type::get_placement () const
+  const placement & model_with_internal_items_tools::item_type::get_placement () const
   {
     return _placement_;
   }
 
-  const physical_volume & 
-  model_with_internal_items_tools::item_type::get_physical_volume () const
+  const physical_volume & model_with_internal_items_tools::item_type::get_physical_volume () const
   {
     return _phys_;
   }
 
-  physical_volume & 
-  model_with_internal_items_tools::item_type::get_physical_volume ()
+  physical_volume & model_with_internal_items_tools::item_type::get_physical_volume ()
   {
     return _phys_;
   }
 
   /************************************************/
-    
-  bool model_with_internal_items_tools::has_item (const string & label_) const
+
+  bool model_with_internal_items_tools::has_item (const std::string & label_) const
   {
-    if (label_.length () == 0)
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::has_item: "
-                << "Empty label !"
-                << endl;
-        throw logic_error (message.str ());
-      }
-    return _items_.find (label_) != _items_.end ();      
+    DT_THROW_IF (label_.length () == 0, std::logic_error, "Empty label !");
+    return _items_.find (label_) != _items_.end ();
   }
-    
-  const model_with_internal_items_tools::item_type & 
-  model_with_internal_items_tools::get_item (const string & label_) const
+
+  const model_with_internal_items_tools::item_type &
+  model_with_internal_items_tools::get_item (const std::string & label_) const
   {
     item_dict_type::const_iterator found = _items_.find (label_);
-    if (found == _items_.end ())
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::get_item: "
-                << "No item with label '" << label_ << "'"
-                << endl;
-        throw logic_error (message.str ());
-      }
+    DT_THROW_IF (found == _items_.end (), std::logic_error, "No item with label '" << label_ << "'");
     return found->second;
   }
 
-  model_with_internal_items_tools::item_type & 
-  model_with_internal_items_tools::get_item (const string & label_)
+  model_with_internal_items_tools::item_type &
+  model_with_internal_items_tools::get_item (const std::string & label_)
   {
     item_dict_type::iterator found = _items_.find (label_);
-    if (found == _items_.end ())
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::get_item: "
-                << "No item with label '" << label_ << "' !"
-                << endl;
-        throw logic_error (message.str ());
-      }
+    DT_THROW_IF (found == _items_.end (), std::logic_error, "No item with label '" << label_ << "'");
     return found->second;
   }
 
-  void model_with_internal_items_tools::add_item (const string & label_, 
+  void model_with_internal_items_tools::add_item (const std::string & label_,
                                                   const i_model & model_,
                                                   const placement & placement_)
   {
-    /*
-    for (item_dict_t::const_iterator i = _items_.begin ();
-         i != _items_.end ();
-         i++)
-      {
-        clog << "DEVEL: model_with_internal_items_tools::add_item: "
-             << "Label = '" << i->first << "'"
-             << endl;
-      }
-    */   
-    if (has_item (label_))
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::add_item: "
-                << "There is already an item with label '" << label_ << "' !"
-                << endl;
-        throw logic_error (message.str ());
-      }
-      
+    DT_THROW_IF (has_item (label_), std::logic_error,
+                 "There is already an item with label '" << label_ << "' !");
     {
       item_type dummy;
       _items_[label_] = dummy;
@@ -165,7 +116,7 @@ namespace geomtools {
     return _items_.size ();
   }
 
-  const model_with_internal_items_tools::item_dict_type & 
+  const model_with_internal_items_tools::item_dict_type &
   model_with_internal_items_tools::get_items () const
   {
     return _items_;
@@ -175,13 +126,13 @@ namespace geomtools {
   {
     return _items_;
   }
- 
+
   // ctor:
   model_with_internal_items_tools::model_with_internal_items_tools ()
   {
     return;
   }
-  
+
   // dtor:
   model_with_internal_items_tools::~model_with_internal_items_tools ()
   {
@@ -193,16 +144,7 @@ namespace geomtools {
                                                               logical_volume & log_,
                                                               models_col_type * models_)
   {
-    bool devel = false;
-    double lunit = CLHEP::mm;
-    vector<string> labels;
- 
-    if (config_.has_key ("length_unit"))
-      {
-        string length_unit_str = config_.fetch_string ("length_unit");
-        lunit = datatools::units::get_length_unit_from (length_unit_str);
-      }
-
+    std::vector<std::string> labels;
     if (config_.has_key ("internal_item.labels"))
       {
         config_.fetch ("internal_item.labels", labels);
@@ -214,38 +156,22 @@ namespace geomtools {
         return;
       }
 
-    if (! models_)
-      {
-        ostringstream message;
-        message << "geomtools::model_with_internal_items_tools::_at_construct: "
-                << "Missing dictionary of models !"; 
-        throw logic_error (message.str ());
-      }
+    DT_THROW_IF (! models_, std::logic_error, "Missing dictionary of models !");
 
-    for (vector<string>::const_iterator i = labels.begin ();
+    for (std::vector<std::string>::const_iterator i = labels.begin ();
          i != labels.end ();
          i++)
       {
-        string item_label = *i;
-        string item_model_name;
-        const i_model * item_model = 0;
+        const std::string item_label = *i;
         placement item_placement;
         // extract placement:
         {
-          string item_placement_str;
-          ostringstream item_placement_oss;
+          std::ostringstream item_placement_oss;
           item_placement_oss << "internal_item.placement." << item_label;
-          if (config_.has_key (item_placement_oss.str ()))
-            {
-              item_placement_str = config_.fetch_string (item_placement_oss.str ());
-            }
-          else
-            {
-              ostringstream message;
-              message << "geomtools::model_with_internal_items_tools::_at_construct: "
-                      << "Missing '" << item_placement_oss.str () << "' property !"; 
-              throw logic_error (message.str ());
-            }
+          DT_THROW_IF (! config_.has_key (item_placement_oss.str ()),
+                       std::logic_error,
+                       "Missing '" << item_placement_oss.str () << "' property !");
+          const std::string item_placement_str = config_.fetch_string (item_placement_oss.str ());
           // parse placement:
           bool res_parsing = true;
           try
@@ -256,49 +182,25 @@ namespace geomtools {
             {
               res_parsing = false;
             }
-          if (! res_parsing)
-            {
-              ostringstream message;
-              message << "geomtools::model_with_internal_items_tools::_at_construct: "
-                      << "Item's placement parsing failed !"; 
-              throw logic_error (message.str ());
-            }
+          DT_THROW_IF (! res_parsing, std::logic_error, "Item's placement parsing failed !");
         }
 
         // extract model:
+        const i_model * item_model = 0;
         {
-          ostringstream item_model_key_oss;
+          std::ostringstream item_model_key_oss;
           item_model_key_oss << "internal_item.model." << item_label;
-          if (config_.has_key (item_model_key_oss.str ()))
-            {
-              item_model_name = config_.fetch_string (item_model_key_oss.str ());
-            }
-          else
-            {
-              ostringstream message;
-              message << "geomtools::model_with_internal_items_tools::_at_construct: "
-                      << "Missing '" << item_model_key_oss.str () << "' property !"; 
-              throw logic_error (message.str ());
-            }
-          {
-            models_col_type::const_iterator found = 
-              models_->find (item_model_name);
-            if (found != models_->end ())
-              {
-                item_model = found->second;
-              }
-            else
-              {
-                ostringstream message;
-                message << "geomtools::model_with_internal_items_tools::_at_construct: "
-                        << "Cannot find model with name '" 
-                        << item_model_name << "' !";
-                throw logic_error (message.str ());
-              }
-          }
+          DT_THROW_IF (! config_.has_key (item_model_key_oss.str ()),
+                       std::logic_error,
+                       "Missing '" << item_model_key_oss.str () << "' property !");
+          const std::string item_model_name = config_.fetch_string (item_model_key_oss.str ());
+          models_col_type::const_iterator found = models_->find (item_model_name);
+          DT_THROW_IF (found == models_->end (),
+                       std::logic_error,
+                       "Cannot find model with name '" << item_model_name << "' !");
+          item_model = found->second;
         }
 
-        //clog << "DEVEL: adding internal item : " << item_label << endl;
         add_item (item_label, *item_model, item_placement);
         physical_volume & item_phys = get_item (item_label).get_physical_volume ();
         const placement & item_plcmt = get_item (item_label).get_placement ();
