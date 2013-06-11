5b21a2431fe4d8cb1b7ffffd7880cfe3bec43a43
Xavier Garrido <garrido@lal.in2p3.fr>
2013-06-11 08:33:22 +0000

    remove devel flag

--- source/bxgeomtools/src/the_serializable.cc
+++ source/bxgeomtools/src/the_serializable.cc
@@ -1,6 +1,6 @@
-// -*- mode: c++; -*-  
-/* the_serializable.cc  
- */ 
+// -*- mode: c++; -*-
+/* the_serializable.cc
+ */
 
 #include <cstdlib>
 #include <iostream>
@@ -18,27 +18,13 @@ namespace geomtools {
   namespace detail {
     namespace serialization {
 
-      bool dynamic_link_guard::_g_devel_ = false;
-
       dynamic_link_guard::dynamic_link_guard ()
       {
-        if (getenv ("GEOMTOOLS_SERIALIZATION_DEVEL") != NULL)
-          {
-            dynamic_link_guard::_g_devel_ = true;
-          }
-        if (dynamic_link_guard::_g_devel_)
-          {
-            std::clog << "geomtools::detail::serialization::dynamic_link_guard::CTOR: Ok." << std::endl;
-          }
         return;
       }
 
       dynamic_link_guard::~dynamic_link_guard ()
       {
-        if (dynamic_link_guard::_g_devel_)
-          {
-            std::clog << "geomtools::detail::serialization::dynamic_link_guard::DTOR: Ok." << std::endl;
-          }
         return;
       }
 
@@ -49,15 +35,11 @@ namespace geomtools {
           {
             g_global_guard.reset (new dynamic_link_guard);
           }
-        if (dynamic_link_guard::_g_devel_)
-          {
-            std::clog << "geomtools::detail::serialization::dynamic_link_guard::instance: Ok." << std::endl;
-          }
         return *g_global_guard.get ();
       }
 
     } // end namespace serialization
-  } // end namespace detail 
+  } // end namespace detail
 } // end namespace geomtools
 
 // end of the_serializable.cc
