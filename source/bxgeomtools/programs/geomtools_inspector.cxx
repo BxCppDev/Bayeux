ca4b072d83dcdee3d2972b8261b90aae99e85bb4
Xavier Garrido <garrido@lal.in2p3.fr>
2013-06-18 08:46:02 +0000

    print help message before exception to clearly show what's going wrong

--- source/bxgeomtools/programs/geomtools_inspector.cxx
+++ source/bxgeomtools/programs/geomtools_inspector.cxx
@@ -506,8 +506,8 @@ int main (int argc_, char ** argv_)
     } // end of interactive
   }
   catch (std::exception & x) {
-    DT_LOG_FATAL(datatools::logger::PRIO_FATAL, "geomtools_inspector: " << x.what ());
     print_help ();
+    DT_LOG_FATAL(datatools::logger::PRIO_FATAL, "geomtools_inspector: " << x.what ());
     error_code = EXIT_FAILURE;
   }
   catch (...) {
