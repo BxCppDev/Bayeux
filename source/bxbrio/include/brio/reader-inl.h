de80cf5b26e4a2cb24079212815020da4d5c61a3
Ben Morgan <ben.morgan@warwick.ac.uk>
2013-08-16 13:09:44 +0000

    Move template methods to -inl headers
    Create dedicated -inl headers for reader and writer headers, moving
    template method definitions from these into the corresponding -inl 
    header. Add #includes of the -inl headers at bottom of reader and
    writer headers.
    
    Update CMake scripts to use and install new headers.

---
+++ source/bxbrio/include/brio/reader-inl.h
@@ -0,0 +1,166 @@
+//! \file  reader-inl.h
+//! \brief Definitions of brio::reader template functions
+//
+// Copyright (c) 2013 by Ben Morgan <bmorgan.warwick@gmail.com>
+// Copyright (c) 2013 by The University of Warwick
+//
+// This file is part of brio.
+//
+// brio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// brio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with brio.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef BRIO_READER_INL_HH
+#define BRIO_READER_INL_HH
+
+namespace brio {
+template<typename T>
+int reader::load_next(T& data_, const std::string& label_) {
+  int64_t entry = this->get_current_entry(label_);
+  return this->load<T>(data_, label_, entry + 1);
+}
+
+template<typename T>
+int reader::load_previous(T& data_, const std::string& label_) {
+  int64_t entry = this->get_current_entry(label_);
+  return this->load<T>(data_, label_, entry - 1);
+}
+
+template<typename T>
+int reader::load(T& data_, int64_t nentry_) {
+  return this->load<T>(data_, "", nentry_);
+}
+
+template<typename T>
+int reader::load(T& data_, const std::string& label_, int64_t nentry_) {
+  DT_THROW_IF(!this->is_opened(),
+              std::logic_error,
+              "Operation prohibited; file is not opened !");
+
+  store_info *ptr_si = this->_get_store_info(label_);
+  if (!ptr_si) {
+    DT_THROW_IF(!label_.empty (),
+                std::logic_error,
+                "No source store with label '" << label_ << "' !");
+    // if we do not allow automatic store, this is a critical error:
+    DT_THROW_IF(!_allow_automatic_store_,
+                std::logic_error,
+                "No source store is selected nor default store is available !");
+    ptr_si = _automatic_store_;
+  }
+  return this->_at_load<T>(data_, ptr_si, nentry_);
+}
+
+template<class T>
+int reader::_at_load(T& data_, store_info *ptr_si_, int64_t nentry_) {
+  DT_LOG_TRACE(this->get_logging_priority(),"Entering...");
+  store_info& si = *ptr_si_;
+
+  if (_check_serial_tag_) {
+    // We check if the serialization tag from the store matches the 
+    // data's one:
+    if (si.has_dedicated_serialization_tag()) {
+      // 2013-02-19 FM : change the way we check the serial tag :
+      //if (data_.get_serial_tag () != si.get_serialization_tag ())
+      DT_THROW_IF(!datatools::check_serial_tag<T>(si.get_serialization_tag ()),
+                  std::logic_error,
+                  "Data serialization tag '" << data_.get_serial_tag()
+                  << "' does not match source store's serialization tag '" << si.get_serialization_tag () << "' !");
+    }
+  }
+
+  DT_THROW_IF(si.number_of_entries == 0,
+              std::logic_error,
+              "Source store '" << si.label << "' has no entry !");
+  int64_t nentry = nentry_;
+  if (nentry >= 0) {
+    // check overflow:
+    DT_THROW_IF(nentry_ >= si.number_of_entries,
+                std::logic_error,
+                "Source store '" << si.label << "' has "
+                << "no serialized entry at index '" << nentry_ << "' !");
+  } else {
+    // if nentry_ < 0: use entry index relative to the current entry 
+    // position
+    if (si.current_entry < 0) {// at rewind position
+      // start with first entry:
+      nentry = 0;
+    } else {
+      // try next entry:
+      nentry = si.current_entry + 1;
+    }
+  }
+
+  // read this tree entry in the ROOT I/O system:
+  si.record.reset();
+  int ret = si.tree->GetEntry(nentry, 1); // -> 1 for all branches
+  if (!ret) {
+    DT_THROW_IF(true,
+                std::logic_error,
+                "No entry '"
+                << nentry << "' at entry # " << nentry
+                << " in source store labelled '" << si.label.c_str()
+                << "' from  file '" << _filename << "' !");
+  } else if (ret == -1) {
+    DT_THROW_IF(true,
+                std::logic_error,
+                "An I/O error occurs from entry '"
+                << nentry 
+                << "' in source store labelled '" << si.label.c_str()
+                << "' from  file '" << _filename << "' !");
+  } else {
+    si.current_entry = nentry;
+  }
+
+  if (_check_serial_tag_) {
+    /* We may be confused with stores without dedicated serialization tag.
+     * Here we test if data and the entry's serial tags match:
+     */
+    if (!si.has_dedicated_serialization_tag()) {
+      // check serial tag associated to the buffered binary archive:
+      std::string serial_tag = si.record.fSerialTag.Data();
+      // 2013-02-19 FM : change the way we check
+      // if (data_.get_serial_tag () != serial_tag)
+      DT_THROW_IF(!datatools::check_serial_tag<T>(serial_tag),
+                  std::logic_error,
+                  "Entry '" << nentry 
+                  << "' with serial tag '" << serial_tag 
+                  << "' in (mixed) source store labelled '" << si.label.c_str()
+
+                  << "' from  file '" << _filename 
+                  << "' does not match the requested '"
+                  << data_.get_serial_tag() << "' data type !");
+    }
+  }
+
+  // Deserialize from the archive:
+  boost::iostreams::stream<boost::iostreams::array_source> 
+      input_stream(si.record.fDataBuffer.fArray,si.record.fDataBuffer.fN);
+  // 2011-06-16 FM: restored
+  if (this->is_format_pba()) {
+    //boost::archive::portable_binary_iarchive ia (input_stream);
+    eos::portable_iarchive ia(input_stream);
+    ia >> data_;
+  }
+  if (this->is_format_text()) {
+    input_stream.imbue (*_locale);
+    boost::archive::text_iarchive ia (input_stream);
+    ia >> data_;
+  }
+
+  _current_store = &si;
+  DT_LOG_TRACE(this->get_logging_priority(),"Exiting.");
+  return 0;
+}
+} // namespace brio
+#endif // BRIO_READER_INL_HH
+
--- source/bxbrio/include/brio/writer.h
+++ source/bxbrio/include/brio/writer.h
@@ -42,13 +42,26 @@ namespace brio {
 //! \brief The brio generic writer class
 class writer : public detail::base_io {
  public:
+  //! Default constructor
+  writer();
+
+  //! Constructor
+  writer(const std::string& a_filename,
+         datatools::logger::priority p_ = datatools::logger::PRIO_FATAL);
+
+  //! Constructor
+  writer(const std::string& a_filename,
+         const std::string& a_format_str,
+         datatools::logger::priority p_ = datatools::logger::PRIO_FATAL);
+
+  //! Destructor
+  virtual ~writer();
+
   //! Lock the writer
   void lock();
 
   //! Unlock the writer
   void unlock();
-
-  // getters:
 
   //! Check if the writer is locked
   bool is_locked() const;
@@ -80,31 +93,6 @@ class writer : public detail::base_io {
   //! Set the protection against file overwriting
   void set_existing_file_protected(bool a_new_value = true);
 
- public:
-  //! Default constructor
-  writer();
-
-  //! Constructor
-  writer(const std::string& a_filename,
-         datatools::logger::priority p_ = datatools::logger::PRIO_FATAL);
-
-  //! Constructor
-  writer(const std::string& a_filename,
-         const std::string& a_format_str,
-         datatools::logger::priority p_ = datatools::logger::PRIO_FATAL);
-
-  //! Destructor
-  virtual ~writer();
-
-  //! Smart print
-  virtual void tree_dump(std::ostream& a_out = std::clog,
-                         const std::string& a_title = "",
-                         const std::string& a_indent = "",
-                         bool a_inherit = false) const;
-
-  //! Print
-  void print_info(std::ostream& a_out = std::clog) const;
-
   /** Add a new store with label 'label_'
    *  to store objects with a dedicated serialization tag 'serial_tag_'
    */
@@ -124,126 +112,18 @@ class writer : public detail::base_io {
   int add_mixed_store(const std::string& a_label,
                       size_t a_buffer_size = store_info::DEFAULT_STORE_BUFFER_SIZE);
 
-
- public:
   // Store template method
   template <typename T>
-  int store(const T& a_data, const std::string& a_label = "") {
-    DT_THROW_IF(!this->is_opened(),
-                std::logic_error,
-                "Operation prohibited; file is not opened !");
-    store_info *ptr_si = this->_get_store_info(a_label);
-    if (!ptr_si) {
-      if (a_label.empty()) {
-        // if we do not allow automatic store, this is a critical error:
-        DT_THROW_IF(!_allow_automatic_store_,
-                    std::logic_error,
-                    "No target store is selected nor target !");
-        ptr_si = this->_add_store(store_info::AUTOMATIC_STORE_LABEL,
-                                  a_data.get_serial_tag(),
-                                  store_info::DEFAULT_STORE_BUFFER_SIZE);
-      } else {
-        ptr_si = this->_add_store(a_label, a_data.get_serial_tag(),
-                                  store_info::DEFAULT_STORE_BUFFER_SIZE);
-      }
-    }
-    // Final check:
-    DT_THROW_IF(ptr_si == 0,
-                std::logic_error,
-                "Could not determine any store to save data !");
-    DT_LOG_DEBUG(this->get_logging_priority(),
-                 "ptr_si = " << std::hex << ptr_si << std::dec);
-    DT_LOG_DEBUG(this->get_logging_priority(),
-                 "Using store with label '" << ptr_si->label << "'...");
-    if (this->get_logging_priority() >= datatools::logger::PRIO_DEBUG) {
-      ptr_si->tree->Print ();
-    }
-    return this->_at_store<T>(a_data, ptr_si);
-  }
+  int store(const T& a_data, const std::string& a_label = "");
 
- protected:
-  template <typename T>
-  int _at_store (const T& a_data, store_info *a_store_info) {
-    DT_LOG_TRACE(this->get_logging_priority(),"Entering...");
-    store_info *ptr_si = a_store_info;
-    // First serialized object sets the serialization tag for this store:
-    if (ptr_si->serialization_tag == 
-        store_info::POSTPONED_DEDICATED_SERIAL_TAG_LABEL) {
-      ptr_si->serialization_tag = a_data.get_serial_tag();
-    }
-    // Else if the store has a dedicated serialization tag:
-    else if (ptr_si->has_dedicated_serialization_tag()) {
-      // Check the data serialization tag matches that requested by the store:
-      DT_THROW_IF(a_data.get_serial_tag() != ptr_si->get_serialization_tag(),
-                  std::logic_error,
-                  "Serialization tag mismatch ! "
-                  << "Attempt to store an object with `" 
-                  << a_data.get_serial_tag ()
-                  << "' serialization tag "
-                  << "in the store labelled '" 
-                  << ptr_si->label 
-                  << "' with dedicated `"
-                  << ptr_si->get_serialization_tag () 
-                  << "' serialization tag !");
-    }
+  //! Smart print
+  virtual void tree_dump(std::ostream& a_out = std::clog,
+                         const std::string& a_title = "",
+                         const std::string& a_indent = "",
+                         bool a_inherit = false) const;
 
-    // Prepare the (std::vector<char>) buffer to host the binary archive 
-    // as a sequence of bytes:
-    typedef std::vector<char> buffer_type;
-
-    /* Clear the buffer of characters for streaming but
-    * intrinsic capacity is kept as is (this is a behaviour of vector
-    * class from the STL library):
-    */
-    if (!ptr_si->buffer.empty()) {
-      ptr_si->buffer.clear();
-    } else if (ptr_si->buffer.capacity() == 0) {
-    // Ensure minimum starting capacity of the buffer of characters for 
-    // streaming in order to optimize possible memory reallocation:
-      ptr_si->buffer.reserve(store_info::DEFAULT_STREAM_BUFFER_SIZE);
-    }
-
-    // Archiving is redirected to the buffer:
-    namespace io = boost::iostreams;
-    io::stream<io::back_insert_device<buffer_type> > output_stream(ptr_si->buffer);
-    // 2011-06-16 FM: restored
-    if (this->is_format_pba()) {
-      //boost::archive::portable_binary_oarchive oa (output_stream);
-      eos::portable_oarchive oa(output_stream);
-      oa << a_data;
-    }
-    if (is_format_text()) {
-      output_stream.imbue(*_locale);
-      boost::archive::text_oarchive oa(output_stream);
-      oa << a_data;
-    }
-    output_stream.flush();
-
-    // Now the buffer contains the final sequence of bytes corresponding to
-    // the serialized output binary archive:
-    DT_LOG_DEBUG(this->get_logging_priority(),
-                 "Buffer size = "
-                 << ptr_si->buffer.size()
-                 << "   buffer capacity = "
-                 << ptr_si->buffer.capacity());
-    // Prepare the container interface to be streamed using the ROOT I/O system:
-    ptr_si->record.fSerialTag  = a_data.get_serial_tag().c_str();
-    ptr_si->record.fVersionTag = 0; // not used
-    ptr_si->record.fDataBuffer.fN     = ptr_si->buffer.size();
-    ptr_si->record.fDataBuffer.fArray =  &(ptr_si->buffer[0]);
-    // Store this tree entry in the ROOT I/O system:
-    ptr_si->tree->Fill ();
-    // Reset the ROOT data buffer:
-    ptr_si->record.fDataBuffer.fN = 0;
-    ptr_si->record.fDataBuffer.fArray = 0;
-    // Update some counters:
-    ptr_si->number_of_entries = ptr_si->tree->GetEntries();
-    ptr_si->current_entry = ptr_si->number_of_entries - 1;
-    // Update the 'current store' pointer:
-    _current_store = ptr_si;
-    DT_LOG_TRACE(this->get_logging_priority(),"Exiting.");
-    return 0;
-  }
+  //! Print
+  void print_info(std::ostream& a_out = std::clog) const;
 
  protected:
   void _set_default();
@@ -252,6 +132,9 @@ class writer : public detail::base_io {
                          const std::string& a_serial_tag,
                          size_t a_buffer_size);
 
+  template <typename T>
+  int _at_store(const T& a_data, store_info *a_store_info);
+ 
   virtual void _at_open(const std::string& a_filename);
 
  private:
@@ -266,6 +149,8 @@ class writer : public detail::base_io {
   store_info *_automatic_store_;     /// A handle to the automatic store (if any)
 };
 } // end of namespace brio
+
+#include <brio/writer-inl.h>
 
 #endif // BRIO_WRITER_H_
 
