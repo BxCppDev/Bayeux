1f8e1a170bc81f484432268d987959a2075dbfa9
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-08-23 16:17:24 +0000

    extract the mataterials_driver class from the materials_inspector program

---
+++ source/bxmaterials/include/materials/materials_driver.h
@@ -0,0 +1,95 @@
+// -*- mode: c++ ; -*-
+/* materials_driver.h
+ * Author (s) : François Mauger <mauger@lpccaen.in2p3.fr>
+ * Creation date: 2013-08-23
+ * Last modified: 2013-08-23
+ *
+ * Description:  Isotopes/elements/materials application driver.
+ */
+
+#ifndef MATERIALS_MATERIALS_DRIVER_H_
+#define MATERIALS_MATERIALS_DRIVER_H_ 1
+
+// Standard Library
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Datatools
+#include <datatools/logger.h>
+
+// Third Party
+#include <boost/scoped_ptr.hpp>
+
+namespace materials {
+
+  class manager;
+
+  struct materials_driver_params {
+  public:
+
+    enum action_flag_type {
+      ACTION_UNDEFINED      = -1,
+      ACTION_LIST_ISOTOPES  =  0x1,
+      ACTION_LIST_ELEMENTS  =  0x2,
+      ACTION_LIST_MATERIALS =  0x4,
+      ACTION_LIST_ALIASES   =  0x8,
+      ACTION_SHOW_ISOTOPE   =  0x10,
+      ACTION_SHOW_ELEMENT   =  0x20,
+      ACTION_SHOW_MATERIAL  =  0x40,
+    };
+
+    materials_driver_params();
+
+    virtual ~materials_driver_params();
+
+    void reset();
+
+  public:
+
+    int         action;
+    bool        with_decoration;
+    std::vector<std::string> action_options;
+    std::string show_target;
+    std::vector<std::string> LL_dlls;
+    std::string LL_config;
+    std::string MaterialsMgrConfigFile;
+    datatools::logger::priority logging;
+
+  };
+
+  class materials_driver
+  {
+  public:
+
+    /// Constructor
+    materials_driver();
+
+    /// Destructor
+    virtual ~materials_driver();
+
+    /// Test initialization status
+    bool is_initialized() const;
+
+    /// Initialization
+    void initialize(const materials_driver_params & mdp_);
+
+    /// reset
+    void reset();
+
+    /// Main run method
+    int run_action(std::ostream & out_ = std::cout,
+		   std::ostream & log_ = std::clog);
+
+  private:
+
+    bool _initialized_; /// Initialization flag
+    materials_driver_params _params_; /// Configuration parameters
+    boost::scoped_ptr<manager> _mgr_; /// Embeded manager
+  };
+
+} // end of namespace materials
+
+#endif // MATERIALS_MATERIALS_DRIVER_H_
+
+// end of materials_driver.h
--- source/bxmaterials/programs/materials_inspector.cxx
+++ source/bxmaterials/programs/materials_inspector.cxx
@@ -1,44 +1,27 @@
 // -*- mode: c++ ; -*-
 // materials_inspector.cxx
 
+// Standard Library
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <exception>
 
-#include <boost/foreach.hpp>
+// Datatools
+#include <datatools/logger.h>
+#include <datatools/exception.h>
 
-#include <datatools/properties.h>
-#include <datatools/clhep_units.h>
-#include <datatools/library_loader.h>
-
-#include <materials/materials_config.h>
-#include <materials/manager.h>
+// Materials
+#include <materials/materials_driver.h>
 
 void print_help(std::ostream & out_);
 
-enum action_flag_type {
-  ACTION_UNDEFINED      = -1,
-  ACTION_LIST_ISOTOPES  =  0x1,
-  ACTION_LIST_ELEMENTS  =  0x2,
-  ACTION_LIST_MATERIALS =  0x4,
-  ACTION_LIST_ALIASES   =  0x8,
-  ACTION_SHOW_ISOTOPE   =  0x10,
-  ACTION_SHOW_ELEMENT   =  0x20,
-  ACTION_SHOW_MATERIAL  =  0x40,
-};
-
 int main (int argc_, char ** argv_)
 {
-  datatools::logger::priority logging = datatools::logger::PRIO_FATAL;
   int error_code = EXIT_SUCCESS;
+  datatools::logger::priority logging = datatools::logger::PRIO_FATAL;
+  materials::materials_driver_params MDP;
   try {
-    int         nshoots = 0;
-    int         action = 0;
-    std::string show_target;
-    std::vector<std::string> LL_dlls;
-    std::string LL_config;
-    std::string MaterialsMgrConfigFile;
 
     int iarg = 1;
     while (iarg < argc_) {
@@ -49,9 +32,9 @@ int main (int argc_, char ** argv_)
           print_help(std::cout);
           return 0;
         } else if (option == "--load-dll") {
-          LL_dlls.push_back (argv_[++iarg]);
+          MDP.LL_dlls.push_back (argv_[++iarg]);
         } else if (option == "--dll-config") {
-          LL_config = argv_[++iarg];
+          MDP.LL_config = argv_[++iarg];
         } else if ((option == "-L") || (option == "--logging-priority")) {
           std::string lp_name = argv_[++iarg];
           logging = datatools::logger::get_priority(lp_name);
@@ -59,31 +42,35 @@ int main (int argc_, char ** argv_)
                       std::logic_error, "Undefined logging priority '"
                       << lp_name << "' !");
         } else if ((option == "-c") || (option == "--manager-config")) {
-          MaterialsMgrConfigFile = argv_[++iarg];
+          MDP.MaterialsMgrConfigFile = argv_[++iarg];
         } else if ((option == "-I") || (option == "--list-isotopes")) {
-          action |= ACTION_LIST_ISOTOPES;
+          MDP.action |= materials::materials_driver_params::ACTION_LIST_ISOTOPES;
         } else if ((option == "-E") || (option == "--list-elements")) {
-          action |= ACTION_LIST_ELEMENTS;
+          MDP.action |= materials::materials_driver_params::ACTION_LIST_ELEMENTS;
         } else if ((option == "-M") || (option == "--list-materials")) {
-          action |= ACTION_LIST_MATERIALS;
+          MDP.action |= materials::materials_driver_params::ACTION_LIST_MATERIALS;
         } else if ((option == "-A") || (option == "--list-aliases")) {
-          action |= ACTION_LIST_ALIASES;
+          MDP.action |= materials::materials_driver_params::ACTION_LIST_ALIASES;
         } else if ((option == "-i") || (option == "--show-isotope")) {
-          action |= ACTION_SHOW_ISOTOPE;
-          show_target = argv_[++iarg];
+          MDP.action |= materials::materials_driver_params::ACTION_SHOW_ISOTOPE;
+          MDP.show_target = argv_[++iarg];
         } else if ((option == "-e") || (option == "--show-element")) {
-          action |= ACTION_SHOW_ELEMENT;
-          show_target = argv_[++iarg];
+          MDP.action |= materials::materials_driver_params::ACTION_SHOW_ELEMENT;
+          MDP.show_target = argv_[++iarg];
         } else if ((option == "-m") || (option == "--show-material")) {
-          action |= ACTION_SHOW_MATERIAL;
-          show_target = argv_[++iarg];
+          MDP.action |= materials::materials_driver_params::ACTION_SHOW_MATERIAL;
+          MDP.show_target = argv_[++iarg];
+        } else if (option == "--with-decoration") {
+          MDP.with_decoration = true;
+        } else if (option == "--without-decoration") {
+          MDP.with_decoration = false;
         } else {
-          DT_LOG_WARNING(logging, "Ignoring option '" << option << "' !");
+          MDP.action_options.push_back(option);
         }
       } else {
         std::string argument = token;
-        if (MaterialsMgrConfigFile.empty()) {
-          MaterialsMgrConfigFile = argument;
+        if (MDP.MaterialsMgrConfigFile.empty()) {
+          MDP.MaterialsMgrConfigFile = argument;
         } else {
           DT_LOG_WARNING(logging, "Ignoring argument '" << argument << "' !");
         }
@@ -91,159 +78,11 @@ int main (int argc_, char ** argv_)
       iarg++;
     }
 
-    uint32_t LL_flags = datatools::library_loader::allow_unregistered;
-    datatools::library_loader LL(LL_flags, LL_config);
-    BOOST_FOREACH (const std::string & dll_name, LL_dlls) {
-      DT_LOG_NOTICE(logging,"Loading DLL '" << dll_name << "'...");
-      DT_THROW_IF (LL.load(dll_name) != EXIT_SUCCESS,
-                   std::runtime_error,
-                   "Loading DLL '" << dll_name << "' failed !");
-    }
-
-    if (action == 0) {
-      DT_LOG_NOTICE(logging, "Perform default action : list materials...");
-      action = ACTION_LIST_MATERIALS;
-    }
-
-    // Configuration files :
-    DT_THROW_IF(MaterialsMgrConfigFile.empty(),
-                std::logic_error,
-                "Missing materials manager configuration file !");
-
-    // Materials manager :
-    materials::manager MatMgr;
-    datatools::fetch_path_with_env(MaterialsMgrConfigFile);
-    datatools::properties MaterialsMgrConfig;
-    datatools::properties::read_config (MaterialsMgrConfigFile, MaterialsMgrConfig);
-    MatMgr.initialize(MaterialsMgrConfig);
-    if (logging >= datatools::logger::PRIO_DEBUG) {
-      DT_LOG_DEBUG(datatools::logger::PRIO_DEBUG, "The materials manager : ");
-      MatMgr.tree_dump(std::clog);
-    }
-
-    if (action & ACTION_LIST_ISOTOPES) {
-      std::cerr << "List of isotopes: " << std::endl;
-      const materials::isotope_dict_type & isotopes = MatMgr.get_isotopes ();
-      for (materials::isotope_dict_type::const_iterator i
-             = isotopes.begin();
-           i != isotopes.end();
-           i++) {
-        materials::isotope_dict_type::const_iterator j = i;
-        j++;
-        if (j == isotopes.end()) {
-          std::cerr << "`-- ";
-        } else {
-          std::cerr << "|-- ";
-        }
-        std::cerr << "'" << std::flush;
-        std::cout << i->first;
-        std::cerr << "'" << std::flush;
-        std::cout << std::endl;
-      }
-    }
-
-    if (action & ACTION_LIST_ELEMENTS) {
-      std::cerr << "List of elements: " << std::endl;
-      const materials::element_dict_type & elements = MatMgr.get_elements ();
-      for (materials::element_dict_type::const_iterator i
-             = elements.begin();
-           i != elements.end();
-           i++) {
-        materials::element_dict_type::const_iterator j = i;
-        j++;
-        if (j == elements.end()) {
-          std::cerr << "`-- ";
-        } else {
-          std::cerr << "|-- ";
-        }
-        std::cerr << "'" << std::flush;
-        std::cout << i->first;
-        std::cerr << "'" << std::flush;
-        std::cout << std::endl;
-      }
-    }
-
-    if (action & ACTION_LIST_MATERIALS) {
-      std::cerr << "List of materials: " << std::endl;
-      const materials::material_dict_type & materials = MatMgr.get_materials ();
-      for (materials::material_dict_type::const_iterator i
-             = materials.begin();
-           i != materials.end();
-           i++) {
-        materials::material_dict_type::const_iterator j = i;
-        j++;
-        if (j == materials.end()) {
-          std::cerr << "`-- ";
-        } else {
-          std::cerr << "|-- ";
-        }
-        std::cerr << std::flush;
-        std::cerr << "'" << std::flush;
-        std::cout << i->first << std::flush;
-        std::cerr << "'" << std::flush;
-        if (i->second.is_alias()) {
-          std::cerr << " alias of '" << i->second.get_alias_of() << "' " << std::flush;;
-        }
-        std::cout << std::endl;
-      }
-    }
-
-    if (action & ACTION_LIST_ALIASES) {
-      std::map<std::string,std::string> aliases;
-      std::cerr << "List of material aliases: " << std::endl;
-      const materials::material_dict_type & materials = MatMgr.get_materials ();
-      for (materials::material_dict_type::const_iterator i
-             = materials.begin();
-           i != materials.end();
-           i++) {
-        if (i->second.is_alias()) {
-          aliases[i->first] = i->second.get_alias_of();
-        }
-      }
-
-      for (std::map<std::string,std::string>::const_iterator i
-             = aliases.begin();
-           i != aliases.end();
-           i++) {
-        std::map<std::string,std::string>::const_iterator j = i;
-        j++;
-        if (j == aliases.end()) {
-          std::cerr << "`-- ";
-        } else {
-          std::cerr << "|-- ";
-        }
-        std::cerr << std::flush;
-        std::cerr << "'" << std::flush;
-        std::cout << i->first << std::flush;
-        std::cerr << "'" << std::flush;
-        std::cerr << " alias of '" << i->second << "' " << std::flush;;
-        std::cout << std::endl;
-      }
-    }
-
-    if (action & ACTION_SHOW_ISOTOPE) {
-      const materials::isotope_dict_type & isotopes = MatMgr.get_isotopes ();
-      materials::isotope_dict_type::const_iterator found = isotopes.find(show_target);
-      DT_THROW_IF (found == isotopes.end(), std::logic_error,
-                   "Unknown isotope '" << show_target << "' !");
-      found->second.get_ref().tree_dump(std::cout, "Isotope: ");
-    }
-
-    if (action & ACTION_SHOW_ELEMENT) {
-      const materials::element_dict_type & elements = MatMgr.get_elements ();
-      materials::element_dict_type::const_iterator found = elements.find(show_target);
-      DT_THROW_IF (found == elements.end(), std::logic_error,
-                   "Unknown element '" << show_target << "' !");
-      found->second.get_ref().tree_dump(std::cout, "Element: ");
-    }
-
-    if (action & ACTION_SHOW_MATERIAL) {
-      const materials::material_dict_type & materials = MatMgr.get_materials ();
-      materials::material_dict_type::const_iterator found = materials.find(show_target);
-      DT_THROW_IF (found == materials.end(), std::logic_error,
-                   "Unknown material '" << show_target << "' !");
-      found->second.get_ref().tree_dump(std::cout, "Material: ");
-    }
+    MDP.logging = logging;
+    materials::materials_driver MD;
+    MD.initialize(MDP);
+    MD.run_action(std::cout, std::clog);
+    MD.reset();
 
   }
   catch (std::exception & x) {
@@ -259,9 +98,7 @@ int main (int argc_, char ** argv_)
 
 void print_help(std::ostream & out)
 {
-  out << "Inspect vertices from a vertex generator managed                            \n";
-  out << "by a vertex generator manager and coupled to a                              \n";
-  out << "geometry manager.                                                           \n";
+  out << "Inspect materials from a isotopes/elements/materials manager.               \n";
   out << "                                                                            \n";
   out << "Usage::                                                                     \n";
   out << "                                                                            \n";
@@ -287,6 +124,10 @@ void print_help(std::ostream & out)
   out << "     list the available materials from the materials manager.               \n";
   out << "  -A, --list-aliases :                                                      \n";
   out << "     list the available materials aliases from the materials manager.       \n";
+  out << "  --with-decoration : print fancy headers, titles and decoration            \n";
+  out << "                      (only for -I, -E, -M and -A)                          \n";
+  out << "  --without-decoration : do not print fancy headers, titles and decoration  \n";
+  out << "                         (only for -I, -E, -M and -A)                       \n";
   out << "  -i, --show-isotope ISONAME  : show isotope ISONAME.                       \n";
   out << "  -e, --show-element ELENAME  : show element ELENAME.                       \n";
   out << "  -m, --show-material MATNAME : show material MATNAME.                      \n";
