05081356681d6f19e49f458121ab006bc4f31941
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-09-14 19:15:19 +0000

    Refactor API version queries into dedicated class

--- source/bxmygsl/testing/test_mygsl.cxx
+++ source/bxmygsl/testing/test_mygsl.cxx
@@ -1,12 +1,13 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 // test_mygsl.cxx
- 
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <exception>
 
 #include <mygsl/mygsl_config.h>
+#include <mygsl/version.h>
 
 using namespace std;
 
@@ -15,8 +16,8 @@ int main (int argc_, char ** argv_)
   int error_code = EXIT_SUCCESS;
   try
     {
-      clog << "Test program for the 'mygsl' program library." << endl; 
-  
+      clog << "Test program for the 'mygsl' program library." << endl;
+
       bool debug = false;
       bool verbose = false;
 
@@ -25,44 +26,44 @@ int main (int argc_, char ** argv_)
         {
           string token = argv_[iarg];
 
-          if (token[0] == '-') 
+          if (token[0] == '-')
             {
-               string option = token; 
-               if ((option == "-d") || (option == "--debug")) 
+               string option = token;
+               if ((option == "-d") || (option == "--debug"))
                  {
                    debug = true;
                  }
-               else if ((option == "-v") || (option == "--verbose")) 
+               else if ((option == "-v") || (option == "--verbose"))
                  {
                    verbose = true;
                  }
-	       else 
-                 { 
-                    clog << "warning: ignoring option '" << option << "'!" << endl; 
+               else
+                 {
+                    clog << "warning: ignoring option '" << option << "'!" << endl;
                  }
             }
           else
             {
-              string argument = token; 
-              { 
-                clog << "warning: ignoring argument '" << argument << "'!" << endl; 
+              string argument = token;
+              {
+                clog << "warning: ignoring argument '" << argument << "'!" << endl;
               }
-            } 
-          iarg++; 
+            }
+          iarg++;
       }
 
       clog << "mygsl version : " << MYGSL_LIB_VERSION << endl;
-  
+
       clog << "The end." << endl;
     }
   catch (exception & x)
-    { 
-      cerr << "error: " << x.what () << endl; 
+    {
+      cerr << "error: " << x.what () << endl;
       error_code = EXIT_FAILURE;
     }
   catch (...)
     {
-      cerr << "error: " << "unexpected error !" << endl; 
+      cerr << "error: " << "unexpected error !" << endl;
       error_code = EXIT_FAILURE;
     }
   return (error_code);
