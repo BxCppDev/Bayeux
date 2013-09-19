b3c79e5ae6efe3af543ffc23b2cbfa9e3b7969c0
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-09-19 01:09:45 +0000

    minor mod in test_datatools.cxx

--- source/bxdatatools/testing/test_datatools.cxx
+++ source/bxdatatools/testing/test_datatools.cxx
@@ -1,8 +1,9 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 // test_datatools.cxx
 
 // - Ourselves
 #include <datatools/datatools_config.h>
+#include <datatools/version.h>
 
 #include <cstdlib>
 #include <iostream>
@@ -12,15 +13,16 @@
 
 int main (int argc_, char ** argv_) {
   try {
+    std::cerr << "Datatools version is: " << datatools::version::get_version() << std::endl;
   }
   catch (std::exception & x)
-  { 
-    std::cerr << "error: " << x.what () << std::endl; 
+  {
+    std::cerr << "error: " << x.what () << std::endl;
     return EXIT_FAILURE;
   }
   catch (...)
   {
-    std::cerr << "error: " << "unexpected error !" << std::endl; 
+    std::cerr << "error: " << "unexpected error !" << std::endl;
     return EXIT_FAILURE;
   }
 
