c22db9f358f2c5f868ee51b664e2c64f603c6edd
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-09-19 01:10:42 +0000

    add bxexample01 example

---
+++ examples/bxexample01/bxexample01.cxx
@@ -0,0 +1,503 @@
+//! \file  bxexample01.cxx
+//! \brief A sample example program for Bayeux's libraries
+//
+// Copyright (c) 2013 by François Mauger <mauger@lpccaen.in2p3.fr>
+// Copyright (c) 2013 by Université de Caen Basse-Normandie
+//
+// This file is part of Bayeux.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Standard libraries
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+// Third Party
+// - Boost
+#include <boost/scoped_ptr.hpp>
+#include <boost/filesystem.hpp>
+
+struct params_type {
+  bool interactive;
+  params_type () : interactive(false) {}
+};
+
+params_type & params()
+{
+  static boost::scoped_ptr<params_type > _params;
+  if (! _params) {
+    _params.reset(new params_type );
+  }
+  return *_params.get();
+}
+
+void title(std::string t_) {
+  std::cout << std::endl;
+  int n = t_.length();
+  for (int i = 0; i < n+4; i++) {
+    std::cout << '*';
+  }
+  std::cout << std::endl;
+  std::cout << "* " << t_ << " *\n";
+  for (int i = 0; i < n+4; i++) {
+    std::cout << '*';
+  }
+  std::cout << std::endl;
+  return;
+}
+
+// - Bayeux
+#include <bayeux/version.h>
+// - Bayeux/datatools
+#include <datatools/library_info.h>
+
+int example_bayeux()
+{
+  title("bayeux");
+
+  if (datatools::library_info::has("datatools")) {
+    datatools::library_info::print("bayeux", std::clog);
+  }
+
+  std::clog << "Bayeux version is: " << bayeux::version::get_version() << std::endl;
+
+  return 0;
+}
+
+
+// - Bayeux/datatools
+#include <datatools/datatools_config.h>
+#include <datatools/version.h>
+#include <datatools/properties.h>
+
+int example_datatools()
+{
+  title("datatools");
+
+  if (datatools::library_info::has("datatools")) {
+    datatools::library_info::print("datatools", std::clog);
+  }
+  std::clog << "datatools version is: " << datatools::version::get_version() << std::endl;
+
+  {
+    std::clog << "datatools::properties object manipulation example: " << std::endl;
+    datatools::properties config;
+    config.store_flag("test");
+    config.store("library.name", "datatools", "The datatools library name", true);
+    config.store("library.version", datatools::version::get_version(),
+                 "The datatools library version", true);
+    config.store("debug.level", 3, "The debug level");
+    config.tree_dump(std::clog, "Some configuration parameters:");
+    std::string filename = "bxexample01_datatools.conf";
+    config.write_configuration(filename);
+    std::clog << "datatools configuration example file is: '" << filename << "'." << std::endl;
+  }
+
+  {
+    std::string filename="@datatools:foo/bar.data";
+    std::clog << "datatools dummy file path is: '"
+              << filename << "'." << std::endl;
+    try {
+      datatools::fetch_path_with_env(filename);
+      std::clog << "datatools expanded data file path is: '"
+                << filename << "'." << std::endl;
+    } catch (std::exception & error) {
+      DT_LOG_NOTICE(datatools::logger::PRIO_NOTICE,
+                    "As expected, we cannot parse this file path directive; reason is: "
+                    << error.what());
+    }
+  }
+
+  return 0;
+}
+
+
+// - Bayeux/cuts
+#include <cuts/version.h>
+
+int example_cuts()
+{
+  title("cuts");
+
+  if (datatools::library_info::has("cuts")) {
+    datatools::library_info::print("cuts", std::clog);
+  }
+
+  std::clog << "cuts version is: " << cuts::version::get_version() << std::endl;
+
+  return 0;
+}
+
+
+// - Bayeux/materials
+#include <materials/version.h>
+#include <materials/resource.h>
+
+int example_materials()
+{
+  title("materials");
+
+  if (datatools::library_info::has("materials")) {
+    datatools::library_info::print("materials", std::clog);
+  }
+
+  std::clog << "materials version is   : "
+            << materials::version::get_version() << std::endl;
+  std::clog << "materials resource dir : "
+            << materials::get_resource_dir() << std::endl;
+
+  {
+    std::string filename="@materials:data/simple_elements.def";
+    std::clog << "materials data file path is: '"
+              << filename << "'." << std::endl;
+    datatools::fetch_path_with_env(filename);
+    std::clog << "materials expanded data file path is: '"
+              << filename << "'." << std::endl;
+  }
+
+  {
+    // Force env. var. :
+    std::string env_name = "MATERIALS_DATA_DIR";
+    std::string env_value = "/tmp/materials/alt.resources";
+    if (! getenv(env_name.c_str())) {
+      setenv(env_name.c_str(), env_value.c_str(), 1);
+    }
+    std::string filename="${MATERIALS_DATA_DIR}/data/simple_elements.def";
+    std::clog << "materials data file alt. path is: '"
+              << filename << "'." << std::endl;
+    datatools::fetch_path_with_env(filename);
+    std::clog << "materials expanded data file alt. path is: '"
+              << filename << "'." << std::endl;
+  }
+
+  return 0;
+}
+
+
+// - Bayeux/mygsl
+#include <mygsl/version.h>
+#include <mygsl/histogram_1d.h>
+
+int example_mygsl()
+{
+  title("mygsl");
+
+  if (datatools::library_info::has("mygsl")) {
+    datatools::library_info::print("mygsl", std::clog);
+  }
+  std::clog << "mygsl version is: "
+            << mygsl::version::get_version() << std::endl;
+
+
+  {
+    std::clog << "mygsl::histogram object manipulation example: " << std::endl;
+    mygsl::histogram h(15, -1.0, 2.0, mygsl::BIN_MODE_LINEAR);
+    h.grab_auxiliaries().store("display.title", "A dummy histogram");
+    h.grab_auxiliaries().store("display.x.label", "Energy");
+    h.grab_auxiliaries().store("display.x.unit", "MeV");
+    h.grab_auxiliaries().store("display.y.label", "dN/E");
+    h.grab_auxiliaries().store("display.y.unit", "Count/0.2MeV");
+    for (int i = 0; i < 1000; i++) {
+      h.fill(drand48());
+    }
+    h.tree_dump(std::clog, "A 1D-histogram:");
+    h.print_ascii(std::cout);
+    h.reset();
+  }
+
+
+  return 0;
+}
+
+
+// - Bayeux/geomtools
+#include <geomtools/version.h>
+#include <geomtools/box.h>
+#include <geomtools/logical_volume.h>
+#include <geomtools/gnuplot_drawer.h>
+
+int example_geomtools()
+{
+  title("geomtools");
+
+  if (datatools::library_info::has("geomtools")) {
+    datatools::library_info::print("geomtools", std::clog);
+  }
+  std::clog << "geomtools version is: "
+            << geomtools::version::get_version() << std::endl;
+
+  {
+    std::clog << "geomtools::box object manipulation example: " << std::endl;
+    geomtools::box b(3.0*CLHEP::cm, 2.0*CLHEP::cm, 0.5*CLHEP::cm);
+    b.tree_dump(std::clog, "A 3D-box:");
+    std::clog << "Surface is: "
+              << b.get_surface()/CLHEP::mm2 << " mm2" << std::endl;
+    std::clog << "Top surface is: "
+              << b.get_surface(geomtools::box::FACE_TOP)/CLHEP::mm2 << " mm2" << std::endl;
+    std::clog << "Volume is: "
+              << b.get_volume()/CLHEP::mm3 << " mm3" << std::endl;
+    geomtools::logical_volume box_log("Box", b);
+    box_log.set_material_ref("Inox");
+    box_log.grab_parameters().store("material.mass_activity.Bi214",
+                                    1.33e-3 * CLHEP::becquerel/CLHEP::kg);
+    box_log.grab_parameters().set_explicit_unit("material.mass_activity.Bi214",
+                                                true);
+    box_log.grab_parameters().store("visibility.color", "red");
+    box_log.tree_dump(std::clog, "Logical associated to the 3D-box:");
+    geomtools::gnuplot_drawer drawer;
+    geomtools::placement box_loc;
+    if (params().interactive) drawer.draw (box_log, box_loc, 100, "Display a 3D-box");
+
+    b.reset();
+  }
+
+  return 0;
+}
+
+
+// - Bayeux/emfield
+#include <emfield/version.h>
+#include <emfield/uniform_magnetic_field.h>
+
+int example_emfield()
+{
+  title("emfield");
+
+  if (datatools::library_info::has("emfield")) {
+    datatools::library_info::print("emfield", std::clog);
+  }
+  std::clog << "emfield version is: "
+            << emfield::version::get_version() << std::endl;
+
+  {
+    std::clog << "emfield::uniform_magnetic_field object manipulation example: "
+              << std::endl;
+    emfield::uniform_magnetic_field b;
+    b.set_uniform_magnetic_field(geomtools::vector_3d(0.0*CLHEP::gauss,
+                                                      0.0*CLHEP::gauss,
+                                                      25.0*CLHEP::gauss));
+    b.initialize_simple();
+    b.tree_dump(std::clog, "Uniform magnetic field:");
+    const geomtools::vector_3d pos(12.0*CLHEP::cm,
+                                   8.4*CLHEP::cm,
+                                   -5.0*CLHEP::mm);
+    double time = 2.34 * CLHEP::s;
+    geomtools::vector_3d bvec;
+    b.compute_magnetic_field(pos, time, bvec);
+    std::clog << "Magnetic field amplitude is: "
+              << bvec.mag()/CLHEP::gauss << " T" << std::endl;
+    b.reset();
+  }
+
+  return 0;
+}
+
+
+// - Bayeux/genbb_help
+#include <genbb_help/version.h>
+#include <genbb_help/wdecay0.h>
+#include <genbb_help/primary_event.h>
+
+int example_genbb_help()
+{
+  title("genbb_help");
+
+  if (datatools::library_info::has("genbb_help")) {
+    datatools::library_info::print("genbb_help", std::clog);
+  }
+  std::clog << "genbb_help version is: "
+            << genbb::version::get_version() << std::endl;
+
+  {
+    std::clog << "genbb::wdecay0 object manipulation example: "
+              << std::endl;
+    datatools::properties config;
+    config.store("seed", 314159);
+    config.store("decay_type", "DBD");
+    config.store("decay_isotope", "Se82");
+    config.store("decay_dbd_level", 0);
+    config.store("decay_dbd_mode", 4);
+    config.store("energy_min", 0.0);
+    config.store("energy_max", 3.2);
+    config.store("energy_unit", "MeV");
+    config.tree_dump(std::clog, "wdecay0 configuration: ");
+
+    genbb::wdecay0 wd0;
+    wd0.initialize_standalone(config);
+    for (int i =0; i < 4; i++) {
+      genbb::primary_event pe;
+      wd0.load_next(pe);
+      pe.tree_dump(std::clog, "Generated primary event: ");
+    }
+    wd0.reset();
+  }
+  return 0;
+}
+
+
+// - Bayeux/genvtx
+#include <genvtx/version.h>
+#include <genvtx/box_vg.h>
+#include <mygsl/rng.h>
+
+int example_genvtx()
+{
+  title("genvtx");
+
+  if (datatools::library_info::has("genvtx")) {
+    datatools::library_info::print("genvtx", std::clog);
+  }
+  std::clog << "genvtx version is: "
+            << genvtx::version::get_version() << std::endl;
+
+  {
+    std::clog << "genvtx::box_vg object manipulation example: "
+              << std::endl;
+    geomtools::box b(3.0*CLHEP::cm, 2.0*CLHEP::cm, 1.0*CLHEP::cm);
+    b.tree_dump(std::clog, "A 3D-box:");
+
+    genvtx::box_vg bvg;
+    bvg.set_box(b);
+    bvg.set_bulk(0.0*CLHEP::cm);
+    bvg.set_skin_skip(-1*CLHEP::mm);
+    bvg.initialize_simple();
+    bvg.tree_dump(std::clog, "A vertex generator for 3D-box:");
+
+    mygsl::rng ran("taus", 314159);
+    for (int i =0; i < 4; i++) {
+     geomtools::vector_3d vertex;
+     bvg.shoot_vertex(ran, vertex);
+     std::clog << "Generated primary vertex: " << vertex/CLHEP::mm << std::endl;
+    }
+    bvg.reset();
+
+  }
+  return 0;
+}
+
+
+// - Bayeux/trackfit
+#include <trackfit/version.h>
+
+int example_trackfit()
+{
+  title("trackfit");
+
+  if (datatools::library_info::has("trackfit")) {
+    datatools::library_info::print("trackfit", std::clog);
+  }
+
+  std::clog << "trackfit version is: " << trackfit::version::get_version() << std::endl;
+  return 0;
+}
+
+
+// - Bayeux/brio
+#include <brio/version.h>
+#include <brio/writer.h>
+#include <brio/reader.h>
+
+int example_brio()
+{
+  title("brio");
+
+  if (datatools::library_info::has("brio")) {
+    datatools::library_info::print("brio", std::clog);
+  }
+
+  std::clog << "brio version is: " << brio::version::get_version() << std::endl;
+
+  std::clog << "Starting brio serialization example..." << std::endl;
+  std::string filename = "bxexample01_brio.brio";
+  {
+    long int seed = 314159;
+    srand48(seed);
+    brio::writer out(filename);
+    for (int i = 0; i < 4; i++) {
+      datatools::properties config;
+      config.store("function", "example_brio", "The test function for brio");
+      config.store("library.name", "brio", "The brio library name");
+      config.store("library.version", brio::version::get_version(),
+                   "The brio library version");
+      config.store("random", drand48(), "A random number (float)");
+      out.store(config);
+    }
+    out.close ();
+  }
+  {
+    brio::reader in(filename);
+    while (in.has_next ()) {
+       datatools::properties config;
+       in.load_next(config);
+    }
+    in.close ();
+  }
+  std::clog << "brio serialization example file is: '" << filename << "'." << std::endl;
+
+  std::clog << "brio serialization example is terminated." << std::endl;
+  return 0;
+}
+
+
+// - Bayeux/dpp
+#include <dpp/version.h>
+
+int example_dpp()
+{
+  title("dpp");
+
+  if (datatools::library_info::has("dpp")) {
+    datatools::library_info::print("dpp", std::clog);
+  }
+
+  std::clog << "dpp version is: " << dpp::version::get_version() << std::endl;
+
+
+  return 0;
+}
+
+
+int main(int argc, const char *argv[]) {
+  try {
+    std::clog << "Welcome to Bayeux bxexample01 !" << std::endl;
+
+    std::vector<std::string> args;
+    for (int i =1; i < argc; i++) {
+      args.push_back(std::string(argv[i]));
+    }
+    if (std::find(args.begin(), args.end(), "--interactive") != args.end()) {
+      params().interactive = true;
+    }
+
+    example_bayeux();
+    example_datatools();
+    example_cuts();
+    example_materials();
+    example_mygsl();
+    example_geomtools();
+    example_emfield();
+    example_genbb_help();
+    example_genvtx();
+    example_trackfit();
+    example_brio();
+    example_dpp();
+
+    std::clog << "The end." << std::endl;
+  } catch (std::exception & error) {
+    DT_LOG_ERROR(datatools::logger::PRIO_ERROR, error.what());
+    return 1;
+  }
+  return 0;
+}
---
+++ examples/bxexample01/post_install/process.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+while [ -n "$1" ]; do
+    token="$1"
+    if [ "x${token:0:1}" = "x-" ]; then
+	option="${token}"
+	if [ "${token}" = "--cadfael-dir" ]; then
+	    shift 1
+	    export CADFAEL_INSTALL_DIR="$1"
+	elif [ "${token}" = "--bayeux-dir" ]; then
+	    shift 1
+	    export BAYEUX_INSTALL_DIR="$1"
+	else
+	    logger --stderr --priority warning "Ignoring option '${option}'!"
+	fi
+    else
+	argument="${token}"
+	logger --stderr --priority warning "Ignoring argument '${argument}'!"
+    fi
+    shift 1
+done
+
+base_dir=$(pwd)
+install_dir=${base_dir}/__install
+# Build:
+mkdir __build
+cd __build
+cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=${install_dir} \
+    -DCMAKE_FIND_ROOT_PATH:PATH=${BAYEUX_INSTALL_DIR}:${CADFAEL_INSTALL_DIR} \
+    ..
+if [ $? -ne 0 ]; then
+    echo "error: CMake configuration failed !" 1>&2
+    exit 1
+fi
+make
+if [ $? -ne 0 ]; then
+    echo "error: Build failed !" 1>&2
+    exit 1
+fi
+
+# Run the freshly built executable:
+echo "" 1>&2
+echo "======= RUN THE BUILT VERSION =======" 1>&2
+DATATOOLS_LIBINFO_TRACE=1 ./bxexample01
+if [ $? -ne 0 ]; then
+    echo "error: Run failed !" 1>&2
+    exit 1
+fi
+make install
+if [ $? -ne 0 ]; then
+    echo "error: Installation failed !" 1>&2
+    exit 1
+fi
+cd ..
+
+# Run the installed executable:
+echo "" 1>&2
+echo "======= RUN THE INSTALLED VERSION =======" 1>&2
+export PATH=${install_dir}/bin:${PATH}
+bxexample01 --interactive
+if [ $? -ne 0 ]; then
+    echo "error: Run failed !" 1>&2
+    exit 1
+fi
+
+# Cleanup
+rm -fr __install/
+rm -fr __build/
+rm -f bxexample01_brio.brio
+rm -f bxexample01_datatools.conf
+rm -f *~
+
+exit 0
