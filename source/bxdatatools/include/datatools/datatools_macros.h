d97af4413d6049975611002394967101f71d9686
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-09-25 10:03:28 +0000

    add header (unused for now)

---
+++ source/bxdatatools/include/datatools/datatools_macros.h
@@ -0,0 +1,33 @@
+// datatools_macros.h
+
+#ifndef DATATOOLS_DATATOOLS_MACROS_H_
+#define DATATOOLS_DATATOOLS_MACROS_H_
+
+// Standard library
+#include <string>
+
+// Third party
+// - Boost
+#include <boost/scoped_ptr.hpp>
+
+#define DT_STATIC_CONST_STRING_REF_DECLARATION(Name) \
+  static const std::string & Name();		     \
+  /**/
+
+// Construct On First Use Idiom
+#define DT_STATIC_CONST_STRING_REF_DEFINITION(Class,Name,Value)		\
+  const std::string & Class::Name()                                     \
+  {                                                                     \
+    DT_STATIC_CONST_STRING_IMPL(Value);					\
+  }									\
+/**/
+
+// Construct On First Use Idiom
+#define DT_STATIC_CONST_STRING_IMPL(Value)				\
+  static std::string _token = Value;					\
+  return _token;							\
+  /**/
+
+#endif // DATATOOLS_DATATOOLS_MACROS_H_
+
+// end of datatools_macros.h
