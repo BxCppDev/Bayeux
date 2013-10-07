8f88885500588ff87cfc18924542b5a33071fa58
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-10-07 13:42:07 +0000

    kozmetix

--- source/bxbrio/testing/test_brio.cxx
+++ source/bxbrio/testing/test_brio.cxx
@@ -14,58 +14,46 @@ using namespace std;
 int main (int argc_, char ** argv_)
 {
   int error_code = EXIT_SUCCESS;
-  try
-    {
-      clog << "Test program for the 'brio' program library." << endl;
+  try {
+    clog << "Test program for the 'brio' program library." << endl;
 
-      bool debug = false;
-      bool verbose = false;
+    bool debug = false;
+    bool verbose = false;
 
-      int iarg = 1;
-      while (iarg < argc_)
+    int iarg = 1;
+    while (iarg < argc_) {
+      string token = argv_[iarg];
+
+      if (token[0] == '-') {
+        string option = token;
+        if ((option == "-d") || (option == "--debug")) {
+          debug = true;
+        } else if ((option == "-v") || (option == "--verbose")) {
+          verbose = true;
+        } else {
+          clog << "warning: ignoring option '" << option << "'!" << endl;
+        }
+      } else {
+        string argument = token;
         {
-          string token = argv_[iarg];
-
-          if (token[0] == '-')
-            {
-               string option = token;
-               if ((option == "-d") || (option == "--debug"))
-                 {
-                   debug = true;
-                 }
-               else if ((option == "-v") || (option == "--verbose"))
-                 {
-                   verbose = true;
-                 }
-               else
-                 {
-                    clog << "warning: ignoring option '" << option << "'!" << endl;
-                 }
-            }
-          else
-            {
-              string argument = token;
-              {
-                clog << "warning: ignoring argument '" << argument << "'!" << endl;
-              }
-            }
-          iarg++;
+          clog << "warning: ignoring argument '" << argument << "'!" << endl;
+        }
       }
+      iarg++;
+    }
 
-      clog << "brio version : " << BRIO_LIB_VERSION << endl;
+    clog << "brio version : " << BRIO_LIB_VERSION << endl;
 
-      clog << "The end." << endl;
-    }
-  catch (exception & x)
-    {
-      cerr << "error: " << x.what () << endl;
-      error_code = EXIT_FAILURE;
-    }
-  catch (...)
-    {
-      cerr << "error: " << "unexpected error !" << endl;
-      error_code = EXIT_FAILURE;
-    }
+    clog << "The end." << endl;
+  }
+  catch (exception & x) {
+    cerr << "error: " << x.what () << endl;
+    error_code = EXIT_FAILURE;
+  }
+  catch (...) {
+    cerr << "error: " << "unexpected error !" << endl;
+    error_code = EXIT_FAILURE;
+  }
   return (error_code);
 }
 
