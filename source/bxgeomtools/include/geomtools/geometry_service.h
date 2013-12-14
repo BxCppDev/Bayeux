3b65815181aba6367f3d94136a76bcefd66d131c
Francois Mauger <mauger@lpccaen.in2p3.fr>
2013-12-14 01:28:36 +0000

    kozmetix and Doxygen-ization on geometry_service.h

--- source/bxgeomtools/include/geomtools/geometry_service.h
+++ source/bxgeomtools/include/geomtools/geometry_service.h
@@ -1,10 +1,10 @@
 /* geometry_service.h
  * Author(s)     :     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date : 2011-06-07
- * Last modified : 2013-03-09
- * 
+ * Last modified : 2013-12-14
+ *
  * Copyright (C) 2011-2013 Francois Mauger <mauger@lpccaen.in2p3.fr>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or (at
@@ -17,15 +17,15 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
- * Description: 
+ * Description:
  *
  *   Generic geometry service.
- * 
- * History: 
- * 
+ *
+ * History:
+ *
  */
 
 #ifndef GEOMTOOLS_GEOMETRY_SERVICE_H_
@@ -37,52 +37,54 @@
 // Service interface :
 #include <datatools/base_service.h>
 #include <datatools/service_tools.h>
-#include <datatools/service_macros.h>
 
 namespace datatools {
   class properties;
 }
 
 namespace geomtools {
-  
+
   class manager;
 
+  /// \brief Generic geometry service
   class geometry_service : public datatools::base_service
   {
-  public:
-    
-    const geomtools::manager & get_geom_manager () const;
- 
+
   public:
 
+    /// Check initialization flag
     virtual bool is_initialized () const;
 
+    /// Initialization
     virtual int initialize (const datatools::properties & a_config,
                             datatools::service_dict_type & a_service_dict);
 
+    /// Termination
     virtual int reset ();
-    
-  public: 
 
-    // ctor:
+    /// Return a reference to the non mutable geoemtry manager
+    const geomtools::manager & get_geom_manager () const;
+
+    /// Default constructor
     geometry_service ();
- 
-    // dtor:
+
+    /// Destructor
     virtual ~geometry_service ();
- 
-    virtual void tree_dump (std::ostream & a_out         = std::clog, 
+
+    /// Smart print
+    virtual void tree_dump (std::ostream & a_out         = std::clog,
                             const std::string & a_title  = "",
                             const std::string & a_indent = "",
                             bool a_inherit               = false) const;
 
   private:
 
-    bool                 _initialized_;
-    geomtools::manager * _geom_manager_;
+    bool                 _initialized_; //!< Initialization flag
+    geomtools::manager * _geom_manager_; //!< Handle to the geometry manager
 
     // Registration :
     DATATOOLS_SERVICE_REGISTRATION_INTERFACE (geometry_service);
-   
+
   };
 
 }  // end of namespace geomtools
