59728e48efa8d251d1a87bd796f8d6e9ed726b59
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-02-12 21:48:58 +0000

    add a method to validate seed values

--- source/bxmygsl/include/mygsl/random_utils.h
+++ source/bxmygsl/include/mygsl/random_utils.h
@@ -1,51 +1,58 @@
-// -*- mode: c++ ; -*- 
-/* random_utils.h
+// -*- mode: c++ ; -*-
+/** \file mygsl/random_utils.h
  * Author (s) :     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2010-02-16
- * Last modified: 2010-02-16
- * 
- * License: 
+ * Last modified: 2014-02-12
  *
- * Copyright 2010-2012 F. Mauger
+ * License:
+ *
+ * Copyright 2010-2014 F. Mauger
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public  License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- * 
- * Description: 
- *  A test class tat does nothing and is not used.
  *
- * History: 
- * 
+ * Description:
+ *
+ *  Some useful resources related to random number generation
+ *
+ * History:
+ *
  */
 
 #ifndef MYGSL_RANDOM_UTILS_H_
 #define MYGSL_RANDOM_UTILS_H_ 1
 
+// Third party:
+// - Boost:
 #include <boost/cstdint.hpp>
 
 namespace mygsl {
-  
+
   /// A class that hosts useful constants related to the management
   /// of pseudo-random number generators (PRNG)
   class random_utils
   {
   public:
-      
+
     static const int32_t SEED_INVALID = -1; //!< Constant that represents an invalid seed value
     static const int32_t SEED_TIME    =  0; //!< Constant that represents a seed that must be initialized from some external 'random' source like the current time, the process ID or the /dev/urandom source (or combinaison of them)
-     
+    static const int32_t SEED_MAX     =  0x7FFFFFFF; //!< Constant that represents the maximum allowed seed value
+
+    /// Check if a given seed is valid
+    static bool is_valid_seed(int32_t);
+
   };
 
 } // end of namespace mygsl
--- source/bxmygsl/src/random_utils.cc
+++ source/bxmygsl/src/random_utils.cc
@@ -1,4 +1,4 @@
-// -*- mode: c++ ; -*- 
+// -*- mode: c++ ; -*-
 /* random_utils.cc
  *
  * Copyright 2007-2011 F. Mauger
@@ -7,22 +7,31 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public  License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
  */
 
+// Ourselves:
 #include <mygsl/random_utils.h>
 
 namespace mygsl {
+
+  // static
+  bool random_utils::is_valid_seed(int32_t seed_)
+  {
+    if (seed_ < SEED_TIME) return false;
+    if (seed_ > SEED_MAX) return false;
+    return true;
+  }
 
 } // end of namespace mygsl
 
