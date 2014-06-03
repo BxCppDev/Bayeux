2e63274e811ff94ec3134cd315a8dfa08f8d250c
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-06-03 21:21:09 +0000

    fix warnings

--- source/bxemfield/testing/test_emfield.cxx
+++ source/bxemfield/testing/test_emfield.cxx
@@ -11,46 +11,44 @@
 
 using namespace std;
 
-int main (int argc_, char ** argv_)
+int main (/*int argc_, char ** argv_*/)
 {
   int error_code = EXIT_SUCCESS;
   try
     {
       clog << "Test program for the 'emfield' program library." << endl;
 
-      bool debug = false;
-      bool verbose = false;
-
-      int iarg = 1;
-      while (iarg < argc_)
-        {
-          string token = argv_[iarg];
-
-          if (token[0] == '-')
-            {
-               string option = token;
-               if ((option == "-d") || (option == "--debug"))
-                 {
-                   debug = true;
-                 }
-               else if ((option == "-v") || (option == "--verbose"))
-                 {
-                   verbose = true;
-                 }
-               else
-                 {
-                    clog << "warning: ignoring option '" << option << "'!" << endl;
-                 }
-            }
-          else
-            {
-              string argument = token;
-              {
-                clog << "warning: ignoring argument '" << argument << "'!" << endl;
-              }
-            }
-          iarg++;
-      }
+      // bool debug = false;
+      // bool verbose = false;
+      // int iarg = 1;
+      // while (iarg < argc_)
+      //   {
+      //     string token = argv_[iarg];
+      //     if (token[0] == '-')
+      //       {
+      //          string option = token;
+      //          if ((option == "-d") || (option == "--debug"))
+      //            {
+      //              debug = true;
+      //            }
+      //          else if ((option == "-v") || (option == "--verbose"))
+      //            {
+      //              verbose = true;
+      //            }
+      //          else
+      //            {
+      //               clog << "warning: ignoring option '" << option << "'!" << endl;
+      //            }
+      //       }
+      //     else
+      //       {
+      //         string argument = token;
+      //         {
+      //           clog << "warning: ignoring argument '" << argument << "'!" << endl;
+      //         }
+      //       }
+      //     iarg++;
+      // }
 
       clog << "emfield version : " << EMFIELD_LIB_VERSION << endl;
 
@@ -68,5 +66,3 @@ int main (int argc_, char ** argv_)
     }
   return (error_code);
 }
-
-// end of test_emfield.cxx
