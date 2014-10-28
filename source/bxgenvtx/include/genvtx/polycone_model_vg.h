dc11169f0b445b4118d414e99a80b0d61b4e3c4c
Xavier Garrido <garrido@lal.in2p3.fr>
2014-10-28 18:44:49 +0000

    add a polycone model
    The polycone model associates a geomtools::geom_id to a polycone shape
    and then use the elementary polycone_vg generator to shoot vertices.
    
    There is actually one limitation since the 'all_frustrum' mode must be
    enabled : no selected frustrum of the polycone can be activated since
    the polycone shape is known after the parsing of the properties
    file. The number of frustrum is thus not known at the parsing time. A
    better way to overpass this issue will be to send the setup file to the
    initialize method of polycone_vg.

---
+++ source/bxgenvtx/include/genvtx/polycone_model_vg.h
@@ -0,0 +1,168 @@
+/// \file genvtx/polycone_model_vg.h
+/* Author(S) :    Francois Mauger <mauger@lpccaen.in2p3.fr>
+ * Creation date: 2013-06-14
+ * Last modified: 2014-04-09
+ *
+ * License:
+ *
+ * Description:
+ *
+ *   Vertex generator from a tube model addressed through some
+ *   collection of geometry IDs extracted from a mapping object.
+ *
+ * History:
+ *
+ */
+
+#ifndef GENVTX_POLYCONE_MODEL_VG_H
+#define GENVTX_POLYCONE_MODEL_VG_H 1
+
+// Standard library:
+#include <string>
+
+// Third party:
+// - Bayeux/geomtools:
+#include <geomtools/id_selector.h>
+
+// This project:
+#include <genvtx/polycone_vg.h>
+#include <genvtx/vg_macros.h>
+#include <genvtx/utils.h>
+
+namespace genvtx {
+
+  /// \brief A vertex generator based on a polycone geometry model
+  class polycone_model_vg : public i_vertex_generator
+  {
+  public:
+
+    bool is_mode_valid() const;
+
+    bool is_mode_bulk() const;
+
+    bool is_mode_surface() const;
+
+    bool is_surface_inner_side() const;
+
+    bool is_surface_outer_side() const;
+
+    bool is_surface_bottom() const;
+
+    bool is_surface_top() const;
+
+    bool is_all_frustrum_active() const;
+
+    void set_surface_inner_side(bool);
+
+    void set_surface_outer_side(bool);
+
+    void set_surface_bottom(bool);
+
+    void set_surface_top(bool);
+
+    void set_skin_skip(double skin_skip_);
+
+    void set_skin_thickness(double skin_thickness_);
+
+    void set_active_all_frustrum(bool);
+
+    const std::string & get_origin_rules() const;
+
+    void set_origin_rules(const std::string &);
+
+    bool has_mapping_plugin_name() const;
+
+    const std::string & get_mapping_plugin_name() const;
+
+    void set_mapping_plugin_name(const std::string & mpn_);
+
+    bool has_materials_plugin_name() const;
+
+    const std::string & get_materials_plugin_name() const;
+
+    void set_materials_plugin_name(const std::string & mpn_);
+
+    int get_mode() const;
+
+    void set_mode(int);
+
+    virtual void tree_dump(std::ostream & out_ = std::clog,
+                           const std::string & title_ = "",
+                           const std::string & indent_ = "",
+                           bool inherit_ = false) const;
+
+    /// Constructor
+    polycone_model_vg();
+
+    /// Destructor
+    virtual ~polycone_model_vg();
+
+    /// Initialization
+    virtual void initialize(const ::datatools::properties &,
+                            ::datatools::service_manager &,
+                            ::genvtx::vg_dict_type &);
+
+    /// Reset
+    virtual void reset();
+
+    /// Check initialization status
+    virtual bool is_initialized() const;
+
+  protected :
+
+    /// Randomize vertex
+    virtual void _shoot_vertex(::mygsl::rng & random_, ::geomtools::vector_3d & vertex_);
+
+    void _shoot_vertex_polycones(mygsl::rng & random_,
+                                 geomtools::vector_3d & vertex_);
+
+  private:
+
+    void _init_();
+
+    void _reset_();
+
+    void _set_defaults_();
+
+  private:
+
+    bool                    _initialized_;           //!< Initialization flag
+    int                     _mode_;                  //!< Mode : "bulk" of "surface"
+    bool                    _surface_inner_side_;    //!< Flag for inner side surface generation mode
+    bool                    _surface_outer_side_;    //!< Flag for outer side surface generation mode
+    bool                    _surface_bottom_;        //!< Flag for bottom surface generation mode
+    bool                    _surface_top_;           //!< Flag for top surface generation mode
+    int                     _bulk_mask_;
+    genvtx::polycone_vg     _polycone_vg_;           //!< Embeded vertex generator from a polycone
+    double                  _skin_skip_;             //!< Skip (normal to the surface) to an effective position of the skin relative to the surface of the box
+    double                  _skin_thickness_;        //!< Intrinsic thickness of the surface
+    bool                    _active_all_frustrum_;   //!< Activate all polycone frustrum
+    std::string             _origin_rules_;          //!< Rules to select the physical volumes from where to generate vertexes
+    std::string             _mapping_plugin_name_;   //!< The name of the geometry 'mapping' plugin
+    std::string             _materials_plugin_name_; //!< The name of the geometry 'materials' plugin
+    geomtools::id_selector  _src_selector_;          //!< A selector of GIDs
+    std::vector<weight_entry_type> _entries_;        //!< Information about the weights
+
+    /// Registration macro
+    /// @arg polycone_model_vg the class to be registered
+    GENVTX_VG_REGISTRATION_INTERFACE(polycone_model_vg);
+
+  };
+
+} // end of namespace genvtx
+
+/***************
+ * OCD support *
+ ***************/
+#include <datatools/ocd_macros.h>
+DOCD_CLASS_DECLARATION(genvtx::polycone_model_vg)
+
+#endif // GENVTX_POLYCONE_MODEL_VG_H
+
+/*
+** Local Variables: --
+** mode: c++ --
+** c-file-style: "gnu" --
+** tab-width: 2 --
+** End: --
+*/
