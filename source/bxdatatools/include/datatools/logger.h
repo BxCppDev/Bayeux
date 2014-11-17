9e040f70d45444e2ca67fdff65260e6034cd6688
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-11-17 22:29:26 +0000

    Minor mods in the implementation of logging macros
    Class datatools::logger :
    
    - add a PRIO_ALWAYS priority level
    - remove deprecated logging functions
    - modify DT_LOG_xxx macros after Ben and Xavier ideas
      in such a way we suppress some clang compiler tautologies
      warnings. This is a questionable and not optimal solution
      but should work for the moment.
      The choice of the name of the local "_dt_xxx_p" variable should
      avoid name clash in any context.
      Another idea: use pragma(s) to mute this specific warning
      when entering these macros only (don't know yet how to do this).

--- source/bxdatatools/include/datatools/logger.h
+++ source/bxdatatools/include/datatools/logger.h
@@ -66,241 +66,162 @@
 
 namespace datatools {
 
-// Forward declarations:
-class properties;
-class object_configuration_description;
+  // Forward declarations:
+  class properties;
+  class object_configuration_description;
 
-//! Organise logging functionality under one roof
-struct logger {
-  //! Priority levels for logging from most to least critical
-  enum priority {
-    PRIO_UNDEFINED = 0,//!< Undefined/invalid priority
-    PRIO_FATAL,        //!< A fatal error. The application will most likely terminate. This is the highest priority.
-    PRIO_CRITICAL,     //!< A critical error. The application might not be able to continue running successfully.
-    PRIO_ERROR,        //!< An error. An operation did not complete successfully, but the application as a whole is not affected.
-    PRIO_WARNING,      //!< A warning. An operation completed with an unexpected result.
-    PRIO_NOTICE,       //!< A notice, which is an information with just a higher priority.
-    PRIO_INFORMATION,  //!< An informational message, usually denoting the successful completion of an operation.
-    PRIO_DEBUG,        //!< A debugging message.
-    PRIO_TRACE         //!< A tracing message. This is the lowest priority.
+  //! Organise logging functionality under one roof
+  struct logger {
+
+    //! Priority levels for logging from most to least critical
+    enum priority {
+      PRIO_ALWAYS   = -1, //!< Always print the message
+      PRIO_UNDEFINED = 0, //!< Undefined/invalid priority
+      PRIO_FATAL,         //!< A fatal error. The application will most likely terminate. This is the highest priority.
+      PRIO_CRITICAL,      //!< A critical error. The application might not be able to continue running successfully.
+      PRIO_ERROR,         //!< An error. An operation did not complete successfully, but the application as a whole is not affected.
+      PRIO_WARNING,       //!< A warning. An operation completed with an unexpected result.
+      PRIO_NOTICE,        //!< A notice, which is an information with just a higher priority.
+      PRIO_INFORMATION,   //!< An informational message, usually denoting the successful completion of an operation.
+      PRIO_DEBUG,         //!< A debugging message.
+      PRIO_TRACE          //!< A tracing message. This is the lowest priority.
+    };
+
+    //! return priority enumeration value from string
+    //! The string can take three forms for matching, e.g. the PRIO_FATAL
+    //! priority would be returned from input strings "PRIO_FATAL", "FATAL"
+    //! or "fatal".
+    //! @param name priority name
+    //! @returns the matching priority enumeration or the PRIO_UNDEFINED
+    //!          if the name is not valid
+    static priority get_priority(const std::string& name);
+
+    //! return priority label from the priority enumeration value
+    //! @param p priority enumeration
+    //! @returns the matching priority label or the empty string if the priority
+    //!          is not valid
+    static std::string get_priority_label(priority p);
+
+    //! return the priority level from a collection of properties
+    //! The following properties are recognized:
+    //!   logging.priority : string = "fatal" # or "trace", "debug",
+    //!                                       # "information", "notice",
+    //!                                       # "warning", "error", "critical"
+    //!
+    //! Shortcuts to activate some specific priorities:
+    //! a)   logging.debug   : boolean = 1
+    //!      debug           : boolean = 1
+    //!   is equivalent to :
+    //!      logging.priority : string = "debug"
+    //!
+    //! b)   logging.verbose : boolean = 1
+    //!      verbose         : boolean = 1
+    //!   is equivalent to :
+    //!      logging.priority : string = "notice"
+    //!
+    //! Note that the "logging.priority" property has precedence on
+    //! "logging.debug" (or "debug") and "logging.verbose" or ("verbose").
+    //! @param config         a collection of properties to be searched
+    //! @param default_prio   the default logging priority threshold
+    //! @param throw_on_error is true, an invalid logging priority label
+    //!        triggers an exception
+    //! @returns the priority as configured from the properties
+    static priority extract_logging_configuration(const datatools::properties & config,
+						  priority default_prio = datatools::logger::PRIO_FATAL,
+						  bool throw_on_error = true);
+
+    static void declare_ocd_logging_configuration(datatools::object_configuration_description &,
+						  const std::string & default_value_ = "fatal",
+						  const std::string & prefix_ = "",
+						  const std::string & from_ = "");
+
   };
-
-
-  //! return priority enumeration value from string
-  //! The string can take three forms for matching, e.g. the PRIO_FATAL
-  //! priority would be returned from input strings "PRIO_FATAL", "FATAL"
-  //! or "fatal".
-  //! @param name priority name
-  //! @returns the matching priority enumeration or the PRIO_UNDEFINED
-  //!          if the name is not valid
-  static priority get_priority(const std::string& name);
-
-  //! return priority label from the priority enumeration value
-  //! @param p priority enumeration
-  //! @returns the matching priority label or the empty string if the priority
-  //!          is not valid
-  static std::string get_priority_label(priority p);
-
-  //! return the priority level from a collection of properties
-  //! The following properties are recognized:
-  //!   logging.priority : string = "fatal" # or "trace", "debug",
-  //!                                       # "information", "notice",
-  //!                                       # "warning", "error", "critical"
-  //!
-  //! Shortcuts to activate some specific priorities:
-  //! a)   logging.debug   : boolean = 1
-  //!      debug           : boolean = 1
-  //!   is equivalent to :
-  //!      logging.priority : string = "debug"
-  //!
-  //! b)   logging.verbose : boolean = 1
-  //!      verbose         : boolean = 1
-  //!   is equivalent to :
-  //!      logging.priority : string = "notice"
-  //!
-  //! Note that the "logging.priority" property has precedence on
-  //! "logging.debug" (or "debug") and "logging.verbose" or ("verbose").
-  //! @param config         a collection of properties to be searched
-  //! @param default_prio   the default logging priority threshold
-  //! @param throw_on_error is true, an invalid logging priority label
-  //!        triggers an exception
-  //! @returns the priority as configured from the properties
-  static priority extract_logging_configuration(const datatools::properties & config,
-						priority default_prio = datatools::logger::PRIO_FATAL,
-						bool throw_on_error = true);
-
-  static void declare_ocd_logging_configuration(datatools::object_configuration_description &,
-						const std::string & default_value_ = "fatal",
-						const std::string & prefix_ = "",
-						const std::string & from_ = "");
-
-};
 } // namespace datatools
 
 //! Log Message if Priority is greater or equal to PRIO_FATAL
-#define DT_LOG_FATAL(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_FATAL) { \
-    std::cerr << "[fatal:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_FATAL(Priority, Message)					\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_FATAL) { \
+      std::cerr << "[fatal:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_CRITICAL
-#define DT_LOG_CRITICAL(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_CRITICAL) { \
-    std::cerr << "[critical:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_CRITICAL(Priority, Message)				\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_CRITICAL) { \
+      std::cerr << "[critical:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_ERROR
-#define DT_LOG_ERROR(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_ERROR) { \
-    std::cerr << "[error:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_ERROR(Priority, Message)					\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p  >= ::datatools::logger::PRIO_ERROR) { \
+      std::cerr << "[error:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_WARNING
-#define DT_LOG_WARNING(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_WARNING) { \
-    std::cerr << "[warning:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_WARNING(Priority, Message)				\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_WARNING) { \
+      std::cerr << "[warning:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_NOTICE
-#define DT_LOG_NOTICE(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_NOTICE) { \
-    std::clog << "[notice:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_NOTICE(Priority, Message)				\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_NOTICE) { \
+      std::clog << "[notice:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_INFORMATION
-#define DT_LOG_INFORMATION(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_INFORMATION) { \
-    std::clog << "[information:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_INFORMATION(Priority, Message)				\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_INFORMATION) { \
+      std::clog << "[information:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_DEBUG
-#define DT_LOG_DEBUG(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_DEBUG) { \
-    std::clog << "[debug:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_DEBUG(Priority, Message)					\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_DEBUG) { \
+      std::clog << "[debug:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log Message if Priority is greater or equal to PRIO_TRACE
-#define DT_LOG_TRACE(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_TRACE) { \
-    std::cerr << "[trace:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
+#define DT_LOG_TRACE(Priority, Message)					\
+  {									\
+    ::datatools::logger::priority _dt_xxx_p = Priority;			\
+    if (_dt_xxx_p == ::datatools::logger::PRIO_ALWAYS || _dt_xxx_p >= ::datatools::logger::PRIO_TRACE) { \
+      std::cerr << "[trace:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
+    }									\
+  }
 
 //! Log an entering message if Priority is greater or equal to PRIO_TRACE
 #define DT_LOG_TRACE_ENTERING(Priority)		\
-{						\
-  DT_LOG_TRACE(Priority, "Entering...");	\
-}
+  {						\
+    DT_LOG_TRACE(Priority, "Entering...");	\
+  }
 
 //! Log an exiting message if Priority is greater or equal to PRIO_TRACE
 #define DT_LOG_TRACE_EXITING(Priority)		\
-{						\
-  DT_LOG_TRACE(Priority, "Exiting.");		\
-}
+  {						\
+    DT_LOG_TRACE(Priority, "Exiting.");		\
+  }
 
-
-//----------------------------------------------------------------------
-// OLD STYLE DEPRECATED MACROS.
-// To actually produce a compiler warning on clang or gcc:
-// Local deprecation namespace which we can use to store deprecated macros
-// This uses a pattern of a deprecated type tag
-// namespace datatools_deprecated {
-// For example, to deprecate datatools_fatal, define:
-// struct datatools_fatal {} __attribute__ ((deprecated)) ;
-// then define datatools_fatal(Priority,Message) as an instantiation
-// of datatools_fatal in a scope block:
-// #define datatools_fatal(Priority, Message) {datatools_deprecated::datatools_fatal a;}
-// Provided the compiler supports deprecation, this should emit a warning
-// that datatools_fatal is deprecated. The name may or may not change
-// depending on the compiler.
-// } // namespace
-
-//! Log Message if Priority is greater or equal to PRIO_FATAL
-//! @deprecated Use DT_LOG_FATAL macro.
-#define datatools_fatal(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_FATAL) { \
-    std::cerr << "[fatal:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_CRITICAL
-//! @deprecated Use DT_LOG_CRITICAL macro.
-#define datatools_critical(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_CRITICAL) { \
-    std::cerr << "[critical:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_ERROR
-//! @deprecated Use DT_LOG_ERROR macro.
-#define datatools_error(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_ERROR) { \
-    std::cerr << "[error:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_WARNING
-//! @deprecated Use DT_LOG_WARNING macro.
-#define datatools_warning(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_WARNING) { \
-    std::cerr << "[warning:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_NOTICE
-//! @deprecated Use DT_LOG_NOTICE macro.
-#define datatools_notice(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_NOTICE) { \
-    std::clog << "[notice:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_INFORMATION
-//! @deprecated Use DT_LOG_INFORMATION macro.
-#define datatools_information(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_INFORMATION) { \
-    std::clog << "[information:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_DEBUG
-//! @deprecated Use DT_LOG_DEBUG macro.
-#define datatools_debug(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_DEBUG) { \
-    std::clog << "[debug:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
-
-//! Log Message if Priority is greater or equal to PRIO_TRACE
-//! @deprecated Use DT_LOG_TRACE macro.
-#define datatools_trace(Priority, Message) \
-{ \
-  if (Priority >= datatools::logger::PRIO_TRACE) { \
-    std::clog << "[trace:" << BOOST_CURRENT_FUNCTION << ":" << __LINE__ << "] " << Message << std::endl; \
-  }\
-}
 
 // Activate reflection layer for the 'datatools::logger::priority' enum:
 DR_TYPE_INIT(::datatools::logger); // mandatory to access the embedded enum
