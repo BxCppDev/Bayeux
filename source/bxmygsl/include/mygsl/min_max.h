7afa93e54f73ca4c2a1a502dda9f8371241eba03
Francois Mauger <mauger@lpccaen.in2p3.fr>
2014-11-20 20:50:14 +0000

    Record more informations from the min/max algorithm
    The mygsl::min_max algorithm record not only the minimum and maximum
    values from a set of values, but also records the indexes of the
    minimum and maximum occurences.

--- source/bxmygsl/include/mygsl/min_max.h
+++ source/bxmygsl/include/mygsl/min_max.h
@@ -1,36 +1,74 @@
 //// \file mygsl/min_max.h
 /* Author(s):     Francois Mauger <mauger@lpccaen.in2p3.fr>
  * Creation date: 2009-10-02
- * Last modified: 2013-04-23
- *
- * License:
+ * Last modified: 2014-11-18
  *
  * Description:
  *
- *   A collection of algorithm to compute the min_max of
- *   a list of numbers
+ *   An algorithm that computes the min and max values from
+ *   a list of numbers.
  *
  */
 
 #ifndef MYGSL_MIN_MAX_H
 #define MYGSL_MIN_MAX_H 1
 
+
+// Third Party:
+// - Bayeux/datatools
+#include <datatools/i_tree_dump.h>
+
 namespace mygsl {
 
   /// \brief Record the min and max values from a set of values
-  class min_max
+  class min_max : public datatools::i_tree_dumpable
   {
   public:
+
+    /// Check validity
     bool is_valid() const;
+
+    /// Return the minimum value
     double get_min() const;
+
+    /// Return the maximum value
     double get_max() const;
+
+    /// Return the median value
     double get_median() const;
+
+    /// Return the index of the minimum value
+    int get_min_index() const;
+
+    /// Return the index of the maximum value
+    int get_max_index() const;
+
+    /// Return the value counter
+    int get_counter() const;
+
+    /// Default constructor
     min_max();
+
+    /// Reset
     void reset();
+
+    /// Add a value
     void add(double value_);
+
+    /// Smart printing
+    virtual void tree_dump(std::ostream & out_ = std::clog,
+                           const std::string & title_  = "",
+                           const std::string & indent_ = "",
+                           bool inherit_ = false) const;
+
   private:
-    double _min_;
-    double _max_;
+
+    int    _counter_;   //!< Value counter
+    double _min_;       //!< Minimum value
+    int    _min_index_; //!< Index of the minimum value
+    double _max_;       //!< Maximum value
+    int    _max_index_; //!< Index of the maximum value
+
   };
 
 } // end of namespace mygsl
--- source/bxmygsl/src/min_max.cc
+++ source/bxmygsl/src/min_max.cc
@@ -1,7 +1,9 @@
-// mygsl::min_max.cc
+// min_max.cc
 
+// Ourselves:
 #include <mygsl/min_max.h>
 
+// Standard library:
 #include <algorithm>
 #include <iostream>
 #include <sstream>
@@ -12,58 +14,102 @@
 
 namespace mygsl {
 
-  using namespace std;
-
-  double min_max::get_min () const
+  double min_max::get_min() const
   {
     return _min_;
   }
 
-  double min_max::get_max () const
+  double min_max::get_max() const
   {
     return _max_;
   }
 
-  bool min_max::is_valid () const
+  bool min_max::is_valid() const
   {
     return _min_ <= _max_;
   }
 
-  double min_max::get_median () const
+  double min_max::get_median() const
   {
     return 0.5 * (_min_ + _max_);
   }
 
-  min_max::min_max ()
+  int min_max::get_min_index() const
   {
-    reset ();
+    return _min_index_;
+  }
+
+  int min_max::get_max_index() const
+  {
+    return _max_index_;
+  }
+
+  int min_max::get_counter() const
+  {
+    return _counter_;
+  }
+
+  min_max::min_max()
+  {
+    reset();
     return;
   }
 
-  void min_max::reset ()
+  void min_max::reset()
   {
-    _min_ = 0.0;
-    _max_ = -1.0;
+    _counter_   =  0;
+    _min_       =  0.0;
+    _min_index_ = -1;
+    _max_       = -1.0;
+    _max_index_ = -1;
     return;
   }
 
-  void min_max::add (double value_)
+  void min_max::add(double value_)
   {
     if (_min_ > _max_) {
       _min_ = value_;
       _max_ = value_;
+      _min_index_ = _counter_;
+      _max_index_ = _counter_;
     } else {
       if (value_ < _min_) {
         _min_ = value_;
+        _min_index_ = _counter_;
       }
       if (value_ > _max_) {
         _max_ = value_;
+        _max_index_ = _counter_;
       }
     }
+    _counter_++;
     return;
   }
 
-}
 
-// end of mygsl::min_max.cc
+  void min_max::tree_dump(std::ostream & out_,
+                          const std::string & title_,
+                          const std::string & indent_,
+                          bool inherit_) const
+  {
+    if (! title_.empty()) out_ << indent_ << title_ << std::endl;
 
+    out_ << indent_ << i_tree_dumpable::tag
+         << "Counter   : " << _counter_ << std::endl;
+
+    out_ << indent_ << i_tree_dumpable::tag
+         << "Min       : " << _min_ << std::endl;
+
+    out_ << indent_ << i_tree_dumpable::tag
+         << "Min index : " << _min_index_ << std::endl;
+
+    out_ << indent_ << i_tree_dumpable::tag
+         << "Max       : " << _max_ << std::endl;
+
+    out_ << indent_ << i_tree_dumpable::inherit_tag(inherit_)
+         << "Max index : " << _max_index_ << std::endl;
+    return;
+  }
+
+
+} // end of namespace mygsl
