28cc0b55fd952138f84ba93b938325b399d796c1
Xavier Garrido <garrido@lal.in2p3.fr>
2015-01-02 17:26:22 +0000

    kozmetix

--- source/bxgenvtx/src/vg_tools.cc
+++ source/bxgenvtx/src/vg_tools.cc
@@ -18,167 +18,163 @@
 
 namespace genvtx {
 
-  using namespace std;
+  const std::string vg_tools::SHAPE_REF_NONE    = "";
+  const std::string vg_tools::SHAPE_REF_PLAIN   = "<plain>";
+  const std::string vg_tools::SHAPE_REF_GETTER  = "<getter>";
 
-  const string vg_tools::SHAPE_REF_NONE    = "";
-  const string vg_tools::SHAPE_REF_PLAIN   = "<plain>";
-  const string vg_tools::SHAPE_REF_GETTER  = "<getter>";
-
-  const datatools::properties & vg_entry_type::get_vg_config () const
+  const datatools::properties & vg_entry_type::get_vg_config() const
   {
     return _vg_config_;
   }
 
-  datatools::properties & vg_entry_type::grab_vg_config ()
+  datatools::properties & vg_entry_type::grab_vg_config()
   {
     return _vg_config_;
   }
 
-  void vg_entry_type::set_vg_config (const datatools::properties & config_)
+  void vg_entry_type::set_vg_config(const datatools::properties & config_)
   {
     _vg_config_ = config_;
     return;
   }
 
-  const std::string & vg_entry_type::get_vg_name () const
+  const std::string & vg_entry_type::get_vg_name() const
   {
     return _vg_name_;
   }
 
-  const std::string & vg_entry_type::get_vg_id () const
+  const std::string & vg_entry_type::get_vg_id() const
   {
     return _vg_id_;
   }
 
-  void vg_entry_type::set_ptr (i_vertex_generator * ptr_)
+  void vg_entry_type::set_ptr(i_vertex_generator * ptr_)
   {
-    _vg_handle_.reset (ptr_);
+    _vg_handle_.reset(ptr_);
     return;
   }
 
-  void vg_entry_type::set_vg_name (const std::string & n_)
+  void vg_entry_type::set_vg_name(const std::string & n_)
   {
     _vg_name_ = n_;
     return;
   }
 
-  void vg_entry_type::set_vg_id (const std::string & id_)
+  void vg_entry_type::set_vg_id(const std::string & id_)
   {
     _vg_id_ = id_;
     return;
   }
 
-  void vg_entry_type::set_blank ()
+  void vg_entry_type::set_blank()
   {
     _vg_status_ = 0;
     return;
   }
 
-  void vg_entry_type::set_created ()
+  void vg_entry_type::set_created()
   {
     _vg_status_ |= vg_entry_type::STATUS_CREATED;
     return;
   }
 
-  void vg_entry_type::set_initialized ()
+  void vg_entry_type::set_initialized()
   {
     _vg_status_ |= vg_entry_type::STATUS_INITIALIZED;
     return;
   }
 
-  void vg_entry_type::set_uninitialized ()
+  void vg_entry_type::set_uninitialized()
   {
     _vg_status_ ^= vg_entry_type::STATUS_INITIALIZED;
     return;
   }
 
-  bool vg_entry_type::has_manager () const
+  bool vg_entry_type::has_manager() const
   {
     return _manager_ != 0;
   }
 
-  void vg_entry_type::set_manager (::genvtx::manager & mgr_)
+  void vg_entry_type::set_manager(::genvtx::manager & mgr_)
   {
     _manager_ = &mgr_;
     return;
   }
 
-  void vg_entry_type::reset_manager ()
+  void vg_entry_type::reset_manager()
   {
     _manager_ = 0;
     return;
   }
 
-  vg_entry_type::vg_entry_type ()
+  vg_entry_type::vg_entry_type()
   {
     _vg_status_ = 0;
     _manager_ = 0;
     return;
   }
 
-  bool vg_entry_type::is_created () const
+  bool vg_entry_type::is_created() const
   {
     return _vg_status_ & STATUS_CREATED;
   }
 
-  bool vg_entry_type::is_initialized () const
+  bool vg_entry_type::is_initialized() const
   {
     return _vg_status_ & STATUS_INITIALIZED;
   }
 
-  bool vg_entry_type::has_vg () const
+  bool vg_entry_type::has_vg() const
   {
     return _vg_handle_;
   }
 
-  const i_vertex_generator & vg_entry_type::get_vg () const
+  const i_vertex_generator & vg_entry_type::get_vg() const
   {
-    return _vg_handle_.get ();
+    return _vg_handle_.get();
   }
 
-  i_vertex_generator & vg_entry_type::grab_vg ()
+  i_vertex_generator & vg_entry_type::grab_vg()
   {
-    return _vg_handle_.grab ();
+    return _vg_handle_.grab();
   }
 
-  const vg_handle_type & vg_entry_type::get_vg_handle () const
+  const vg_handle_type & vg_entry_type::get_vg_handle() const
   {
     return _vg_handle_;
   }
 
-  vg_handle_type & vg_entry_type::grab_vg_handle ()
+  vg_handle_type & vg_entry_type::grab_vg_handle()
   {
     return _vg_handle_;
   }
 
-  vg_handle_type & vg_entry_type::grab_initialized_vg_handle ()
+  vg_handle_type & vg_entry_type::grab_initialized_vg_handle()
   {
-    if (! has_vg ()) {
-      DT_THROW_IF (_manager_ == 0,std::logic_error,
-                   "No manager is available to create the vg '" << this->_vg_name_ << "' ! ");
-      _manager_->create_vg (*this);
+    if (! has_vg()) {
+      DT_THROW_IF(_manager_ == 0,std::logic_error,
+                  "No manager is available to create the vg '" << this->_vg_name_ << "' ! ");
+      _manager_->create_vg(*this);
     }
-    DT_THROW_IF (_manager_ == 0,std::logic_error,
-                 "No manager is available to initialize the vg '" << this->_vg_name_ << "' !");
-    _manager_->initialize_vg (*this);
+    DT_THROW_IF(_manager_ == 0,std::logic_error,
+                "No manager is available to initialize the vg '" << this->_vg_name_ << "' !");
+    _manager_->initialize_vg(*this);
     return _vg_handle_;
   }
 
-  void vg_entry_type::tree_dump (std::ostream & out_ ,
-                                 const std::string & title_,
-                                 const std::string & indent_,
-                                 bool inherit_) const
+  void vg_entry_type::tree_dump(std::ostream & out_ ,
+                                const std::string & title_,
+                                const std::string & indent_,
+                                bool inherit_) const
   {
     namespace du = datatools;
     std::string indent;
-    if (! indent_.empty ())
-      {
-        indent = indent_;
-      }
-    if ( ! title_.empty () )
-      {
-        out_ << indent << title_ << std::endl;
-      }
+    if (! indent_.empty()) {
+      indent = indent_;
+    }
+    if (! title_.empty()) {
+      out_ << indent << title_ << std::endl;
+    }
 
     out_ << indent << du::i_tree_dumpable::tag
          << "Vg name     : '" << _vg_name_ << "'" << std::endl;
@@ -187,45 +183,39 @@ namespace genvtx {
          << "Vg ID       : '" << _vg_id_ << "'" << std::endl;
 
     out_ << indent << du::i_tree_dumpable::tag
-         << "Vg config   : " << _vg_config_.size () << " properties" << std::endl;
+         << "Vg config   : " << _vg_config_.size() << " properties" << std::endl;
 
     out_ << indent << du::i_tree_dumpable::tag
          << "Vg status   : " << _vg_status_;
     {
       size_t count = 0;
       std::ostringstream status_info;
-      if (is_created ())
-        {
-          if (count) status_info << ',';
-          status_info << "created";
-          count++;
-        }
-      if (is_initialized ())
-        {
-          if (count) status_info << ',';
-          status_info << "initialized";
-          count++;
-        }
-      if (count)
-        {
-          out_ << ' ' << '(' << status_info.str () << ')';
-        }
+      if (is_created()) {
+        if (count) status_info << ',';
+        status_info << "created";
+        count++;
+      }
+      if (is_initialized()) {
+        if (count) status_info << ',';
+        status_info << "initialized";
+        count++;
+      }
+      if (count) {
+        out_ << ' ' << '(' << status_info.str() << ')';
+      }
     }
     out_ << std::endl;
 
     out_ << indent << du::i_tree_dumpable::tag
          << "Vg handle   : ";
-    if (has_vg ())
-      {
-        out_ << "'" << "yes" << "'";
-      }
-    else
-      {
-        out_ << "<null>";
-      }
+    if (has_vg()) {
+      out_ << "'" << "yes" << "'";
+    } else {
+      out_ << "<null>";
+    }
     out_ << std::endl;
 
-    out_ << indent << du::i_tree_dumpable::inherit_tag (inherit_)
+    out_ << indent << du::i_tree_dumpable::inherit_tag(inherit_)
          << "Manager : " << _manager_ << std::endl;
 
     return;
