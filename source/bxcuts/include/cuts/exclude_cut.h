06e0e32223de5ecd5fdcfc55e5d33cfef54c4fdb
Francois Mauger <mauger@lpccaen.in2p3.fr>
2015-06-12 15:46:11 +0000

    Add the cuts::exclude_cut class
    Thsi class was missing to represent the subtraction operation
    between sets. It now comes in complement of:
    
     * cuts::and_cuts : Set intersection (A*B) (binary)
     * cuts::or_cuts : Set union (A+B) (binary)
     * cuts::not_cuts : Negation (!A) (unary)
    
    with:
    
     * cuts::exclude_cuts : Set subtraction (A-B) (binary)

---
+++ source/bxcuts/include/cuts/exclude_cut.h
@@ -0,0 +1,57 @@
+/// \file cuts/exclude_cut.h
+/* Author(s) :    Francois Mauger <mauger@lpccaen.in2p3.fr>
+ * Creation date: 2015-06-10
+ * Last modified: 2015-06-10
+ *
+ * License:
+ *
+ * Description:
+ *   EXCLUDE binary cut
+ *
+ * History:
+ *
+ */
+
+#ifndef CUTS_EXCLUDE_CUT_H
+#define CUTS_EXCLUDE_CUT_H 1
+
+// This project:
+#include <cuts/i_binary_cut.h>
+
+namespace cuts {
+
+  /// \brief The EXCLUDE binary cut (a OR NOT b)
+  class exclude_cut : public i_binary_cut
+  {
+  public:
+
+    /// Constructor
+    exclude_cut(datatools::logger::priority a_logging_priority =
+            datatools::logger::PRIO_FATAL);
+
+    /// Destructor
+    virtual ~exclude_cut();
+
+  protected :
+
+    /// Selection
+    virtual int _accept();
+
+  private:
+
+    /// Macro to automate the registration of the cut
+    CUT_REGISTRATION_INTERFACE(exclude_cut);
+
+  };
+
+} // end of namespace cuts
+
+#endif // CUTS_EXCLUDE_CUT_H
+
+/*
+** Local Variables: --
+** mode: c++ --
+** c-file-style: "gnu" --
+** tab-width: 2 --
+** End: --
+*/
