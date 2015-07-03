81d799922375bca519a2c6d0b58c244fb8d56750
Francois Mauger <mauger@lpccaen.in2p3.fr>
2015-07-03 21:57:02 +0000

    Fix invalid test at initialization

--- source/bxmygsl/include/mygsl/von_neumann_method.h
+++ source/bxmygsl/include/mygsl/von_neumann_method.h
@@ -9,7 +9,7 @@
  * Description:
  *
  *  A Von Neumann Pseudo Random Number generator
- *  for an arbitrary  R to R functor used as the density function.
+ *  for an arbitrary R to R functor used as the density function.
  *
  * History:
  *
@@ -33,41 +33,50 @@ namespace mygsl {
   public:
 
     static const double AUTO_FMAX;
+    static const size_t MIN_NSAMPLES       = 2;
     static const size_t DEFAULT_NSAMPLES   = 1000;
     static const size_t DEFAULT_MAX_COUNTS = 10000;
     static const size_t NO_MAX_COUNTS      = 0;
 
-    bool is_initialized () const;
+    bool is_initialized() const;
 
-    double get_xmin () const;
+    double get_xmin() const;
 
-    double get_xmax () const;
+    double get_xmax() const;
 
-    double get_fmax () const;
+    double get_fmax() const;
 
-    size_t get_max_counts () const;
+    size_t get_max_counts() const;
 
-    von_neumann_method ();
+    von_neumann_method();
 
-    von_neumann_method (double xmin_,
-                        double xmax_,
-                        const i_unary_function & func_,
-                        double fmax_ = 0.0,
-                        size_t nsamples_ = DEFAULT_NSAMPLES,
-                        size_t max_counts_ = DEFAULT_MAX_COUNTS);
+    von_neumann_method(double xmin_,
+                       double xmax_,
+                       const i_unary_function & func_,
+                       double fmax_ = 0.0,
+                       size_t nsamples_ = DEFAULT_NSAMPLES,
+                       size_t max_counts_ = DEFAULT_MAX_COUNTS);
 
-    void init (double xmin_,
-               double xmax_,
-               const i_unary_function & func_,
-               double fmax_,
-               size_t nsamples_ = 0,
-               size_t max_counts_ = DEFAULT_MAX_COUNTS);
+    void initialize(double xmin_,
+                    double xmax_,
+                    const i_unary_function & func_,
+                    double fmax_,
+                    size_t nsamples_,
+                    size_t max_counts_);
 
-    void reset ();
+    /// @deprecated @see initialize
+    void init(double xmin_,
+              double xmax_,
+              const i_unary_function & func_,
+              double fmax_,
+              size_t nsamples_ = DEFAULT_NSAMPLES,
+              size_t max_counts_ = DEFAULT_MAX_COUNTS);
 
-    double shoot (rng &);
+    void reset();
 
-    void dump (std::ostream & out_ = std::clog) const;
+    double shoot(rng &);
+
+    void dump(std::ostream & out_ = std::clog) const;
 
   private:
 
