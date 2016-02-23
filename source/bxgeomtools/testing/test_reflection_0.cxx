e4e373f34b1149b50a92fbdc5390b0f5f313d205
Ben Morgan <ben.morgan@warwick.ac.uk>
2016-02-23 12:48:14 +0000

    Fix tests for CAMP API

--- source/bxgeomtools/testing/test_reflection_0.cxx
+++ source/bxgeomtools/testing/test_reflection_0.cxx
@@ -26,9 +26,8 @@ void test_placement()
   datatools::logger::priority logging = datatools::logger::PRIO_NOTICE;
   const DR_CLASS & plctMetaClass  = DR_CLASS_BY_NAME("geomtools::placement");
 
-  boost::scoped_ptr<geomtools::placement> plct0(plctMetaClass.construct<geomtools::placement>());
+  DR_OBJECT plctObj0 = plctMetaClass.construct();
 
-  DR_OBJECT plctObj0(*plct0.get());
   DT_LOG_NOTICE(logging, "Initializing the placement object...");
 
   plctObj0.DR_CALL("set_translation", DR_ARGS(3.9 * CLHEP::mm,
@@ -38,8 +37,6 @@ void test_placement()
                                                   25.0 * CLHEP::degree,
                                                    0.0 * CLHEP::degree));
   plctObj0.DR_CALL("tree_print", DR_ARGS(1,"The placement: "));
-
-  boost::scoped_ptr<geomtools::placement> plct1(plctMetaClass.construct<geomtools::placement>());
 
   // Does not work... to be investigated...
   // DR_OBJECT plctObj1(*plct1.get());
@@ -56,21 +53,18 @@ void test_base_hit()
   const DR_CLASS & gidMetaClass  = DR_CLASS_BY_NAME("geomtools::geom_id");
   const DR_CLASS & propMetaClass = DR_CLASS_BY_NAME("datatools::properties");
 
-  boost::scoped_ptr<geomtools::base_hit> hit0(hitMetaClass.construct<geomtools::base_hit>());
-  boost::scoped_ptr<geomtools::geom_id> gid0(gidMetaClass.construct<geomtools::geom_id>(DR_ARGS(1203, 1,3,4)));
-  boost::scoped_ptr<datatools::properties> prop0(propMetaClass.construct<datatools::properties>(DR_ARGS("Hit auxiliary properties")));
 
-  DR_OBJECT gObj0(*gid0.get());
+  DR_OBJECT gObj0 = gidMetaClass.construct(DR_ARGS(1203, 1,3,4));
   DT_LOG_NOTICE(logging, "Mutable GID from the hit object : " << gObj0);
 
-  DR_OBJECT pObj0(*prop0.get());
+  DR_OBJECT pObj0 = propMetaClass.construct(DR_ARGS("Hit auxiliary properties"));
   pObj0.DR_CALL("store_flag",    DR_ARGS("raw", "", false));
   pObj0.DR_CALL("store_real",    DR_ARGS("time", 0.343, "Hit timestamp", false));
   pObj0.DR_CALL("store_real",    DR_ARGS("energy", 1.278, "Hit energy", false));
   pObj0.DR_CALL("store_integer", DR_ARGS("status", 3, "Hit status", false));
 
   DT_LOG_NOTICE(logging, "Modify the ID and GID of the hit object : ");
-  DR_OBJECT hObj0(*hit0.get());
+  DR_OBJECT hObj0 = hitMetaClass.construct();
   hObj0.set("hit_id",        DR_VALUE(3));
   hObj0.set("geom_id",       DR_VALUE(gObj0));
   hObj0.set("auxiliaries",   DR_VALUE(pObj0));
@@ -105,33 +99,23 @@ void test_geom_id()
   datatools::logger::priority logging = datatools::logger::PRIO_NOTICE;
   const DR_CLASS & gidMetaClass = DR_CLASS_BY_NAME("geomtools::geom_id");
 
-  boost::scoped_ptr<geomtools::geom_id> gid0(gidMetaClass.construct<geomtools::geom_id>(DR_ARGS(1203, 1,3,4)));
-
-  DT_LOG_NOTICE(logging, "Original GID object : ");
-  std::clog << "gid0 = " << *gid0.get() << std::endl;
 
   DT_LOG_NOTICE(logging, "Modify the type and depth of the GID object : ");
-  DR_OBJECT vObj0(*gid0.get());
+  DR_OBJECT vObj0 = gidMetaClass.construct(DR_ARGS(1203, 1,3,4));
   vObj0.set("type", DR_VALUE(1220));
   vObj0.set("depth", DR_VALUE(5));
-  std::clog << "gid0 = " << *gid0.get() << std::endl;
 
   DT_LOG_NOTICE(logging, "Set missing sub-addresses in the GID object : ");
   gidMetaClass.function("set").call(vObj0, DR_ARGS(3, 17) );
   gidMetaClass.function("set").call(vObj0, DR_ARGS(4, 666) );
-  std::clog << "gid0 = " << *gid0.get() << std::endl;
 
-  boost::scoped_ptr<geomtools::geom_id> gid1(gidMetaClass.construct<geomtools::geom_id>(DR_ARGS(1220, 1,3,4)));
-  DR_OBJECT vObj1(*gid1.get());
+  DR_OBJECT vObj1 = gidMetaClass.construct(DR_ARGS(1220, 1,3,4));
   gidMetaClass.function("set_any").call(vObj1, DR_ARGS(3) );
   gidMetaClass.function("set_any").call(vObj1, DR_ARGS(4) );
-  std::clog << "gid1 = " << *gid1.get() << std::endl;
   DR_VALUE matchVal = gidMetaClass.function("match").call(vObj1, DR_ARGS(vObj0, false) );
   std::clog << "matchVal = " << matchVal << std::endl;
 
-  boost::scoped_ptr<geomtools::geom_id> gid2(gidMetaClass.construct<geomtools::geom_id>(DR_ARGS(1223, 9,4)));
-  DR_OBJECT vObj2(*gid2.get());
-  std::clog << "gid2 = " << *gid2.get() << std::endl;
+  DR_OBJECT vObj2 = gidMetaClass.construct(DR_ARGS(1223, 9,4));
   DR_VALUE matchVal2 = gidMetaClass.function("match").call(vObj2, DR_ARGS(vObj1, false) );
   std::clog << "matchVal2 = " << matchVal2 << std::endl;
 
@@ -143,10 +127,7 @@ void test_vector_3d()
 
   const DR_CLASS & v3dMetaClass = DR_CLASS_BY_NAME("geomtools::vector_3d");
 
-  boost::scoped_ptr<geomtools::vector_3d> v0(v3dMetaClass.construct<geomtools::vector_3d>(DR_ARGS(1.0, 2.0, 3.0)));
-
-  std::clog << "v0 = " << *v0.get() << std::endl;
-  DR_OBJECT vObj0(*v0.get());
+  DR_OBJECT vObj0 = v3dMetaClass.construct(DR_ARGS(1.0, 2.0, 3.0));
 
   vObj0.set("x", DR_VALUE(4.0));
   vObj0.set("y", DR_VALUE(0.0));
@@ -154,12 +135,9 @@ void test_vector_3d()
   std::clog << "v0.x = " << vObj0.get("x") << std::endl;
   std::clog << "v0.y = " << vObj0.get("y") << std::endl;
   std::clog << "v0.z = " << vObj0.get("z") << std::endl;
-  std::clog << "v0   = " << *v0.get() << std::endl;
 
   DR_VALUE magValue = vObj0.get("mag");
   std::clog << "|v0| = " << magValue << std::endl;
-
-  std::clog << "v0 = " << *v0.get() << std::endl;
 
   return;
 }
