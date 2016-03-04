a891b798fef838e2beee327ae24a2fa12ccb1e5b
Ben Morgan <ben.morgan@warwick.ac.uk>
2016-03-04 18:03:07 +0000

    Apply minimum warnings and C++ std flags
    In preparation for C++11 migration, bump warning default warning levels
    in Bayeux for GNU/Clang to all, extra and shadow, plus pedantic as
    an initial minimum set. Update optional warnings-are-errors option
    to make these fail as quickly as possible to help track down cause
    more efficiently.
    
    Remove developer option to remove warnings to enforce minimum set, but
    retain ability to switch off warnings-are-errors.
    
    Update brio build to disable warnings on generated sources via
    COMPILE_OPTIONS.

--- source/bxbrio_module.cmake
+++ source/bxbrio_module.cmake
@@ -61,6 +61,12 @@ if(BAYEUX_USE_LEGACY_ROOT)
     "${module_include_dir}"
     )
   list(APPEND ${module_name}_MODULE_SOURCES ${PROJECT_BINARY_DIR}/brio_dict.cc)
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)+Clang")
+    set_property(SOURCE ${PROJECT_BINARY_DIR}/brio_dict.cc
+      PROPERTY COMPILE_FLAGS "-Wno-keyword-macro -Wno-c++11-long-long"
+      )
+  endif()
 else()
   root_generate_dictionary(brio_dict
     ${module_include_dir}/${module_name}/detail/brio_record.h
@@ -68,6 +74,11 @@ else()
     LINKDEF "${PROJECT_SOURCE_DIR}/source/brio_linkdef.h"
     )
   list(APPEND ${module_name}_MODULE_SOURCES ${CMAKE_CURRENT_BINARY_DIR}/brio_dict.cxx)
+  if(CMAKE_CXX_COMPILER_ID MATCHES "(Apple)+Clang")
+    set_property(SOURCE ${CMAKE_CURRENT_BINARY_DIR}/brio_dict.cxx
+      PROPERTY COMPILE_FLAGS "-Wno-keyword-macro -Wno-c++11-long-long"
+      )
+    endif()
 endif()
 
 # - Published headers
