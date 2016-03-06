076c8855009cd43445c8cc86bb87b5e190812d6b
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-03-06 21:26:04 +0000

    Add GCC pragma

--- source/bxbrio/include/brio/writer-inl.h
+++ source/bxbrio/include/brio/writer-inl.h
@@ -30,40 +30,48 @@
 // ROOT can shadow CLHEP and vice versa, appears not to be serious
 #pragma clang diagnostic ignored "-Wshadow"
 #endif
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+// ROOT can shadow CLHEP and vice versa, appears not to be serious
+#pragma GCC diagnostic ignored "-Wshadow"
+#endif
 #include <TTree.h>
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif
 
 namespace brio {
   template <typename T>
-    int writer::store(const T& a_data, const std::string& a_label) {
+  int writer::store(const T& a_data, const std::string& a_label) {
     DT_THROW_IF(!this->is_opened(),
-		std::logic_error,
-		"Operation prohibited; file is not opened !");
+                std::logic_error,
+                "Operation prohibited; file is not opened !");
     store_info *ptr_si = this->_get_store_info(a_label);
     if (!ptr_si) {
       if (a_label.empty()) {
-	// if we do not allow automatic store, this is a critical error:
-	DT_THROW_IF(!_allow_automatic_store_,
-		    std::logic_error,
-		    "No target store is selected nor target !");
-	ptr_si = this->_add_store(store_info::constants::automatic_store_label(),
-				  a_data.get_serial_tag(),
-				  store_info::constants::default_store_buffer_size());
+        // if we do not allow automatic store, this is a critical error:
+        DT_THROW_IF(!_allow_automatic_store_,
+                    std::logic_error,
+                    "No target store is selected nor target !");
+        ptr_si = this->_add_store(store_info::constants::automatic_store_label(),
+                                  a_data.get_serial_tag(),
+                                  store_info::constants::default_store_buffer_size());
       } else {
-	ptr_si = this->_add_store(a_label, a_data.get_serial_tag(),
-				  store_info::constants::default_store_buffer_size());
+        ptr_si = this->_add_store(a_label, a_data.get_serial_tag(),
+                                  store_info::constants::default_store_buffer_size());
       }
     }
     // Final check:
     DT_THROW_IF(ptr_si == 0,
-		std::logic_error,
-		"Could not determine any store to save data !");
+                std::logic_error,
+                "Could not determine any store to save data !");
     DT_LOG_DEBUG(this->get_logging_priority(),
-		 "ptr_si = " << std::hex << ptr_si << std::dec);
+                 "ptr_si = " << std::hex << ptr_si << std::dec);
     DT_LOG_DEBUG(this->get_logging_priority(),
-		 "Using store with label '" << ptr_si->label << "'...");
+                 "Using store with label '" << ptr_si->label << "'...");
     if (this->get_logging_priority() >= datatools::logger::PRIO_DEBUG) {
       ptr_si->tree->Print ();
     }
@@ -71,28 +79,28 @@ namespace brio {
   }
 
   template <typename T>
-    int writer::_at_store(const T& a_data, store_info *a_store_info) {
+  int writer::_at_store(const T& a_data, store_info *a_store_info) {
     DT_LOG_TRACE(this->get_logging_priority(),"Entering...");
     store_info *ptr_si = a_store_info;
     // First serialized object sets the serialization tag for this store:
     if (ptr_si->serialization_tag ==
-	store_info::constants::postponed_dedicated_serial_tag_label()) {
+        store_info::constants::postponed_dedicated_serial_tag_label()) {
       ptr_si->serialization_tag = a_data.get_serial_tag();
     }
     // Else if the store has a dedicated serialization tag:
     else if (ptr_si->has_dedicated_serialization_tag()) {
       // Check the data serialization tag matches that requested by the store:
       DT_THROW_IF(a_data.get_serial_tag() != ptr_si->get_serialization_tag(),
-		  std::logic_error,
-		  "Serialization tag mismatch ! "
-		  << "Attempt to store an object with `"
-		  << a_data.get_serial_tag ()
-		  << "' serialization tag "
-		  << "in the store labelled '"
-		  << ptr_si->label
-		  << "' with dedicated `"
-		  << ptr_si->get_serialization_tag ()
-		  << "' serialization tag !");
+                  std::logic_error,
+                  "Serialization tag mismatch ! "
+                  << "Attempt to store an object with `"
+                  << a_data.get_serial_tag ()
+                  << "' serialization tag "
+                  << "in the store labelled '"
+                  << ptr_si->label
+                  << "' with dedicated `"
+                  << ptr_si->get_serialization_tag ()
+                  << "' serialization tag !");
     }
 
     // Prepare the (std::vector<char>) buffer to host the binary archive
@@ -130,10 +138,10 @@ namespace brio {
     // Now the buffer contains the final sequence of bytes corresponding to
     // the serialized output binary archive:
     DT_LOG_DEBUG(this->get_logging_priority(),
-		 "Buffer size = "
-		 << ptr_si->buffer.size()
-		 << "   buffer capacity = "
-		 << ptr_si->buffer.capacity());
+                 "Buffer size = "
+                 << ptr_si->buffer.size()
+                 << "   buffer capacity = "
+                 << ptr_si->buffer.capacity());
     // Prepare the container interface to be streamed using the ROOT I/O system:
     ptr_si->record.fSerialTag  = a_data.get_serial_tag().c_str();
     ptr_si->record.fVersionTag = 0; // not used
