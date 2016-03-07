1d71ab3fbbc027ce455f0c646b8bf544233b7c15
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-03-07 09:47:12 +0000

    Make test progs faster

--- source/bxdatatools/testing/test_time_tools.cxx
+++ source/bxdatatools/testing/test_time_tools.cxx
@@ -12,10 +12,9 @@
 #include <datatools/time_tools.h>
 #include <datatools/clhep_units.h>
 
-using namespace std;
-
-int main (int argc_ , char ** argv_)
+int main(int argc_ , char ** argv_)
 {
+  using namespace std;
   int error_code = EXIT_SUCCESS;
   try
     {
@@ -24,52 +23,52 @@ int main (int argc_ , char ** argv_)
       bool debug = false;
 
       int iarg = 1;
-      while (iarg <  argc_)
+      while(iarg <  argc_)
         {
           string arg = argv_[iarg];
 
-          if (arg == "-d" || arg == "--debug") debug =  true;
+          if(arg == "-d" || arg == "--debug") debug =  true;
 
           iarg++;
         }
 
       long seed = 314159;
-      srand48 (seed);
+      srand48(seed);
 
       datatools::computing_time CT;
 
       size_t max_run = 50;
-      for (int run = 0; run < (int) max_run; run++)
+      for(int run = 0; run <(int) max_run; run++)
         {
-          size_t run_size = (size_t) (10000 + 10000 * drand48 ());
-          if (debug) cerr << "DEBUG: Run size = " << run_size << endl;
-          CT.start (); // start the clock
-          for (int i = 0; i < (int) run_size; i++)
+          size_t run_size =(size_t)(1000 + 1000 * drand48());
+          if(debug) cerr << "DEBUG: Run size = " << run_size << endl;
+          CT.start(); // start the clock
+          for(int i = 0; i <(int) run_size; i++)
             {
-              double t = drand48 () * 2 * M_PI;
-              double x = cos (t);
-              CT.pause ();
+              double t = drand48() * 2 * M_PI;
+              double x = cos(t);
+              CT.pause();
               clog << "x=" << x << endl;
-              double y = sin (t);
+              double y = sin(t);
               clog << "y=" << y << endl;
-              CT.resume ();
+              CT.resume();
             }
-          CT.stop (); // stop the clock
-          cout << CT.get_last_elapsed_time () / CLHEP::second << endl;
+          CT.stop(); // stop the clock
+          cout << CT.get_last_elapsed_time() / CLHEP::second << endl;
         }
 
-      CT.tree_dump (clog, "Computing time: ", "NOTICE: ");
-      CT.reset ();
+      CT.tree_dump(clog, "Computing time: ", "NOTICE: ");
+      CT.reset();
       clog << "Reset the 'computing time' tools !" << endl;
-      CT.tree_dump (clog, "Computing time: ", "NOTICE: ");
+      CT.tree_dump(clog, "Computing time: ", "NOTICE: ");
 
     }
-  catch (exception & x)
+  catch(exception & x)
     {
       cerr << "error: " << x.what() << endl;
       error_code = EXIT_FAILURE;
     }
-  catch (...)
+  catch(...)
     {
       cerr << "error: " << "unexpected error!" << endl;
       error_code = EXIT_FAILURE;
