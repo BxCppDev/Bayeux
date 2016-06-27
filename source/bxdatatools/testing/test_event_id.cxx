1859759b452f175c66f5100be4e1eb1ea76483c1
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-06-27 15:58:43 +0000

    Migrate a test program to Catch

--- source/bxdatatools/testing/test_event_id.cxx
+++ source/bxdatatools/testing/test_event_id.cxx
@@ -1,5 +1,8 @@
 // test_event_id.cxx
 
+// The Catch API:
+#include "catch.hpp"
+
 // Ourselves:
 #include <datatools/event_id.h>
 
@@ -10,81 +13,88 @@
 #include <stdexcept>
 #include <list>
 
+// Third party:
+// - Boost:
+#include <boost/filesystem.hpp>
+
 // This project:
 #include <datatools/io_factory.h>
 
-int main (int /* argc_ */, char ** /* argv_ */)
+void test_1();
+
+TEST_CASE("Test Bayeux/datatools::event_id class", "[bxdatatools][event_id]")
+{
+  test_1();
+}
+
+// int main(int /* argc_ */, char ** /* argv_ */)
+// {
+//   int error_code = EXIT_SUCCESS;
+//   try {
+//     std::clog << "Hello, this is a test program for class 'datatools::event_id'!" << std::endl;
+//     test_1();
+//   } catch(std::exception & x) {
+//     std::cerr << "error: " << x.what() << std::endl;
+//     error_code = EXIT_FAILURE;
+//   } catch(...) {
+//     std::cerr << "error: " << "unexpected error!" << std::endl;
+//     error_code = EXIT_FAILURE;
+//   }
+//   return error_code;
+// }
+
+void test_1()
 {
   using namespace std;
-  int error_code = EXIT_SUCCESS;
-  try
-    {
-      clog << "Hello, this is a test program for class 'datatools::event_id'!" << endl;
+  datatools::event_id my_id;
 
-      datatools::event_id my_id;
+  my_id.set(666, 34);
 
-      my_id.set (666, 34);
+  clog << "Event id = " << my_id << endl;
 
-      clog << "Event id = " << my_id << endl;
-
-      try {
-        std::istringstream ins("666_7635");
-        clog << "Enter event id [format=XXX_YYY] ? ";
-        ins >> my_id;
-        if (! ins) {
-          throw runtime_error ("Format error!");
-        }
-        clog << "Event id = " << my_id << endl;
-        my_id.tree_dump (clog, "datatools::event_id:");
-      }
-      catch (exception & x) {
-        cerr << "Format error !" << endl;
-      }
-
-      string filename = "test_event_id.xml";
-      {
-        clog << "Storing..." << endl;
-        list<datatools::event_id> ids;
-        datatools::data_writer writer(filename);
-
-        for (int i = 0; i < 4; i++)
-          {
-            ids.push_back (datatools::event_id (666, i));
-            ids.back ().tree_dump (clog, "event_id","<<< ");
-            writer.store (ids.back ());
-          }
-      }
-
-      {
-        clog << "Loading..." << endl;
-        list<datatools::event_id> ids;
-        datatools::data_reader reader (filename);
-        while (reader.has_record_tag ())
-          {
-            if (reader.record_tag_is (datatools::event_id::SERIAL_TAG))
-              {
-                datatools::event_id id;
-                ids.push_back (id);
-                reader.load (ids.back ());
-                ids.back ().tree_dump (clog, "event_id",">>> ");
-              }
-            else
-              {
-                break;
-              }
-          }
-      }
-
+  try {
+    std::istringstream ins("666_7635");
+    clog << "Enter event id [format=XXX_YYY] ? ";
+    ins >> my_id;
+    if(! ins) {
+      throw runtime_error("Format error!");
     }
-  catch (exception & x)
-    {
-      cerr << "error: " << x.what () << endl;
-      error_code = EXIT_FAILURE;
+    clog << "Event id = " << my_id << endl;
+    my_id.tree_dump(clog, "datatools::event_id:");
+  } catch(exception & x) {
+    cerr << "Format error !" << endl;
+  }
+
+  string filename = "test_event_id.xml";
+  {
+    clog << "Storing..." << endl;
+    list<datatools::event_id> ids;
+    datatools::data_writer writer(filename);
+    for(int i = 0; i < 4; i++) {
+      ids.push_back(datatools::event_id(666, i));
+      ids.back().tree_dump(clog, "event_id","<<< ");
+      writer.store(ids.back());
     }
-  catch (...)
-    {
-      cerr << "error: " << "unexpected error!" << endl;
-      error_code = EXIT_FAILURE;
+  }
+
+  {
+    clog << "Loading..." << endl;
+    list<datatools::event_id> ids;
+    datatools::data_reader reader(filename);
+    while(reader.has_record_tag()) {
+      if(reader.record_tag_is(datatools::event_id::SERIAL_TAG)) {
+        datatools::event_id id;
+        ids.push_back(id);
+        reader.load(ids.back());
+        ids.back().tree_dump(clog, "event_id",">>> ");
+      } else {
+        break;
+      }
     }
-  return error_code;
+  }
+
+  if (boost::filesystem::exists(filename)) {
+    boost::filesystem::remove(filename);
+  }
+  return;
 }
