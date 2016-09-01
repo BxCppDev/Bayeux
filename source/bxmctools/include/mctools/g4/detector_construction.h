8a20add67a313eddb183ec761385d4d73c856c92
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-09-01 11:01:19 +0000

    Kozmetix

--- source/bxmctools/include/mctools/g4/detector_construction.h
+++ source/bxmctools/include/mctools/g4/detector_construction.h
@@ -71,75 +71,77 @@ namespace mctools {
     public:
 
       typedef std::map<std::string, sensitive_detector *>      sensitive_detector_dict_type;
-      //typedef std::map<std::string, std::vector<std::string> > region_infos_dict_type;
       typedef mctools::step_hit_processor_factory              SHPF_type;
 
       /// Default control distance for particle tracking in magnetic field
       static const double DEFAULT_MISS_DISTANCE;
 
       /// Constructor
-      detector_construction (manager & mgr_);
+      detector_construction();
+
+      /// Constructor
+      detector_construction(manager & mgr_);
 
       /// Destructor
-      virtual ~detector_construction ();
+      virtual ~detector_construction();
 
       /// Check initialization flag
-      bool is_initialized () const;
+      bool is_initialized() const;
 
       /// Return a non-mutable reference to the simulation manager
-      const manager & get_manager () const;
+      const manager & get_manager() const;
 
       /// Return a mutable reference to the simulation manager
-      manager & grab_manager ();
+      manager & grab_manager();
 
       /// Check for a referenced geometry manager
       bool has_geometry_manager() const;
 
       /// Set the reference to a geometry manager
-      void set_geometry_manager (const geomtools::manager & gm_);
+      void set_geometry_manager(const geomtools::manager & gm_);
 
       /// Return a non-mutable reference to the geometry manager
-      const geomtools::manager & get_geometry_manager () const;
+      const geomtools::manager & get_geometry_manager() const;
 
       /// Check for a referenced magnetic field manager
-      bool has_mag_field_manager () const;
+      bool has_mag_field_manager() const;
 
       /// Set the reference to a magnetic field manager
-      void set_mag_field_manager (const emfield::electromagnetic_field_manager &);
+      void set_mag_field_manager(const emfield::electromagnetic_field_manager &);
 
       /// Return a non-mutable reference to the magnetic field manager
-      const emfield::electromagnetic_field_manager & get_mag_field_manager () const;
+      const emfield::electromagnetic_field_manager & get_mag_field_manager() const;
 
       /// Check for a referenced electromagnetic field manager
-      bool has_em_field_manager () const;
+      bool has_em_field_manager() const;
 
       /// Set the reference to a electromagnetic field manager
-      void set_em_field_manager (const emfield::electromagnetic_field_manager &);
+      void set_em_field_manager(const emfield::electromagnetic_field_manager &);
 
       /// Return a non-mutable reference to the electromagnetic field manager
-      const emfield::electromagnetic_field_manager & get_em_field_manager () const;
+      const emfield::electromagnetic_field_manager & get_em_field_manager() const;
 
       /// Main initialization method
-      void initialize (const datatools::properties & config_);
+      void initialize(const datatools::properties & config_);
 
       /// Main reset method
       void reset();
 
       /// Return a non-mutable reference to the collection of embedded sensitive detectors
-      const sensitive_detector_dict_type & get_sensitive_detectors () const;
+      const sensitive_detector_dict_type & get_sensitive_detectors() const;
 
       /// Return a mutable reference to the embeded step hit processor factory
-      SHPF_type & grab_step_hit_processor_factory ();
+      SHPF_type & grab_step_hit_processor_factory();
 
-      void set_materials_geom_plugin_name (const std::string & mpn_);
+      void set_materials_geom_plugin_name(const std::string & mpn_);
 
-      void set_emfield_geom_plugin_name (const std::string & fpn_);
+      void set_emfield_geom_plugin_name(const std::string & fpn_);
 
       /// G4 interface
-      G4VPhysicalVolume * Construct ();
+      G4VPhysicalVolume * Construct();
 
       /** Generate the GDML file from the geometry manager */
-      void write_tmp_gdml_file ();
+      void write_tmp_gdml_file();
 
     protected:
 
@@ -159,30 +161,30 @@ namespace mctools {
        *    add some additionnal (non-official) sensitive detectors
        *
        */
-      void _construct_sensitive_detectors ();
+      void _construct_sensitive_detectors();
 
       /** Construct regions */
-      void _construct_regions ();
+      void _construct_regions();
 
       /** Construct biasing */
-      void _construct_biasing ();
+      void _construct_biasing();
 
       /** Setup electromagnetic field */
-      void _construct_electromagnetic_field ();
+      void _construct_electromagnetic_field();
 
       /** Destroy electromagnetic field */
-      void _destroy_electromagnetic_field ();
+      void _destroy_electromagnetic_field();
 
       /** This method automatically setup G4 visualization attributes
        * from the main geometry model.
        */
-      void _set_visualization_attributes ();
+      void _set_visualization_attributes();
 
       /** Setup user limits */
-      void _set_user_limits ();
+      void _set_user_limits();
 
       /** Hidden construction method */
-      virtual G4VPhysicalVolume * _g4_construct ();
+      virtual G4VPhysicalVolume * _g4_construct();
 
     protected:
 
@@ -190,19 +192,19 @@ namespace mctools {
 
     private:
 
-      bool _initialized_;                        //!< Initialization flag
-      bool _abort_on_error_;                     //!< Flag to force abortion on error while building the geometry setup
+      bool _initialized_ = false;                        //!< Initialization flag
+      bool _abort_on_error_ = true;                     //!< Flag to force abortion on error while building the geometry setup
 
       //! Main Geant4 manager:
-      manager * _g4_manager_;                    //!< Handle to the Geant4 simulation manager
+      manager * _g4_manager_ = nullptr;                    //!< Handle to the Geant4 simulation manager
 
       //! Geometry manager:
-      const geomtools::manager * _geom_manager_; //!< Reference to the geometry manager
+      const geomtools::manager * _geom_manager_ = nullptr; //!< Reference to the geometry manager
 
       //! Magnetic field manager:
-      bool   _using_em_field_;                   //!< Flag to use the Geant4 EM field system
+      bool   _using_em_field_ = false;                   //!< Flag to use the Geant4 EM field system
       std::string _emfield_geom_plugin_name_;    //!< The name of the EM field manager geometry plugin
-      const emfield::electromagnetic_field_manager * _em_field_manager_; //!< Reference to the EM field manager
+      const emfield::electromagnetic_field_manager * _em_field_manager_ = nullptr; //!< Reference to the EM field manager
       datatools::properties _em_field_aux_;      //!< Auxiliary properties related to EM field
       double _miss_distance_unit_;               //!< Default miss distance length unit
       double _general_miss_distance_;            //!< Default general miss distance
@@ -212,7 +214,7 @@ namespace mctools {
       em_field_g4_data_type _em_field_g4_data_; //!< Dictionary of G4 EM field working data
 
       //! User limits:
-      bool                      _using_user_limits_;
+      bool                      _using_user_limits_ = true;
       datatools::properties     _limits_config_;
       std::list<G4UserLimits *> _user_limits_col_;
 
@@ -225,25 +227,25 @@ namespace mctools {
       std::string _gdml_filename_;
 
       // Visualization:
-      bool   _using_vis_attributes_;
+      bool   _using_vis_attributes_ = true;
       std::map<std::string, G4VisAttributes *> _vis_attributes_;
 
       // Sensitive detectors:
-      bool _using_sensitive_detectors_;
+      bool _using_sensitive_detectors_ = true;
       sensitive_detector_dict_type _sensitive_detectors_;
       datatools::multi_properties _SD_params_;
 
       // Regions:
-      bool _using_regions_;
+      bool _using_regions_ = true;
       regions_setup _region_definitions_;
 
-      // Step hit processor factory stuff (from mctools::core::step_hit_processor_factory) :
+      // Step hit processor factory stuff (from mctools::step_hit_processor_factory) :
       datatools::properties _SHPF_config_;
       SHPF_type             _SHPF_;
 
-      bool   _using_biasing_; //!< Flag to use the Geant4 biasing system
+      bool   _using_biasing_ = false; //!< Flag to use the Geant4 biasing system
       datatools::properties _biasing_config_; //!< Configuration properties for the biasing manager
-      boost::scoped_ptr<biasing_manager> _biasing_manager_;
+      boost::scoped_ptr<biasing_manager> _biasing_manager_; //!< Biaising manager
 
     };
 
