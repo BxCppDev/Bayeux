93ed9fe1ec3ee1db98967c40c7b5c85312b97729
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-09-01 11:12:42 +0000

    Modify test program

--- source/bxmctools/testing/test_g4_detector_construction.cxx
+++ source/bxmctools/testing/test_g4_detector_construction.cxx
@@ -1,6 +1,6 @@
 // test_g4_detector_construction.cxx
 /*
- * Copyright (C) 2011-2013 Francois Mauger <mauger@lpccaen.in2p3.fr>
+ * Copyright (C) 2011-2016 Francois Mauger <mauger@lpccaen.in2p3.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,55 +30,77 @@
 
 // This project:
 #include <mctools/g4/detector_construction.h>
-#include <mctools/g4/manager.h>
+
+void test_1(bool debug_);
 
 int main (int argc_, char ** argv_)
 {
   using namespace std;
   int error_code = EXIT_SUCCESS;
   try {
-    clog << "Test program for class 'mctools::g4::detector_construction'!" << endl;
+    std::clog << "Test program for class 'mctools::g4::detector_construction'!" << std::endl;
 
     bool debug = false;
 
     int iarg = 1;
     while (iarg < argc_) {
-      string token = argv_[iarg];
+      std::string token = argv_[iarg];
       if (token[0] == '-') {
-        string option = token;
+        std::string option = token;
         if ((option == "-d") || (option == "--debug")) {
           debug = true;
         } else {
-          clog << "warning: ignoring option '" << option << "'!" << endl;
+          std::clog << "warning: ignoring option '" << option << "'!" << std::endl;
         }
       } else {
-        string argument = token;
+        std::string argument = token;
         {
-          clog << "warning: ignoring argument '" << argument << "'!" << endl;
+          std::clog << "warning: ignoring argument '" << argument << "'!" << std::endl;
         }
       }
       iarg++;
     }
 
-    // Geometry manager :
-    geomtools::manager geo_manager;
-    if (debug) geo_manager.set_logging_priority (datatools::logger::PRIO_DEBUG);
-    datatools::properties gm_setup;
-    std::string gm_filename = "${MCTOOLS_TESTING_DIR}/config/g4/test-2.0/geometry/manager.conf";
-    datatools::fetch_path_with_env(gm_filename);
-    datatools::properties::read_config(gm_filename, gm_setup);
-    geo_manager.initialize (gm_setup);
+    test_1(debug);
 
-    // Additional plugins :
-    // std::string mag_field_plugin_file="${MCTOOLS_TESTING_DIR}/config/fields/test_emfield_geom_plugin.conf";
-    // datatools::fetch_path_with_env(mag_field_plugin_file);
-    // datatools::multi_properties mag_field_plugin_config;
-    // mag_field_plugin_config.read(mag_field_plugin_file);
-    // geo_manager.load_plugins(mag_field_plugin_config);
+    std::clog << "The end." << std::endl;
+
+  } catch (std::exception & x) {
+    std::cerr << "error: " << x.what () << std::endl;
+    error_code = EXIT_FAILURE;
+  } catch (...) {
+    std::cerr << "error: " << "unexpected error!" << std::endl;
+    error_code = EXIT_FAILURE;
+  }
+  return (error_code);
+}
+
+void test_1(bool debug_)
+{
+  std::clog << "\ntest_1: Entering..." << std::endl;
+
+  // Geometry manager :
+  geomtools::manager geo_manager;
+  if (debug_) geo_manager.set_logging_priority(datatools::logger::PRIO_DEBUG);
+  datatools::properties gm_setup;
+  std::string gm_filename = "${MCTOOLS_TESTING_DIR}/config/g4/test-2.0/geometry/manager.conf";
+  datatools::fetch_path_with_env(gm_filename);
+  datatools::properties::read_config(gm_filename, gm_setup);
+  geo_manager.initialize (gm_setup);
+
+  // Additional plugins :
+  // std::string mag_field_plugin_file="${MCTOOLS_TESTING_DIR}/config/fields/test_emfield_geom_plugin.conf";
+  // datatools::fetch_path_with_env(mag_field_plugin_file);
+  // datatools::multi_properties mag_field_plugin_config;
+  // mag_field_plugin_config.read(mag_field_plugin_file);
+  // geo_manager.load_plugins(mag_field_plugin_config);
+
+  {
+    std::clog << "\ntest_1: Entering Geant4 detector construction block..." << std::endl;
 
     // Geant4 detector construction :
-    mctools::g4::manager my_g4_manager;
-    mctools::g4::detector_construction my_detector_construction (my_g4_manager);
+
+    mctools::g4::detector_construction my_detector_construction;
     my_detector_construction.set_geometry_manager(geo_manager);
     datatools::properties dc_setup;
     dc_setup.store("logging.priority", "trace");
@@ -91,17 +113,12 @@ int main (int argc_, char ** argv_)
     std::cerr << std::endl << "******************* INITIALIZE *************" << std::endl;
     my_detector_construction.initialize(dc_setup);
     my_detector_construction.Construct();
+    my_detector_construction.reset();
 
+    std::clog << "\ntest_1: Exiting Geant4 detector construction block...." << std::endl;
   }
-  catch (exception & x) {
-    cerr << "error: " << x.what () << endl;
-    error_code = EXIT_FAILURE;
-  }
-  catch (...) {
-    cerr << "error: " << "unexpected error!" << endl;
-    error_code = EXIT_FAILURE;
-  }
-  return (error_code);
+  std::clog << "\ntest_1: Geant4 detector construction block is closed." << std::endl;
+
+  std::clog << "\ntest_1: Exiting." << std::endl;
+  return;
 }
-
-// end of test_g4_detector_construction.cxx
