3580b7109b47cd09befb260d0a03cb8abcc0e6ef
Francois Mauger <mauger@lpccaen.in2p3.fr>
2016-09-26 23:07:16 +0000

    New mctools::signal::base_signal class

--- source/bxmctools/src/the_serializable.cc
+++ source/bxmctools/src/the_serializable.cc
@@ -13,9 +13,9 @@
 #include <mctools/base_step_hit.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(mctools::base_step_hit)
 
+#include <mctools/signal/base_signal.ipp>
+DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(mctools::signal::base_signal)
 
 #include <mctools/simulated_data.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(mctools::simulated_data)
 BOOST_CLASS_EXPORT_IMPLEMENT(mctools::simulated_data)
-
-
