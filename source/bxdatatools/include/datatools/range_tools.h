c45c09988e463f63ec5e69ffcbbc0f1f2de8251a
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-07-12 21:49:19 +0200

    [datatools] Add static constant delimiters

--- source/bxdatatools/include/datatools/range_tools.h
+++ source/bxdatatools/include/datatools/range_tools.h
@@ -49,7 +49,9 @@ namespace datatools {
     static const char token_open_included  = '[';
     static const char token_close_included = ']';
     static const char token_open_excluded  = '(';
+    static const char token_open_excluded_alt  = ']';
     static const char token_close_excluded = ')';
+    static const char token_close_excluded_alt = '[';
     static const char token_separator      = ','; // Should not be favored
     static const char token_separator2     = ';'; // Favored separator
     static const char token_invalid        = '!';
